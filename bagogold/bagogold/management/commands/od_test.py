e865253ebaf7cc92c87850f9cda444d4c61bb158
Guilherme de Deus <kingbowserii@gmail.com>
2016-03-31 22:21:44 -0300

    Transa??o de preenchimento de compras funciona!

--- bagogold/bagogold/management/commands/od_test.py
+++ bagogold/bagogold/management/commands/od_test.py
@@ -2,11 +2,18 @@
 from bagogold.bagogold.utils.divisoes import preencher_operacoes_div_principal
 from django.core.management.base import BaseCommand
 from bagogold.bagogold.models.td import OperacaoTitulo
+from bagogold.bagogold.models.fii import OperacaoFII
+from bagogold.bagogold.models.lc import OperacaoLetraCredito
 
 class Command(BaseCommand):
     help = 'Teste preencher operação divisão principal'
 
     def handle(self, *args, **options):
-        for operacao in OperacaoTitulo.objects.all():
-            preencher_operacoes_div_principal(operacao)
+        print 'Descomente por sua conta e risco'
+#         for operacao in OperacaoFII.objects.all():
+#             preencher_operacoes_div_principal(operacao)
+#         for operacao in OperacaoLetraCredito.objects.all():
+#             preencher_operacoes_div_principal(operacao)
+#         for operacao in OperacaoTitulo.objects.all():
+#             preencher_operacoes_div_principal(operacao)
 
