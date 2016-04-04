239cac283af89ab2c5126069f51490cf938c4095
Guilherme Henrique <kingbowserii@gmail.com>
2016-04-03 23:43:47 -0300

    Adicionar checkbox para as tabelas do detalhamento de divis?es

--- bagogold/bagogold/models/acoes.py
+++ bagogold/bagogold/models/acoes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+import datetime
  
 class Acao (models.Model):
     ticker = models.CharField(u'Ticker da ação', max_length=10)
@@ -7,6 +8,14 @@ class Acao (models.Model):
     
     def __unicode__(self):
         return self.ticker
+    
+    def valor_no_dia(self, dia):
+        if dia == datetime.date.today():
+            try:
+                return ValorDiarioAcao.objects.filter(acao__ticker=self.ticker, data_hora__day=dia.day, data_hora__month=dia.month).order_by('-data_hora')[0].preco_unitario
+            except:
+                pass
+        return HistoricoAcao.objects.filter(acao__ticker=self.ticker, data__lte=dia).order_by('-data')[0].preco_unitario
     
 class Provento (models.Model):
     acao = models.ForeignKey('Acao')
