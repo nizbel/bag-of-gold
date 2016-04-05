c99a13fe22619d653619218c636c1a778564fb24
Guilherme Henrique <kingbowserii@gmail.com>
2016-04-05 16:20:08 -0300

    FII com ticker unicos. Script para adicionar pelo site da Bovespa funcionando.

--- bagogold/bagogold/testFII.py
+++ bagogold/bagogold/testFII.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.acoes import Acao, OperacaoAcao, Provento, HistoricoAcao
-from bagogold.bagogold.models.fii import FII, HistoricoFII
-from decimal import Decimal
-from django.contrib.auth.models import User
-from django.utils import timezone
+from bagogold.bagogold.models.fii import FII
+from random import randint
+from threading import Thread
 from urllib2 import Request, urlopen, URLError, HTTPError
-from yahoo_finance import Share
 import datetime
-import pyexcel
+import re
 import simplejson
+import sys
 import time
 
 # Para buscar valor de multiplas acoes
@@ -21,115 +19,70 @@ try:
 except ImportError:
     from urllib import urlencode
 
+class BuscaTickerThread(Thread):
+    def __init__(self, ticker):
+        self.ticker = ticker 
+        super(BuscaTickerThread, self).__init__()
 
-def buscar_historico(ticker):
+    def run(self):
+        try:
+            busca_ticker(self.ticker, 0)
+        except:
+            print sys.exc_info()[1]
+            pass
+
+
+def verificar_fiis_listados():
+#     http://bvmf.bmfbovespa.com.br/Fundos-Listados/FundosListadosDetalhe.aspx?Sigla=AEFI&tipoFundo=Imobiliario&aba=abaPrincipal&idioma=pt-br
+    # Verificar siglas listadas
+    fii_url = 'http://bvmf.bmfbovespa.com.br/Fundos-Listados/FundosListados.aspx?tipoFundo=imobiliario&amp;Idioma=pt-br'
+    req = Request(fii_url)
     try:
-        historico = list()
-        response_csv = urlopen('http://ichart.finance.yahoo.com/table.csv?s=%s.SA&a=0&b=01&c=2010&d=%s&e=%s&f=%s&g=d&ignore=.csv' % \
-                               (ticker, int(datetime.date.today().month), datetime.date.today().day, datetime.date.today().year))
-        csv = response_csv.read()
-        book = pyexcel.get_book(file_type="csv", file_content=csv)
-        sheets = book.to_dict()
-        for key in sheets.keys():
-            dados_papel = sheets[key]
-            for linha in range(1,len(dados_papel)):
-                # Testar se a linha de data está vazia, passar ao proximo
-                if dados_papel[linha][0] == '':
-                    break
-                dados_data = {}
-                dados_data['Date'] = dados_papel[linha][0]
-                dados_data['Close'] = dados_papel[linha][4]
-                historico.append(dados_data)
-    except:
-        papel = Share('%s.SA' % (ticker))
-        historico = papel.get_historical('2010-01-01', datetime.datetime.now().strftime('%Y-%m-%d'))
-    
-    return historico
-        
-def preencher_historico_acao(ticker, historico):
-    acao = Acao.objects.get(ticker=ticker)
-#         print acao
-    for dia_papel in historico:
-        if not HistoricoAcao.objects.filter(acao=acao, data=dia_papel['Date']):
-            historico_acao = HistoricoAcao(acao=acao, data=dia_papel['Date'], preco_unitario=Decimal(dia_papel['Close']).quantize(Decimal('0.01')))
-            historico_acao.save()
-    return
+        response = urlopen(req)
+    except HTTPError as e:
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+    except URLError as e:
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    else:
+        print 'Host: %s' % (req.get_host())
+        data = response.read()
+        string_importante = (data[data.find('<table>'):data.find('</table>')])
+#         urls = re.findall('[h]?[t]?[t]?[p]?[s]?[:]?[\/]?[\/]?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\.xls', string_importante)
+        siglas = re.findall('[A-Za-z0-9]+', re.sub(r'.*?<tr>.*?<td>.*?<\/td>.*?<td>.*?<\/td>.*?<td>.*?<\/td>.*?<td><span.*?>(.*?)<\/span><\/td>.*?<\/tr>.*?', r'\1,', string_importante, flags=re.DOTALL))
+        threads = []
+        start_time = time.time()
+        for sigla in siglas:
+            t = BuscaTickerThread(sigla)
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+        print time.time() - start_time, len(siglas)
+            
 
-def preencher_historico_fii(ticker, historico):
-    fii = FII.objects.get(ticker=ticker)
-#         print fii
-    for dia_papel in historico:
-        if not HistoricoFII.objects.filter(fii=fii, data=dia_papel['Date']):
-            historico_fii = HistoricoFII(fii=fii, data=dia_papel['Date'], preco_unitario=Decimal(dia_papel['Close']).quantize(Decimal('0.01')))
-            historico_fii.save()
-    return
-        
-def buscar_ultimos_valores_geral_acao():
-    # Dados para a conexão
-    PUBLIC_API_URL  = 'http://query.yahooapis.com/v1/public/yql'
-    DATATABLES_URL  = 'store://datatables.org/alltableswithkeys'
-    connection = HTTPConnection('query.yahooapis.com')
-    
-    # Preparar acoes a serem buscadas
-    acoes_formatadas = ''
-    for acao in Acao.objects.all():
-        acoes_formatadas += '%s.SA ' % (acao.ticker)
-    acoes_formatadas.strip()
-    
-    yql = 'select * from yahoo.finance.quotes where symbol = "%s"' % (acoes_formatadas)
-    connection.request('GET', PUBLIC_API_URL + '?' + urlencode({ 'q': yql, 'format': 'json', 'env': DATATABLES_URL }))
-    resultado = simplejson.loads(connection.getresponse().read())
-    
-    # Preencher valores de ultimas negociacoes
-    valores_diarios = {}
-    
-    for dados in resultado['query']['results']['quote']:
-        valores_diarios[dados['Symbol']] = dados['LastTradePriceOnly']
-    return valores_diarios
-
-def buscar_ultimos_valores_geral_fii():
-    # Dados para a conexão
-    PUBLIC_API_URL  = 'http://query.yahooapis.com/v1/public/yql'
-    DATATABLES_URL  = 'store://datatables.org/alltableswithkeys'
-    connection = HTTPConnection('query.yahooapis.com')
-    
-    # Preparar acoes a serem buscadas
-    fiis_formatados = ''
-    for fii in FII.objects.all():
-        fiis_formatados += '%s.SA ' % (fii.ticker)
-    fiis_formatados.strip()
-    
-    yql = 'select * from yahoo.finance.quotes where symbol = "%s"' % (fiis_formatados)
-    connection.request('GET', PUBLIC_API_URL + '?' + urlencode({ 'q': yql, 'format': 'json', 'env': DATATABLES_URL }))
-    resultado = simplejson.loads(connection.getresponse().read())
-    
-    # Preencher valores de ultimas negociacoes
-    valores_diarios = {}
-    
-    for dados in resultado['query']['results']['quote']:
-        valores_diarios[dados['Symbol']] = dados['LastTradePriceOnly']
-    return valores_diarios
-
-# TODO TEST THIS
-def buscar_historico_geral():
-    # Dados para a conexão
-    PUBLIC_API_URL  = 'http://query.yahooapis.com/v1/public/yql'
-    DATATABLES_URL  = 'store://datatables.org/alltableswithkeys'
-    connection = HTTPConnection('query.yahooapis.com')
-    
-    # Preparar acoes a serem buscadas
-    acoes_formatadas = ''
-    for acao in Acao.objects.all():
-        acoes_formatadas += '%s.SA ' % (acao.ticker)
-    acoes_formatadas.strip()
-    
-    yql = 'select * from yahoo.finance.quotes where symbol = "%s"' % (acoes_formatadas)
-    connection.request('GET', PUBLIC_API_URL + '?' + urlencode({ 'q': yql, 'format': 'json', 'env': DATATABLES_URL }))
-    resultado = simplejson.loads(connection.getresponse().read())
-    
-    # Preencher valores de ultimas negociacoes
-    valores_diarios = {}
-    
-    for dados in resultado['query']['results']['quote']:
-        valores_diarios[dados['Symbol']] = dados['LastTradePriceOnly']
-    return valores_diarios
+def busca_ticker(sigla, num_tentativas):
+    tempo_espera = randint(1,max(1, 30-(num_tentativas*10)))
+    time.sleep(tempo_espera)
+    sigla_url = 'http://bvmf.bmfbovespa.com.br/Fundos-Listados/FundosListadosDetalhe.aspx?Sigla=%s&tipoFundo=Imobiliario&aba=abaPrincipal&idioma=pt-br' % (sigla)
+    resposta = urlopen(sigla_url)
+    string_retorno = resposta.read()
+    if 'Sistema indisponivel' in string_retorno:
+#         tempo_espera = randint(max(1, 3-num_tentativas),max(2, 8-num_tentativas))
+#         print sigla, ": sistema indisponivel, tentando novamente em %s segundos" % (tempo_espera)
+#         time.sleep(tempo_espera)
+        busca_ticker(sigla, num_tentativas+1)
+        return
+    string_importante = string_retorno[string_retorno.find('Códigos de Negociação'):string_retorno.find('CNPJ')]
+    if '</a>' in string_importante:
+        cod_negociacao = re.sub(r'.*?<a.*?>(.*?)<\/a>.*', r'\1', string_importante, flags=re.DOTALL)
+#         print sigla, ": ", cod_negociacao
+        try:
+            fii = FII(ticker=cod_negociacao)
+            fii.save()
+            print 'FII:', fii, 'criado'
+        except:
+            print 'FII:', cod_negociacao, 'ja existia'
+    else:
+        print sigla, ": Não possui código"
