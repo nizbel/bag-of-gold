0ddfb347bf846e3554a1a4e3a1d38e68c7757591
Guilherme Henrique <kingbowserii@gmail.com>
2016-04-06 00:40:07 -0300

    Usando threads para buscar pdfs

--- bagogold/bagogold/management/commands/fii_prov.py
+++ bagogold/bagogold/management/commands/fii_prov.py
@@ -3,10 +3,30 @@ from bagogold.bagogold.models.fii import FII
 from bagogold.bagogold.testFII import buscar_rendimentos_fii, \
     ler_demonstrativo_rendimentos
 from django.core.management.base import BaseCommand
+from threading import Thread
+
+class BuscaRendimentosFIIThread(Thread):
+    def __init__(self, ticker):
+        self.ticker = ticker 
+        super(BuscaRendimentosFIIThread, self).__init__()
+
+    def run(self):
+        try:
+            buscar_rendimentos_fii(self.ticker)
+        except Exception as ex:
+            template = "An exception of type {0} occured. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print self.ticker, "Thread:", message
+            pass
 
 class Command(BaseCommand):
     help = 'Teste buscar distribuições de rendimentos na bovespa'
 
     def handle(self, *args, **options):
+        threads = []
         for fii in FII.objects.all():
-            buscar_rendimentos_fii(fii.ticker)
+            t = BuscaRendimentosFIIThread(fii.ticker)
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
