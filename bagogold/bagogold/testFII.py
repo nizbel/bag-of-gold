ddb130a3e47b44086d442c1f499793aa9047b2ec
Guilherme Henrique <kingbowserii@gmail.com>
2016-04-08 00:31:19 -0300

    Testando proventos vindo de pdf

--- bagogold/bagogold/testFII.py
+++ bagogold/bagogold/testFII.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.fii import FII
+from bagogold.bagogold.models.fii import FII, ProventoFII
 from cStringIO import StringIO
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from decimal import Decimal
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
 from random import randint
+from subprocess import call
 from threading import Thread
 from urllib2 import Request, urlopen, URLError, HTTPError
 import datetime
+import os
 import re
 import simplejson
-from subprocess import call
 import sys
 import time
 
@@ -119,16 +121,31 @@ def buscar_rendimentos_fii(ticker):
 #         http://bvmf.bmfbovespa.com.br/sig/FormConsultaPdfDocumentoFundos.asp?strSigla=BBPO&amp;strData=
         urls = re.findall('href=\"(.*?)\".*?>Distribuição', string_importante,flags=re.IGNORECASE)
         urls += re.findall('href=\"(.*?)\".*?>Amortização', string_importante,flags=re.IGNORECASE)
-        # Teste
-        pdfs_convertidos = 0
-        total = 0
 #         print len(urls)
+        proventos = list()
         for url in urls:
             url = url.replace('&amp;', '&')
 #             print url
-            pdfs_convertidos += ler_demonstrativo_rendimentos(url, ticker)
-            total += 1
-        return (pdfs_convertidos, total)
+            proventos.append((ler_demonstrativo_rendimentos(url, ticker),url))
+        
+        # Adicionar proventos
+        # Buscar FII
+        fii = FII.objects.get(ticker=ticker)
+        for provento in proventos:
+            if provento[0][0] and provento[0][1]:
+                datas_lidas = list()
+                for data_lida in provento[0][0]:
+                    datas_lidas.append(datetime.datetime.strptime(data_lida, "%d/%m/%Y").date())
+                datas_lidas.sort()
+                datas_lidas = list(set(datas_lidas))
+                # Se lista tiver pelo menos 2 datas diferentes, preparar para adicionar
+                if len(datas_lidas) >= 2:
+                    # Primeiro valor é o do provento
+                    valor = Decimal(provento[0][1][0].replace(' ', '').replace(',', '.'))
+                    print ticker, ':', datas_lidas[-2:], valor 
+                    novo_provento = ProventoFII(fii=fii, data_ex=datas_lidas[-2], data_pagamento=datas_lidas[-1], valor_unitario=valor, url_documento=provento[1])
+                    novo_provento.save()
+                
         
         
 
@@ -141,9 +158,11 @@ def ler_demonstrativo_rendimentos(pdf_url, ticker):
     except HTTPError as e:
         print 'The server couldn\'t fulfill the request.'
         print 'Error code: ', e.code
+        return ()
     except URLError as e:
         print 'We failed to reach a server.'
         print 'Reason: ', e.reason
+        return ()
     else:
         try:
             arquivo_rendimentos = StringIO(response.read())
@@ -165,17 +184,51 @@ def ler_demonstrativo_rendimentos(pdf_url, ticker):
             device.close()
             text = retstr.getvalue()
             retstr.close()
-            datas = re.findall('(\d{1,2}[\.\/]\d{1,2}[\.\/]\d\d\d\d)', text)
-            valor = re.findall('(\d+\s*?,\s*?\d+)', text)
-#             if len(datas) == 0:
-            print pdf_url, ticker, '-> datas:', datas, 'valor', valor
-            return 1
+            return buscar_info_proventos_fii_no_texto(text)
         except PDFTextExtractionNotAllowed as e:
-            print 'Using qpdf'
-            
-            call('qpdf --password=%s --decrypt %s %s' %('', pdf_filename, pdf_filename_decr), shell=True)
+            # Tenta descriptografar o arquivo com qpdf para poder ler
+            try:
+#                 print 'Using qpdf'
+                with open('%s.pdf' % (ticker), "wb") as local_file:
+                    local_file.write(arquivo_rendimentos.getvalue())
+                call('qpdf --password=%s --decrypt %s %s' %('', '%s.pdf' % (ticker), '%s-decrypted.pdf' % (ticker)), shell=True)
+#                 print 'Done!'
+                # Abrir arquivo
+                arquivo_rendimentos = open('%s-decrypted.pdf' % (ticker), 'rb')
+                rsrcmgr = PDFResourceManager()
+                retstr = StringIO()
+                codec = 'utf-8'
+                laparams = LAParams()
+                device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+                interpreter = PDFPageInterpreter(rsrcmgr, device)
+                password = ""
+                maxpages = 0
+                caching = True
+                pagenos=set()
+                
+                for page in PDFPage.get_pages(arquivo_rendimentos, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+                    interpreter.process_page(page)
+                 
+                arquivo_rendimentos.close()
+                device.close()
+                text = retstr.getvalue()
+                retstr.close()
+                os.remove('%s.pdf' % (ticker))
+                os.remove('%s-decrypted.pdf' % (ticker))
+                return buscar_info_proventos_fii_no_texto(text)
+            except Exception as e:
+                template = "An exception of type {0} occured. Arguments:\n{1!r}"
+                message = template.format(type(e).__name__, e.args)
+                print pdf_url, "->", message
+                return ()
         except Exception as e:
             template = "An exception of type {0} occured. Arguments:\n{1!r}"
             message = template.format(type(e).__name__, e.args)
             print pdf_url, "->", message
-    return 0
+            return ()
+
+def buscar_info_proventos_fii_no_texto(texto):
+    datas = re.findall('(\d{1,2}[\.\/]\d{1,2}[\.\/]\d\d\d\d)', texto)
+    valor = re.findall('(\d+\s*?,\s*?\d+)', texto)
+    print 'datas:', datas, 'valor', valor
+    return (datas, valor)
