dc24e3ea8c15a0ea1c41ad66ae9d1aea2ed5d95f
Guilherme Henrique <kingbowserii@gmail.com>
2016-04-10 11:43:37 -0300

    Adicionada restri??o de unicidade para provento FII

--- bagogold/bagogold/models/fii.py
+++ bagogold/bagogold/models/fii.py
@@ -14,6 +14,9 @@ class ProventoFII (models.Model):
     data_pagamento = models.DateField(u'Data do pagamento')
     url_documento = models.CharField(u'URL do documento', blank=True, null=True, max_length=200)
     
+    class Meta:
+        unique_together=(('data_ex', 'data_pagamento'), ('fii',))
+    
 class OperacaoFII (models.Model):
     preco_unitario = models.DecimalField(u'Preço unitário', max_digits=11, decimal_places=2)  
     quantidade = models.IntegerField(u'Quantidade') 
