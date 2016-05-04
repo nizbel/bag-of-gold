ccba9f61d3331b1f72be1bec551a35a9ee0699f4
Guilherme de Deus <kingbowserii@gmail.com>
2016-05-03 23:47:46 -0300

    Migrando codigo cvm podendo ser nulo

--- bagogold/bagogold/models/empresa.py
+++ bagogold/bagogold/models/empresa.py
@@ -3,4 +3,4 @@ from django.db import models
  
 class Empresa (models.Model):
     nome = models.CharField('Nome da empresa', max_length=100)
-    codigo_cvm = models.CharField('Código CVM', max_length=10)
+    codigo_cvm = models.CharField('Código CVM', max_length=10, blank=True, null=True)
