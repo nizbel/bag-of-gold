b0f89dfb003d707587efa7795f830748d2711e5d
Guilherme Henrique <kingbowserii@gmail.com>
2016-05-16 20:16:31 -0300

    Busca de proventos fii parcialmente testado e funcional

--- bagogold/bagogold/testFII.py
+++ bagogold/bagogold/testFII.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from bagogold.bagogold.models.fii import FII, ProventoFII
+from bagogold.bagogold.utils.fii import calcular_qtd_fiis_ate_dia_por_ticker
 from cStringIO import StringIO
 from decimal import Decimal
 from pdfminer.converter import TextConverter
@@ -11,9 +12,9 @@ from subprocess import call
 from threading import Thread
 from urllib2 import Request, urlopen, URLError, HTTPError
 import datetime
+import mechanize
 import os
 import re
-import simplejson
 import sys
 import time
 
@@ -128,25 +129,56 @@ def buscar_rendimentos_fii(ticker):
 #             print url
             proventos.append((ler_demonstrativo_rendimentos(url, ticker),url))
         
-        # Adicionar proventos
-        # Buscar FII
-        fii = FII.objects.get(ticker=ticker)
-        for provento in proventos:
-            if provento[0][0] and provento[0][1]:
-                datas_lidas = list()
-                for data_lida in provento[0][0]:
-                    datas_lidas.append(datetime.datetime.strptime(data_lida, "%d/%m/%Y").date())
-                # Retirar datas duplicadas
-                datas_lidas = list(set(datas_lidas))
-                # Ordenar por data
-                datas_lidas.sort()
-                # Se lista tiver pelo menos 2 datas diferentes, preparar para adicionar
-                if len(datas_lidas) >= 2:
-                    # Primeiro valor é o do provento
-                    valor = Decimal(provento[0][1][0].replace(' ', '').replace(',', '.'))
-                    print ticker, ':', datas_lidas[-2:], valor 
-                    novo_provento, criado = ProventoFII(fii=fii, data_ex=datas_lidas[-2], data_pagamento=datas_lidas[-1], valor_unitario=valor, url_documento=provento[1])
-                    novo_provento.save()
+    # Pesquisar no novo formato
+    fii_url = 'http://bvmf.bmfbovespa.com.br/Fundos-Listados/FundosListadosDetalhe.aspx?Sigla=%s&tipoFundo=Imobiliario&aba=tabInformacoesRelevantes&idioma=pt-br' % ticker[0:4]
+    # Usar mechanize para simular clique do usuario no javascript
+    br = mechanize.Browser()
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    response = br.open(fii_url)
+    
+    html = response.read()
+
+    br.select_form(nr=0)
+    br.set_all_readonly(False)
+    br.find_control("ctl00$botaoNavegacaoVoltar").disabled = True
+    br.find_control(id='ctl00_contentPlaceHolderConteudo_ucInformacoesRelevantes_txtPeriodoDe').value = '01/01/2001'
+    br.find_control(id='ctl00_contentPlaceHolderConteudo_ucInformacoesRelevantes_txtPeriodoAte').value = time.strftime("%d/%m/%Y")
+    br.find_control(id='ctl00_contentPlaceHolderConteudo_ucInformacoesRelevantes_ddlCategoria').value = ['4']
+    
+    response = br.submit()
+    html = response.read()
+    if 'Sistema indisponivel' in html:
+        return buscar_rendimentos_fii(ticker)
+    
+    inicio = html.find('<div id="ctl00_contentPlaceHolderConteudo_pvwInfoRelevantes">')
+    fim = html.find('id="ctl00_contentPlaceHolderConteudo_pvwItem2"', inicio)
+    string_importante = (html[inicio:fim])
+    
+    urls = re.findall('<tr><td>Assunto:</td><td>Distribuição de (?:Rendimento[s]?|Amortização)</td></tr>.*?<a href=\"(https://fnet.bmfbovespa.com.br/fnet/publico/downloadDocumento\?id=[\d]*?)\">Aviso aos Cotistas</a>', string_importante,flags=re.IGNORECASE|re.DOTALL)
+    
+#     proventos_novo = list()
+    for url in urls:
+        proventos.append((ler_demonstrativo_rendimentos(url, ticker),url))
+    
+    # Adicionar proventos
+    # Buscar FII
+    fii = FII.objects.get(ticker=ticker)
+    for provento in proventos:
+        if provento[0][0] and provento[0][1]:
+            datas_lidas = list()
+            for data_lida in provento[0][0]:
+                datas_lidas.append(datetime.datetime.strptime(data_lida, "%d/%m/%Y").date())
+            # Retirar datas duplicadas
+            datas_lidas = list(set(datas_lidas))
+            # Ordenar por data
+            datas_lidas.sort()
+            # Se lista tiver pelo menos 2 datas diferentes, preparar para adicionar
+            if len(datas_lidas) >= 2:
+                # Primeiro valor é o do provento
+                valor = Decimal(provento[0][1][0].replace(' ', '').replace(',', '.'))
+#                     print ticker, ':', datas_lidas[-2:], valor 
+                novo_provento = ProventoFII(fii=fii, data_ex=datas_lidas[-2], data_pagamento=datas_lidas[-1], valor_unitario=valor, url_documento=provento[1])
+#                     novo_provento.save()
                 
         
         
