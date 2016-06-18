40831876ca27a62fbda8e29f6fdd2ab8c39c9aa8
Guilherme Henrique <kingbowserii@gmail.com>
2016-06-18 12:22:57 -0300

    Adicionando login e logout Alterando home para ficar mais rapida ao carregar

--- bagogold/bagogold/views/investidores/investidores.py
+++ bagogold/bagogold/views/investidores/investidores.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import views
+from django.contrib import messages
+
+
+def logout(request, *args, **kwargs):
+    messages.success(request, 'Logout feito com sucesso')
+    return views.logout(request, *args, **kwargs)
--- bagogold/urls.py
+++ bagogold/urls.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout, password_change, password_change_done, \
-    password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
     # Examples:
@@ -10,15 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', admin.site.urls),
-    
-    url(r'^login/$', login, {'template_name': 'login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'login.html'}),
-    url(r'^password_change/$', password_change, name='password_change'),
-    url(r'^password_change/done/$', password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
     
     # App bagogold
     url(r'', include('bagogold.bagogold.urls')),
