8f8e2e76be05f55ac36db3b2bfd91c03b2383aad
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-02 23:50:48 -0300

    Adicionando atualiza??o automatica de saldo. Removendo historico de corretagem padr?o

--- bagogold/bagogold/models/investidores.py
+++ bagogold/bagogold/models/investidores.py
@@ -9,16 +9,8 @@ class Investidor (models.Model):
     F = valor fixo, P = valor percentual da operação
     """
     tipo_corretagem = models.CharField(u'Tipo de corretagem', max_length=1, blank=True, null=True)
+    auto_atualizar_saldo = models.BooleanField(u'Atualizar saldo automaticamente?', default=False)
     
     def __unicode__(self):
         return self.user.first_name + ' ' + self.user.last_name
-    
-class HistoricoCorretagemPadrao (models.Model):
-    investidor = models.ForeignKey('Investidor')
-    data_vigencia = models.DateField(u'Início da vigência', blank=True, null=True)
-    corretagem_padrao = models.DecimalField(u'Corretagem padrão', max_digits=5, decimal_places=2, blank=True, null=True)
-    """
-    F = valor fixo, P = valor percentual da operação
-    """
-    tipo_corretagem = models.CharField(u'Tipo de corretagem', max_length=1, blank=True, null=True)
     
