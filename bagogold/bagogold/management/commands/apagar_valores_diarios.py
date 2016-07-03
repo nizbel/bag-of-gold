56c2f302e8861b38cb45af296229c942ec6bfe68
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-02 22:42:28 -0300

    Valores diarios mais atuais n?o s?o apagados no dia a dia

--- bagogold/bagogold/management/commands/apagar_valores_diarios.py
+++ bagogold/bagogold/management/commands/apagar_valores_diarios.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.acoes import ValorDiarioAcao, Acao
+from bagogold.bagogold.models.acoes import ValorDiarioAcao, Acao, HistoricoAcao
 from bagogold.bagogold.models.fii import ValorDiarioFII, FII
 from bagogold.bagogold.models.td import ValorDiarioTitulo
 from django.core.management.base import BaseCommand
+import datetime
 
 class Command(BaseCommand):
     help = 'Apaga valores diários ao fim do dia'
 
     def handle(self, *args, **options):
+        # Buscar ultimo dia util para remover valores diarios anteriores
+        ultimo_dia_util = HistoricoAcao.objects.filter(data__lt=datetime.date.today()).latest('data').data
         for acao in Acao.objects.all():
-            ValorDiarioAcao.objects.filter(acao=acao).delete()
-        ValorDiarioTitulo.objects.all().delete()
+            ValorDiarioAcao.objects.filter(acao=acao, data_hora__date__lt=ultimo_dia_util).delete()
+        ValorDiarioTitulo.objects.filter(data_hora__date__lt=ultimo_dia_util).delete()
         for fii in FII.objects.all():
-            ValorDiarioFII.objects.filter(fii=fii).delete()
+            ValorDiarioFII.objects.filter(fii=fii, data_hora__date__lt=ultimo_dia_util).delete()
 
