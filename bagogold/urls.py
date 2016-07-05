6a6a8e18c4edf7d27ff27f4bb1c9a4b4ffca3e10
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-05 13:02:29 -0300

    Investidores sendo criados com Users

--- bagogold/urls.py
+++ bagogold/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'bagogold.views.home'', name='home'),
