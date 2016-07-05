cf77c6ccd448c0c94b259d1a9963fd8960f8ae73
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-05 13:02:29 -0300

    Investidores sendo criados com Users

---
+++ bagogold/bagogold/admin/investidores.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from bagogold.bagogold.models.investidores import Investidor
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+   
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class InvestidorInline(admin.StackedInline):
+    model = Investidor
+    can_delete = False
+    verbose_name_plural = 'investidores'
+  
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (InvestidorInline, )
+  
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
