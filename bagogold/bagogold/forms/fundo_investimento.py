1d448b3f75440eb7412b2b1dc06c6d6d87e15636
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-05 22:55:38 -0300

    Inserindo forms para fundo investimento COnsertando erros de lc em cdb rdb

---
+++ bagogold/bagogold/forms/fundo_investimento.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from bagogold.bagogold.models.fundo_investimento import FundoInvestimento, OperacaoFundoInvestimento, \
+    HistoricoPorcentagemFundoInvestimento, HistoricoCarenciaFundoInvestimento
+from django import forms
+from django.forms import widgets
+
+ESCOLHAS_TIPO_PRAZO=(('L', 'Longo prazo'), 
+                            ('C', 'Curto prazo'))
+
+ESCOLHAS_TIPO_OPERACAO=(('C', "Compra"),
+                        ('V', "Venda"))
+
+class FundoInvestimentoForm(forms.ModelForm):
+    class Meta:
+        model = FundoInvestimento
+        fields = ('nome', 'tipo_prazo', 'descricao')
+        widgets={'tipo_prazo': widgets.Select(choices=ESCOLHAS_TIPO_PRAZO),}
+
+class OperacaoFundoInvestimentoForm(forms.ModelForm):
+    
+    class Meta:
+        model = OperacaoFundoInvestimento
+        fields = ('tipo_operacao', 'quantidade_cotas', 'valor', 'data', 'fundo_investimento')
+        widgets={'data': widgets.DateInput(attrs={'class':'datepicker', 
+                                            'placeholder':'Selecione uma data'}),
+                 'tipo_operacao': widgets.Select(choices=ESCOLHAS_TIPO_OPERACAO),}
+        
+    class Media:
+        js = ('js/bagogold/fundo_investimento.js',)
+        
+class HistoricoCarenciaFundoInvestimentoForm(forms.ModelForm):
+    
+    class Meta:
+        model = HistoricoCarenciaFundoInvestimento
+        fields = ('carencia', 'data',
+                  'fundo_investimento')
+        widgets={'data': widgets.DateInput(attrs={'class':'datepicker', 
+                                            'placeholder':'Selecione uma data'}),}
