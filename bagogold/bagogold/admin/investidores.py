1218c0464612f64d9b464a634a5c3bc3eb201fb7
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-08 02:11:04 -0300

    Adicionando investidores ao admin

--- bagogold/bagogold/admin/investidores.py
+++ bagogold/bagogold/admin/investidores.py
@@ -4,16 +4,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
    
+admin.site.register(Investidor)
+
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class InvestidorInline(admin.StackedInline):
+class UserInvestidorInline(admin.StackedInline):
     model = Investidor
     can_delete = False
     verbose_name_plural = 'investidores'
   
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = (InvestidorInline, )
+    inlines = (UserInvestidorInline, )
   
 # Re-register UserAdmin
 admin.site.unregister(User)
