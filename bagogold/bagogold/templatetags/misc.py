35959d8e763830ebade1c4998211fa12c8e8a226
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-18 18:46:50 -0300

    Preparando prejuizos a compensar

---
+++ bagogold/bagogold/templatetags/misc.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django import template
+
+register = template.Library()
+
+@register.filter
+def dict_index(Dict, index):
+    return Dict[index]
--- bagogold/bagogold/views/imposto_renda/imposto_renda.py
+++ bagogold/bagogold/views/imposto_renda/imposto_renda.py
@@ -52,6 +52,10 @@ def detalhar_imposto_renda(request, ano):
     total_acima_vinte_mil = Decimal(0)
     lucro_venda = OrderedDict()
     lucro_venda_dt = OrderedDict()
+    prejuizo_a_compensar = OrderedDict()
+    prejuizo_a_compensar_dt = OrderedDict()
+    prejuizo_acumulado = Decimal(0)
+    prejuizo_acumulado_dt = Decimal(0)
     
     # Inicializar variáveis
     for mes_atual in range(1, 13):
@@ -60,6 +64,8 @@ def detalhar_imposto_renda(request, ano):
         ganho_acima_vinte_mil[mes_atual] = (Decimal(0), Decimal(0))
         lucro_venda[mes_atual] = Decimal(0)
         lucro_venda_dt[mes_atual] = Decimal(0)
+        prejuizo_a_compensar[mes_atual] = Decimal(0)
+        prejuizo_a_compensar_dt[mes_atual] = Decimal(0)
     
     acoes = {}
     for evento in lista_eventos:
@@ -143,6 +149,10 @@ def detalhar_imposto_renda(request, ano):
 #                         total_gasto += (((acoes[evento.acao.ticker] * evento.valor_unitario ) / 100 ) % 1) * provento_acao.valor_calculo_frac
 #                         total_proventos += (((acoes[evento.acao.ticker] * evento.valor_unitario ) / 100 ) % 1) * provento_acao.valor_calculo_frac
 
+
+    # Busca prejuízos acumulados
+    prejuizo_acumulado = Decimal(0)
+    prejuizo_acumulado_dt = Decimal(0)
     # Pegar ganhos líquidos por ações até 20.000 reais no mês
     for mes in range(1, 13):
 #         print mes
@@ -150,10 +160,23 @@ def detalhar_imposto_renda(request, ano):
 #             print 'mes', mes, lucro_venda
             ganho_abaixo_vinte_mil[mes] = lucro_venda[mes]
             total_abaixo_vinte_mil += lucro_venda[mes]
-        elif total_mes[mes] > 20000 or lucro_venda[mes] < 0 or lucro_venda_dt[mes] != 0:
+        if total_mes[mes] > 20000 or lucro_venda[mes] < 0 or lucro_venda_dt[mes] != 0:
 #             print 'mes', mes, lucro_venda, lucro_venda_dt
+            if total_mes[mes] <= 20000 and lucro_venda[mes] > 0:
+                lucro_venda[mes] = 0
             ganho_acima_vinte_mil[mes] = (lucro_venda[mes], lucro_venda_dt[mes])
             # TODO computar ganhos liquidos em RV
+            if lucro_venda[mes] < 0:
+                prejuizo_acumulado -= lucro_venda[mes]
+            elif lucro_venda[mes] > 0 and prejuizo_acumulado > 0:
+                prejuizo_acumulado -= min(lucro_venda[mes], prejuizo_acumulado)
+            if lucro_venda_dt[mes] < 0:
+                prejuizo_acumulado_dt -= lucro_venda_dt[mes]
+            elif lucro_venda_dt[mes] > 0 and prejuizo_acumulado_dt > 0:
+                prejuizo_acumulado_dt -= min(lucro_venda_dt[mes], prejuizo_acumulado_dt)
+        prejuizo_a_compensar[mes] = prejuizo_acumulado
+        prejuizo_a_compensar_dt[mes] = prejuizo_acumulado_dt
+                
     
     total_dividendos = Decimal(0)
     total_jscp = Decimal(0)
@@ -300,8 +323,10 @@ def detalhar_imposto_renda(request, ano):
     # Editar ano para string
     ano = str(ano).replace('.', '')
     
-    return render_to_response('imposto_renda/detalhar_imposto_ano.html', {'ano': ano, 'acoes': acoes, 'cdb_rdb': cdb_rdb, 'fundos_investimento': fundos_investimento, 'fiis': fiis, 'ganho_abaixo_vinte_mil': ganho_abaixo_vinte_mil,
-                                                                          'ganho_acima_vinte_mil': ganho_acima_vinte_mil, 'letras_credito': letras_credito,'dados': dados}, context_instance=RequestContext(request))
+    return render_to_response('imposto_renda/detalhar_imposto_ano.html', {'ano': ano, 'acoes': acoes, 'ganho_abaixo_vinte_mil': ganho_abaixo_vinte_mil, 'ganho_acima_vinte_mil': ganho_acima_vinte_mil, 
+                                                                          'prejuizo_a_compensar': prejuizo_a_compensar, 'prejuizo_a_compensar_dt': prejuizo_a_compensar_dt, 'cdb_rdb': cdb_rdb, 
+                                                                          'fundos_investimento': fundos_investimento, 'fiis': fiis, 'letras_credito': letras_credito,'dados': dados}, 
+                              context_instance=RequestContext(request))
 
 def listar_anos(request):
     class Object(object):
