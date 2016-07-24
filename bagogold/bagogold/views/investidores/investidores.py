4630bb44a778028659da3032d45414f06fef4002
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-24 04:41:07 -0300

    Cadastro funcionando

--- bagogold/bagogold/views/investidores/investidores.py
+++ bagogold/bagogold/views/investidores/investidores.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import views, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import login
+from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -17,9 +18,11 @@ def cadastrar(request):
             novo_usuario = authenticate(username=form_usuario.cleaned_data['username'], \
                                     password=form_usuario.cleaned_data['password1'],)
             login(request, novo_usuario)
-            return HttpResponseRedirect("/home/")
+            return HttpResponseRedirect(reverse('home'))
     else:
         form_usuario = UserCreationForm()
+        for field in form_usuario:
+            print dir(field)
             
     return render_to_response('investidores/cadastrar.html', {'form_usuario': form_usuario}, context_instance=RequestContext(request))
 
