75c86f8c6066d49efe377e68d090f483fa2efd00
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-24 02:02:49 -0300

    Settings arrumado, posicionamento do download arrumado

--- bagogold/bagogold/models/gerador_proventos.py
+++ bagogold/bagogold/models/gerador_proventos.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.models.acoes import Acao
 from django.db import models
+import os
+import re
+
+def ticker_path(instance, filename):
+    ticker = Acao.objects.filter(empresa=instance.empresa)[0].ticker
+    return 'doc proventos/{0}/{1}'.format(re.sub('\d', '', ticker), filename)
 
 class DocumentoBovespa (models.Model):
     url = models.CharField(u'URL do documento', blank=True, null=True, max_length=200)
@@ -9,7 +16,19 @@ class DocumentoBovespa (models.Model):
     Define se é provento de ação ou FII
     """
     tipo = models.CharField(u'Tipo de provento', max_length=1)
-    documento = models.FileField(upload_to='doc proventos/', blank=True, null=True)
+    documento = models.FileField(upload_to=ticker_path, blank=True, null=True)
+    data_referencia = models.DateField(u'Data de referência')
+    
+    class Meta:
+        unique_together = ('empresa', 'protocolo')
+        
+    def save(self, *args, **kw):
+        try:
+            documento_existente = DocumentoBovespa.objects.get(empresa=self.empresa, protocolo=self.protocolo)
+            if os.path.isfile(self.documento.path):
+                os.remove(self.documento.path)
+        except DocumentoBovespa.DoesNotExist:
+            super(DocumentoBovespa, self).save(*args, **kw)
     
 #     def pendente(self):
 #        if self.tipo == 'A':
