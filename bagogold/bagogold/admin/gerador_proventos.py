f1b3d1c01c481647adec586698be597f95f521c7
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-24 22:13:07 -0300

    Melhorando admin de documentos

--- bagogold/bagogold/admin/gerador_proventos.py
+++ bagogold/bagogold/admin/gerador_proventos.py
@@ -4,7 +4,16 @@ from bagogold.bagogold.models.gerador_proventos import DocumentoBovespa, \
 from django.contrib import admin
 
 
-admin.site.register(DocumentoBovespa)
+
+class DocumentoBovespaAdmin(admin.ModelAdmin):
+    search_fields = ['empresa__nome', 'protocolo']
+    list_display = ('nome_documento', 'data_referencia', 'protocolo', 'empresa')
+    
+    def nome_documento(self, obj):
+        return obj.documento.name.split('/')[-1]
+    nome_documento.short_description = 'Nome'
+    
+admin.site.register(DocumentoBovespa, DocumentoBovespaAdmin)
     
 admin.site.register(ProventoAcaoDocumento)
     
