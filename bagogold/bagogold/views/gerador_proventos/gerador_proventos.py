232a50af6cfdb79ac18095c36be4ee9bc7774389
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-26 16:44:07 -0300

    Listagem de documentos por empresa OK

--- bagogold/bagogold/views/gerador_proventos/gerador_proventos.py
+++ bagogold/bagogold/views/gerador_proventos/gerador_proventos.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.gerador_proventos import DocumentoProventoBovespa, PendenciaDocumentoProvento
+from bagogold.bagogold.models.acoes import Acao
+from bagogold.bagogold.models.empresa import Empresa
+from bagogold.bagogold.models.gerador_proventos import DocumentoProventoBovespa, \
+    PendenciaDocumentoProvento
 from bagogold.bagogold.utils.investidores import is_superuser
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render_to_response
@@ -21,8 +24,20 @@ def resolver_pendencia(request):
 @login_required
 @user_passes_test(is_superuser)
 def listar_documentos(request):
-    empresas = Empresa.objects.all()
-    documentos = DocumentoProventoBovespa.objects.filter(empresa__id=1).order_by('data_referencia')
+    empresa_id = 1
+    if request.method == 'POST':
+        if request.POST.get("busca_empresa"):
+            empresa_id = Acao.objects.filter(ticker__istartswith=request.POST['busca_empresa'])[0].empresa.id
+    
+    # Mostrar empresa atual
+    empresa_atual = Empresa.objects.get(id=empresa_id)
+    empresa_atual.ticker = empresa_atual.ticker_empresa()
+    
+    empresas = [empresa.ticker_empresa() for empresa in Empresa.objects.all().order_by('id')]
+#     empresas = map(str, empresas)
+    empresas = '["' + '","'.join(empresas) + '"]'
+    print empresas
+    documentos = DocumentoProventoBovespa.objects.filter(empresa__id=empresa_id).order_by('data_referencia')
     
     for documento in documentos:
         documento.nome = documento.documento.name.split('/')[-1]
@@ -32,7 +47,7 @@ def listar_documentos(request):
         if documento.tipo == 'A':
             documento.ha_proventos_vinculados = False
             
-    return render_to_response('gerador_proventos/listar_documentos.html', {'documentos': documentos, 'empresas': empresas},
+    return render_to_response('gerador_proventos/listar_documentos.html', {'documentos': documentos, 'empresas': empresas, 'empresa_atual': empresa_atual},
                               context_instance=RequestContext(request))
 
 @login_required
