015bd9ad361060a61d2dad716337810258a452e9
Guilherme Henrique <kingbowserii@gmail.com>
2016-07-29 18:09:00 -0300

    Preparando tela de resolver pendencias

--- bagogold/bagogold/views/gerador_proventos/gerador_proventos.py
+++ bagogold/bagogold/views/gerador_proventos/gerador_proventos.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.forms.provento_acao import ProventoAcaoForm
 from bagogold.bagogold.models.acoes import Acao
 from bagogold.bagogold.models.empresa import Empresa
 from bagogold.bagogold.models.gerador_proventos import DocumentoProventoBovespa, \
     PendenciaDocumentoProvento
+from bagogold.bagogold.testFII import ler_documento_proventos
 from bagogold.bagogold.utils.investidores import is_superuser
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.forms.formsets import formset_factory
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-
-
-@login_required
-@user_passes_test(is_superuser)
-def listar_proventos(request):
-    pass
-
-
-@login_required
-@user_passes_test(is_superuser)
-def resolver_pendencia(request):
-    pass
 
 
 @login_required
@@ -58,4 +49,25 @@ def listar_pendencias(request):
         pendencia.nome = pendencia.documento.documento.name.split('/')[-1]
         
     return render_to_response('gerador_proventos/listar_pendencias.html', {'pendencias': pendencias},
+                              context_instance=RequestContext(request))
+
+
+@login_required
+@user_passes_test(is_superuser)
+def listar_proventos(request):
+    pass
+
+
+@login_required
+@user_passes_test(is_superuser)
+def resolver_pendencia(request, id):
+    pendencia = PendenciaDocumentoProvento.objects.get(id=id)
+    
+    texto_documento = ler_documento_proventos(pendencia.documento.documento)
+    
+    # Preparar formset de proventos
+    if pendencia.documento.tipo == 'A':
+        formset = formset_factory(ProventoAcaoForm, extra=1)
+    
+    return render_to_response('gerador_proventos/resolver_pendencia.html', {'pendencia': pendencia, 'texto_documento': texto_documento, 'formset': formset},
                               context_instance=RequestContext(request))
