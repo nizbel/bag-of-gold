e59958e896a8f5ae1ea6f7548fc9bf459489bb3d
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-30 16:31:23 -0300

    Inserindo django-registration

--- bagogold/bagogold/views/investidores/investidores.py
+++ bagogold/bagogold/views/investidores/investidores.py
@@ -5,22 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
+from registration.backends.hmac import urls
 
-
-def cadastrar(request):
-    if request.method == 'POST':
-        form_usuario = UserCreationForm(request.POST)
-        if form_usuario.is_valid():
-            form_usuario.save()
-            messages.success(request, 'Conta criada com sucesso')
-            novo_usuario = authenticate(username=form_usuario.cleaned_data['username'], \
-                                    password=form_usuario.cleaned_data['password1'],)
-            login(request, novo_usuario)
-            return redirect('home')
-    else:
-        form_usuario = UserCreationForm()
-            
-    return render_to_response('investidores/cadastrar.html', {'form_usuario': form_usuario}, context_instance=RequestContext(request))
 
 def logout(request, *args, **kwargs):
     messages.success(request, 'Logout feito com sucesso')
