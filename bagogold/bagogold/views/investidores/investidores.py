216583ce9f9bcd84b64355b49d1610b8279f8111
Guilherme de Deus <kingbowserii@gmail.com>
2016-07-31 02:34:56 -0300

    Preparando ativa??o

--- bagogold/bagogold/views/investidores/investidores.py
+++ bagogold/bagogold/views/investidores/investidores.py
@@ -6,8 +6,18 @@ from django.contrib.auth import login
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 from registration.backends.hmac import urls
+from django.dispatch import receiver
+from registration.signals import user_activated
 
 
 def logout(request, *args, **kwargs):
     messages.success(request, 'Logout feito com sucesso')
     return views.logout(request, *args, **kwargs)
+
+
+# Sinal para logar após ativação
+@receiver(user_activated)
+def login_on_activation(sender, user, request, **kwargs):
+    """Loga o usuário após ativação"""
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
+    login(request, user)
