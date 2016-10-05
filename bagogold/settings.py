2145164cbbe39b87f12ae60e0a142e3991665d6d
Guilherme de Deus <kingbowserii@gmail.com>
2016-10-05 00:04:46 -0300

    Arrumando importa??o do settings_local

--- bagogold/settings.py
+++ bagogold/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -135,4 +135,4 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
-import conf.settings_local
+from conf.settings_local import *
