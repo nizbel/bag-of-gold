d24c5e950f3314fc8072add2e2b58ace7149fb00
Guilherme de Deus <kingbowserii@gmail.com>
2016-10-19 00:03:42 -0200

    Corrigido erro na cria??o de opera??es de compra inv?lidas

--- bagogold/bagogold/forms/lc.py
+++ bagogold/bagogold/forms/lc.py
@@ -39,8 +39,8 @@ class OperacaoLetraCreditoForm(forms.ModelForm):
         self.fields['letra_credito'].queryset = LetraCredito.objects.filter(investidor=self.investidor)
         self.fields['operacao_compra'].queryset = OperacaoLetraCredito.objects.filter(investidor=self.investidor, tipo_operacao='C')
         # Remover operações que já tenham sido totalmente vendidas e a própria operação
-        operacoes_compra_invalidas = [operacao_compra_invalida.id for operacao_compra_invalida in self.fields['operacao_compra'].queryset if operacao_compra_invalida.qtd_disponivel_venda() == 0] + \
-            [self.instance.id] if self.instance.id != None else []
+        operacoes_compra_invalidas = [operacao_compra_invalida.id for operacao_compra_invalida in self.fields['operacao_compra'].queryset if operacao_compra_invalida.qtd_disponivel_venda() <= 0] + \
+            ([self.instance.id] if self.instance.id != None else [])
         # Manter operação de compra atual, caso seja edição de venda
         if self.instance.operacao_compra_relacionada():
             operacoes_compra_invalidas.remove(self.instance.operacao_compra_relacionada().id)
