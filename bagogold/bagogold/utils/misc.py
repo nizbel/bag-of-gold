64715e851d6780211dc7cb7fb0ad798bad9440cd
Guilherme Henrique <kingbowserii@gmail.com>
2016-10-26 19:06:11 -0200

    Teste para busca de ?ltimas opera??es OK

--- bagogold/bagogold/utils/misc.py
+++ bagogold/bagogold/utils/misc.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.td import HistoricoIPCA
+from bagogold.bagogold.models.acoes import OperacaoAcao
+from bagogold.bagogold.models.fii import OperacaoFII
+from bagogold.bagogold.models.fundo_investimento import \
+    OperacaoFundoInvestimento
+from bagogold.bagogold.models.lc import OperacaoLetraCredito
+from bagogold.bagogold.models.td import HistoricoIPCA, OperacaoTitulo
 from decimal import Decimal
+from itertools import chain
+from operator import attrgetter
 from urllib2 import Request, urlopen, URLError, HTTPError
 import datetime
 import math
 import re
+
 
 def calcular_iof_regressivo(dias):
     return Decimal(max((100 - (dias * 3 + math.ceil((float(dias)/3)))), 0)/100)
@@ -84,3 +92,26 @@ def qtd_dias_uteis_no_periodo(data_inicial, data_final):
         raise ValueError('Data final deve ser igual ou maior que data inicial')
     daygenerator = (data_inicial + datetime.timedelta(days=x) for x in xrange((data_final - data_inicial).days))
     return sum(1 for day in daygenerator if day.weekday() < 5 and not verificar_feriado_bovespa(day))
+
+def buscar_ultimas_operacoes(investidor, quantidade_operacoes):
+    from bagogold.bagogold.models.cdb_rdb import OperacaoCDB_RDB
+    """
+    Busca as últimas operações feitas pelo investidor, ordenadas por data decrescente
+    Parâmetros: Investidor
+                Quantidade de operações a ser retornada
+    Retorno: Lista com as operações ordenadas por data
+    """
+    # Juntar todas as operações em uma lista
+    operacoes_fii = OperacaoFII.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')
+    operacoes_td = OperacaoTitulo.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')
+    operacoes_acoes = OperacaoAcao.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')
+    operacoes_lc = OperacaoLetraCredito.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')  
+    operacoes_cdb_rdb = OperacaoCDB_RDB.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')  
+    operacoes_fundo_investimento = OperacaoFundoInvestimento.objects.filter(investidor=investidor).exclude(data__isnull=True).order_by('data')
+    
+    lista_operacoes = sorted(chain(operacoes_fii, operacoes_td, operacoes_acoes, operacoes_lc, operacoes_cdb_rdb, operacoes_fundo_investimento),
+                            key=attrgetter('data'), reverse=True)
+    
+    ultimas_operacoes = lista_operacoes[:min(quantidade_operacoes, len(lista_operacoes))]
+    
+    return ultimas_operacoes
