2bdf6682f1b68aedd74611a13dbf61d6462da6cc
Guilherme de Deus <kingbowserii@gmail.com>
2016-10-29 15:50:53 -0200

    Adicionando middleware

--- bagogold/settings.py
+++ bagogold/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'bagogold.bagogold.middleware.teste_nova_aparencia.TesteNovaAparenciaMiddleWare',
 )
 
 ROOT_URLCONF = 'bagogold.urls'
