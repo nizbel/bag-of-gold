a46cca06ac60be8970813ae017da8667fa37dea3
Guilherme Henrique <kingbowserii@gmail.com>
2016-10-30 16:53:28 -0200

    Adicionando view que faz botao de atualizar dados funcionar

--- bagogold/bagogold/views/misc.py
+++ bagogold/bagogold/views/misc.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http.response import HttpResponseRedirect
+from fabric.context_managers import settings
+from bagogold.fabfile import metronic_test_update
 
 @login_required
-def teste_nova_aparencia(request, url):
+def ver_nova_aparencia(request, url):
+    # Apenas eu e Camila podemos acessar
+    if request.id not in [1,31]:
+        raise PermissionDenied
+    
     if 'testando_aparencia' in request.session:
         request.session['testando_aparencia'] = not request.session['testando_aparencia']
     else:
         request.session['testando_aparencia'] = True
     return HttpResponseRedirect('/' + url + '/')
     
+@login_required
+def carregar_nova_aparencia(request, url):
+    # Apenas eu e Camila podemos acessar
+    if request.id not in [1,31]:
+        raise PermissionDenied
+
+    with settings(host_string='bagofgold@bagofgold.com.br'):
+        metronic_test_update()
