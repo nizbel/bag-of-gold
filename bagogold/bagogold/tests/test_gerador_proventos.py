c0ce89fd5a51e38cf3a730fa199e63f24c214a20
Guilherme de Deus <kingbowserii@gmail.com>
2016-11-02 18:36:24 -0200

    Melhorando descri??es dos testes

--- bagogold/bagogold/tests/test_gerador_proventos.py
+++ bagogold/bagogold/tests/test_gerador_proventos.py
@@ -12,6 +12,7 @@ class GeradorProventosTestCase(TestCase):
         user = User.objects.create(username='tester')
 
     def test_baixar_arquivo(self):
+        """Testa se o arquivo baixado realmente é um arquivo"""
         arquivo = File(baixar_demonstrativo_rendimentos('http://www2.bmfbovespa.com.br/empresas/consbov/ArquivosExibe.asp?site=B&protocolo=507317'))
         self.assertTrue(hasattr(arquivo, 'size'))
         self.assertTrue(hasattr(arquivo, 'file'))
@@ -19,21 +20,27 @@ class GeradorProventosTestCase(TestCase):
         self.assertTrue(hasattr(arquivo, 'open'))
         
     def test_nao_baixar_se_url_invalida(self):
+        """Testa se é jogado URLError quando enviada uma URL da bovespa inválida"""
         with self.assertRaises(URLError):
             baixar_demonstrativo_rendimentos('http://www2.bmfbovespa.com.br/empresas/consbov/ArquivosExibe.asp?site=B&protcolo=507317')
     
     def test_excluir_arquivo_sem_info(self):
+        """Testa exclusão de arquivo por um validador do site"""
         pass
     
     def test_gerar_provento(self):
+        """Testa criação do provento por um validador"""
         pass
     
     def test_gerar_versao_doc_provento(self):
+        """Testa criação automática de versão a partir de um documento de provento"""
         pass
     
     def test_nao_permitir_gerar_prov_repetido(self):
+        """Testa erro caso um provento seja gerado novamente"""
         pass
     
     def test_puxar_validacao_para_investidor(self):
+        """Testa investidor puxando para si uma pendencia"""
         pass
     
