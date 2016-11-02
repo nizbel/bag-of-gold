9dc00bae38db125b907292ffab6ca68724698f7f
Guilherme Henrique <kingbowserii@gmail.com>
2016-11-02 02:47:17 -0200

    Preparando testes para o gerador de proventos

---
+++ bagogold/bagogold/tests/test_gerador_proventos.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+class GeradorProventosTestCase(TestCase):
+
+    def test_baixar_arquivos(self):
+        pass
+    
+    def test_excluir_arquivo_sem_info(self):
+        pass
+    
+    def test_gerar_provento(self):
+        pass
+    
+    def test_gerar_versao_doc_provento(self):
+        pass
+    
+    def test_nao_permitir_gerar_prov_repetido(self):
+        pass
+    
+    def test_puxar_validacao_para_investidor(self):
+        pass
+    
