32ceeb3d235b6fd5f083d65ccd9014497d36b729
Guilherme Henrique <kingbowserii@gmail.com>
2016-11-20 14:57:52 -0200

    Testando se templates existem para evitar erros ao ver admin

--- bagogold/bagogold/middleware/teste_nova_aparencia.py
+++ bagogold/bagogold/middleware/teste_nova_aparencia.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+from django.template.exceptions import TemplateDoesNotExist
 
 
 class TesteNovaAparenciaMiddleWare(object): 
      
     def process_template_response(self, request, response):
-        response.template_name = ('teste/' + response.template_name) if request.session.get('testando_aparencia', False) else response.template_name
-        response.render()
-        
+        try:
+            template_name_original = response.template_name
+            response.template_name = ('teste/' + response.template_name) if request.session.get('testando_aparencia', False) else response.template_name
+            response.render()
+        except TemplateDoesNotExist:
+            response.template_name = template_name_original
+            response.render()
+            
         return response
         
