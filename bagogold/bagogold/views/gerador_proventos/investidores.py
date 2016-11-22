ed0d3525c24e34360639d071e454f29377e15dca
Guilherme Henrique <kingbowserii@gmail.com>
2016-11-22 02:25:46 -0200

    Inserindo gr?fico de valida??es e leituras por usuario

--- bagogold/bagogold/views/gerador_proventos/investidores.py
+++ bagogold/bagogold/views/gerador_proventos/investidores.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import Permission, User
 from django.db.models.query_utils import Q
 from django.template.response import TemplateResponse
+import calendar
+import datetime
 
 
 @login_required
@@ -20,7 +22,22 @@ def detalhar_pendencias_usuario(request, id_usuario):
     for pendencia in usuario.pendencias_alocadas:
         pendencia.tipo_pendencia = 'Leitura' if pendencia.tipo == 'L' else 'Validação'
     
-    return TemplateResponse(request, 'gerador_proventos/detalhar_pendencias_usuario.html', {'usuario': usuario})
+    # Preparar gráficos de acompanhamento
+    graf_leituras = list()
+    graf_validacoes = list()
+    
+    # Iterar mes a mes sobre a data de 2 anos atrás
+    data_2_anos_atras = datetime.date.today().replace(year=datetime.date.today().year-2)
+    while data_2_anos_atras <= datetime.date.today():
+        # Preparar data
+        graf_leituras += [[str(calendar.timegm(data_2_anos_atras.replace(day=12).timetuple()) * 1000), usuario.leituras.filter(data_leitura__month=data_2_anos_atras.month, data_leitura__year=data_2_anos_atras.year).count()]]
+        graf_validacoes += [[str(calendar.timegm(data_2_anos_atras.replace(day=18).timetuple()) * 1000), 2]]
+        if data_2_anos_atras.month < 12:
+            data_2_anos_atras = data_2_anos_atras.replace(month=data_2_anos_atras.month+1)
+        else:
+            data_2_anos_atras = data_2_anos_atras.replace(year=data_2_anos_atras.year+1, month=1)
+    print graf_leituras
+    return TemplateResponse(request, 'gerador_proventos/detalhar_pendencias_usuario.html', {'usuario': usuario, 'graf_leituras': graf_leituras, 'graf_validacoes': graf_validacoes})
 
 @login_required
 @permission_required('bagogold.pode_gerar_proventos', raise_exception=True)
