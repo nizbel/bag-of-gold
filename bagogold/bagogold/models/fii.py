a7348c4e1ca3898b64ee26561f5f0b4bf97a5cec
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-04 23:08:43 -0200

    Adicionando mais restri??es aos proventos de FII e a??o

--- bagogold/bagogold/models/fii.py
+++ bagogold/bagogold/models/fii.py
@@ -33,7 +33,7 @@ class ProventoFII (models.Model):
     oficial_bovespa = models.BooleanField(u'Oficial Bovespa?', default=False)
     
     class Meta:
-        unique_together=(('data_ex', 'data_pagamento', 'fii',))
+        unique_together=(('data_ex', 'data_pagamento', 'fii', 'valor_unitario'))
         
     def __unicode__(self):
         return 'R$ %s de %s em %s com data EX %s' % (str(self.valor_unitario), self.fii.ticker, str(self.data_pagamento), str(self.data_ex))
