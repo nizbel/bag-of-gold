dfd05f614ffef2ee219be8071c145beb42097854
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-12 12:50:55 -0200

    Adicionando modelos de debentures

---
+++ bagogold/bagogold/models/debentures.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+class Debenture (models.Model):
+    nome = models.CharField(u'Nome', max_length=50)
+    investidor = models.ForeignKey('Investidor')
+    """
+    1 = Prefixado, 2 = IPCA, 3 = DI
+    """
+    tipo_indexacao = models.PositiveSmallIntegerField(u'Tipo de indexação')
+    porcentagem = models.DecimalField(u'Porcentagem sobre indexação', decimal_places=3, max_digits=6)
+    juros_adicional = models.DecimalField(u'Juros adicional', decimal_places=3, max_digits=6)
+    data_emissao = models.DateField(u'Data de emissão')
+    valor_emissao = models.DecimalField(u'Valor nominal na emissão', max_digits=15, decimal_places=8)
+    data_vencimento = models.DateField(u'Data de vencimento')
+    
+    class Meta:
+        unique_together=('nome', 'investidor')
+    
+    def __unicode__(self):
+        return self.nome
+    
+class OperacaoDebenture (models.Model):
+    debenture = models.ForeignKey('Debenture')
+    preco_unitario = models.DecimalField(u'Preço unitário', max_digits=15, decimal_places=8)  
+    quantidade = models.IntegerField(u'Quantidade') 
+    data = models.DateField(u'Data', blank=True, null=True)
