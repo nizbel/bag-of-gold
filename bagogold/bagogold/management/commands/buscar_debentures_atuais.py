be7690077a8bb75b20a20169fb11c015c7f26ea2
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-13 11:52:30 -0200

    Preparando leitura de debentures e removendo imports nao mais utilizados

---
+++ bagogold/bagogold/management/commands/buscar_debentures_atuais.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from decimal import Decimal
+from django.core.management.base import BaseCommand
+from urllib2 import Request, urlopen, HTTPError, URLError
+import re
+
+
+
+class Command(BaseCommand):
+    help = 'Busca as Debêntures atualmente válidos'
+
+    def handle(self, *args, **options):
+        url_debentures = 'http://www.debentures.com.br/exploreosnd/consultaadados/emissoesdedebentures/caracteristicas_r.asp?tip_deb=publicas&op_exc=Nada'
+        
+#         codigos = buscar_lista_debentures(url_debentures)
+        codigos = ['AARJ11']
+        for codigo in codigos[:1]:
+            buscar_info_debenture(codigo)
+
+def buscar_lista_debentures(url_debentures):
+    req = Request(url_debentures)
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+    except URLError as e:
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    else:
+        data = response.read()
+        
+        # Buscar pela URL padrão
+        registros = re.findall('caracteristicas_d.asp\?tip_deb=publicas&selecao=([^"]+?)?"', data)
+        registros = [registro.replace(' ', '') for registro in registros]
+        
+        return registros
+    
+def buscar_info_debenture(codigo):
+    url_download_arquivo = 'http://www.debentures.com.br/exploreosnd/consultaadados/emissoesdedebentures/caracteristicas_e.asp?Ativo=%s' % (codigo)
+    req = Request(url_download_arquivo)
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+    except URLError as e:
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    else:
+        data = response.read()
+        print response.headers['content-type']
+        for linha in data.split('\n'):
+            if codigo in linha:
+                print linha
