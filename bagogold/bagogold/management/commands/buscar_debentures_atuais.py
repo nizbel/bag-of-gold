765052f58a82a6568f9b20e4c081fb07ceb2fb22
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-16 22:45:31 -0200

    Preparandbo busca de historico de valores de debenture

--- bagogold/bagogold/management/commands/buscar_debentures_atuais.py
+++ bagogold/bagogold/management/commands/buscar_debentures_atuais.py
@@ -69,30 +69,3 @@ def buscar_info_debenture(codigo):
                 percentual_indice = campos[47]
                 incentivada = campos[88]
 #                 print ','.join(campos[41:71])
-
-def buscar_historico_debenture(codigo):
-    url_historico = 'http://www.debentures.com.br/exploreosnd/consultaadados/emissoesdedebentures/puhistorico_e.asp?op_exc=Nada&ativo=%s&dt_ini=&dt_fim=' % (codigo)
-    req = Request(url_historico)
-    try:
-        response = urlopen(req)
-    except HTTPError as e:
-        print 'The server couldn\'t fulfill the request.'
-        print 'Error code: ', e.code
-    except URLError as e:
-        print 'We failed to reach a server.'
-        print 'Reason: ', e.reason
-    else:
-        data = response.read()
-
-        for linha in data.decode('latin-1').split('\n'):
-#             if u'Código do Ativo' in linha:
-#                 for indice, campo in enumerate([campo.strip() for campo in linha.split('\t')]):
-#                     print indice, campo
-            if codigo in linha:
-                campos = [campo.strip() for campo in linha.split('\t')]
-                if campos[4] != '-':
-                    print campos[4], codigo, 'em', campos[0]
-    
-    # TODO ler como texto separado por \t
-    
-    
