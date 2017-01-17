5e02e3d41969e399b038b5da815b3be228e33cd6
Guilherme de Deus <kingbowserii@gmail.com>
2017-01-17 03:37:53 -0200

    Adicionando debentures mais simples

--- bagogold/bagogold/management/commands/buscar_debentures.py
+++ bagogold/bagogold/management/commands/buscar_debentures.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from bagogold.bagogold.models.debentures import Debenture
+from bagogold.bagogold.models.debentures import Debenture, JurosDebenture, \
+    PremioDebenture, AmortizacaoDebenture
 from decimal import Decimal
 from django.core.management.base import BaseCommand
 from threading import Thread
@@ -23,41 +24,40 @@ class ProcessaDebentureThread(Thread):
                     codigo = debenture.keys()[0]
                     campos = debenture[codigo]
                     
-                    print codigo, campos[41]
-                    if campos[41] not in tipos_remuneracao:
-                        tipos_remuneracao[campos[41]] = 1
-                    else:
-                        tipos_remuneracao[campos[41]] += 1
-                    
                     padrao_snd = campos[43]
                     # Por enquanto, apenas tratar os que forem padronizados
                     if padrao_snd == u'Padrão - SND': 
+                        print codigo
                         try:
                             data_emissao = datetime.datetime.strptime(campos[11], '%d/%m/%Y').date()
                             data_vencimento = None if campos[12] == u'Indeterminado' else datetime.datetime.strptime(campos[12] , '%d/%m/%Y').date()
                             data_inicio_rendimento = datetime.datetime.strptime(campos[15], '%d/%m/%Y').date()
                             valor_nominal_emissao = campos[37]
                             indice = campos[41]
-                            if indice in ['SELIC', 'PRÉ', 'IGP-M', 'IPCA', 'DI']:
+                            if indice in [u'SELIC', u'PRÉ', u'IGP-M', u'IPCA', u'DI']:
                                 # Buscar juros, amortização e prêmio
-                                amortizacao_taxa = campos[52]
-                                amortizacao_periodo = campos[53]
+                                amortizacao_taxa = Decimal(re.sub('[^\d,\.]', '', campos[52]).replace(',', '.') or '0')
+                                amortizacao_periodo = re.sub('[^\d]', '', campos[53]) or '0'
                                 amortizacao_unidade = campos[54]
-                                amortizacao_carencia = campos[55]
-                                amortizacao_tipo = campos[57]
+                                amortizacao_carencia = None if re.sub('[^\d/]', '', campos[55]) == '' else datetime.datetime.strptime(re.sub('[^\d/]', '', campos[55]), '%d/%m/%Y').date()
+                                amortizacao_tipo = 0
+                                for indice, tipo in [tipo for tipo in AmortizacaoDebenture.TIPOS_AMORTIZACAO]:
+                                    if campos[57] == tipo:
+                                        amortizacao_tipo = indice
+                                        break
                                 
-                                juros_taxa = campos[58]
-                                juros_periodo = campos[60]
+                                juros_taxa = Decimal(re.sub('[^\d,\.]', '', campos[58]).replace(',', '.') or '0')
+                                juros_periodo = re.sub('[^\d]', '', campos[60]) or '0'
                                 juros_unidade = campos[61]
-                                juros_carencia = campos[62]
+                                juros_carencia = None if re.sub('[^\d/]', '', campos[62]) == '' else datetime.datetime.strptime(re.sub('[^\d/]', '', campos[62]), '%d/%m/%Y').date()
                                 
-                                premio_taxa = campos[65]
-                                premio_periodo = campos[67]
+                                premio_taxa = Decimal(re.sub('[^\d,\.]', '', campos[65]).replace(',', '.') or '0')
+                                premio_periodo = re.sub('[^\d]', '', campos[67]) or '0'
                                 premio_unidade = campos[68]
-                                premio_carencia = campos[69]
+                                premio_carencia = None if re.sub('[^\d/]', '', campos[69]) == '' else datetime.datetime.strptime(re.sub('[^\d/]', '', campos[69]), '%d/%m/%Y').date()
                             else:
-                                raise ValueError(u'Tipo não previsto')
-                            percentual_indice = campos[47]
+                                raise ValueError('Tipo não previsto: %s' % (campos[41]))
+                            percentual_indice = Decimal(re.sub('[^\d,\.]', '', campos[47]).replace(',', '.') or '100')
                             situacao = campos[5]
                             data_saida = campos[14]
                             incentivada = campos[88]
@@ -78,11 +78,26 @@ class ProcessaDebentureThread(Thread):
             
                                 if situacao == u'Excluído' and data_saida != '':
                                     debenture.data_fim = datetime.datetime.strptime(data_saida, '%d/%m/%Y').date()
+                                
+                                debenture.save()
+                                
+                                if amortizacao_carencia:
+                                    amortizacao = AmortizacaoDebenture(data = datetime.date.today(), debenture=debenture, taxa=amortizacao_taxa, \
+                                                                       periodo=amortizacao_periodo, unidade_periodo=amortizacao_unidade, tipo=amortizacao_tipo, \
+                                                                       carencia=amortizacao_carencia)
+                                    amortizacao.save()
+                                if juros_carencia:
+                                    juros = JurosDebenture(data = datetime.date.today(), debenture=debenture, taxa=juros_taxa, \
+                                                                       periodo=juros_periodo, unidade_periodo=juros_unidade, carencia=juros_carencia)
+                                    juros.save()
+                                if premio_carencia:
+                                    premio = PremioDebenture(data = datetime.date.today(), debenture=debenture, taxa=premio_taxa, \
+                                                                       periodo=premio_periodo, unidade_periodo=premio_unidade, carencia=premio_carencia)
+                                    premio.save()
                         except Exception as e:
-                            print codigo, e
+                            print codigo, unicode(e.args)
                 
                 time.sleep(1)
-            print tipos_remuneracao
         except Exception as e:
             template = "An exception of type {0} occured. Arguments:\n{1!r}"
             message = template.format(type(e).__name__, e.args)
@@ -109,6 +124,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            print Debenture.objects.all().count()
+            Debenture.objects.all().delete()
             inicio = datetime.datetime.now()
             url_debentures = 'http://www.debentures.com.br/exploreosnd/consultaadados/emissoesdedebentures/caracteristicas_r.asp?tip_deb=publicas&op_exc=Nada'
             
