81acfff10bf74d65ba8ab36f7bfcf999f57b2a76
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-21 15:53:47 -0200

    Melhorando busca e listagem de debentures

--- bagogold/bagogold/views/debentures/debentures.py
+++ bagogold/bagogold/views/debentures/debentures.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from bagogold.bagogold.models.debentures import OperacaoDebenture, Debenture
+from bagogold.bagogold.utils.misc import \
+    formatar_zeros_a_direita_apos_2_casas_decimais
 from decimal import Decimal
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -43,9 +45,15 @@ def inserir_operacao_debenture(request):
     return TemplateResponse(request, 'cdb_rdb/inserir_operacao_debenture.html', {})
 
 def listar_debentures(request):
-    investidor = request.user.investidor
-    
     debentures = Debenture.objects.all()
+    
+    for debenture in debentures:
+        if hasattr(debenture, 'jurosdebenture'):
+            debenture.jurosdebenture.taxa = Decimal(formatar_zeros_a_direita_apos_2_casas_decimais(debenture.jurosdebenture.taxa))
+        if hasattr(debenture, 'amortizacaodebenture'):
+            debenture.amortizacaodebenture.taxa = Decimal(formatar_zeros_a_direita_apos_2_casas_decimais(debenture.amortizacaodebenture.taxa))
+        if hasattr(debenture, 'premiodebenture'):
+            debenture.premiodebenture.taxa = Decimal(formatar_zeros_a_direita_apos_2_casas_decimais(debenture.premiodebenture.taxa))
     
     return TemplateResponse(request, 'debentures/listar_debentures.html', {'debentures': debentures})
 
