bff3b833709fcb48a716c85d6c4e8c7b4a9347d2
Guilherme de Deus <kingbowserii@gmail.com>
2017-01-23 02:09:35 -0200

    REmovendo FIIForm n?o utilizado

--- bagogold/bagogold/forms/fii.py
+++ bagogold/bagogold/forms/fii.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from bagogold.bagogold.forms.utils import LocalizedModelForm
-from bagogold.bagogold.models.fii import FII, OperacaoFII, ProventoFII, \
+from bagogold.bagogold.models.fii import OperacaoFII, ProventoFII, \
     UsoProventosOperacaoFII
 from decimal import Decimal
 from django import forms
@@ -15,14 +15,6 @@ ESCOLHAS_CONSOLIDADO=(
 ESCOLHAS_TIPO_OPERACAO=(('C', "Compra"),
                         ('V', "Venda"))
 
-class FIIForm(LocalizedModelForm):
-
-
-    class Meta:
-        model = FII
-        fields = ('ticker', )
-        
-        
 class ProventoFIIForm(LocalizedModelForm):
 
 
