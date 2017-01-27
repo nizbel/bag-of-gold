9310085d8b3369286d71ccc1aa104d151217fbf8
Guilherme Henrique <kingbowserii@gmail.com>
2017-01-27 01:50:04 -0200

    Alterando manager do admin

--- bagogold/bagogold/admin/fii.py
+++ bagogold/bagogold/admin/fii.py
@@ -6,7 +6,18 @@ from django.contrib import admin
 admin.site.register(FII)
 
 class ProventoFIIAdmin(admin.ModelAdmin):
+    search_fields = ['fii__ticker']
     list_display = ('fii', 'valor_unitario', 'data_ex', 'data_pagamento')
+    
+    def get_queryset(self, request):
+        # use our manager, rather than the default one
+        qs = self.model.gerador_objects.get_queryset()
+    
+        # we need this from the superclass method
+        ordering = self.ordering or () # otherwise we might try to *None, which is bad ;)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
     
 admin.site.register(ProventoFII, ProventoFIIAdmin)
     
