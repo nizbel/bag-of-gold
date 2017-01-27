86d4de9c12bc073cb08bc5f23f6ca2a84d22c8b8
Guilherme de Deus <kingbowserii@gmail.com>
2017-01-27 01:50:04 -0200

    Alterando manager do admin

--- bagogold/bagogold/admin/acoes.py
+++ bagogold/bagogold/admin/acoes.py
@@ -9,8 +9,22 @@ import datetime
 
 
 admin.site.register(Acao)
+
+class ProventoAdmin(admin.ModelAdmin):
+    search_fields = ['acao__ticker']
+    list_display = ('acao', 'tipo_provento', 'valor_unitario', 'data_ex', 'data_pagamento')
     
-admin.site.register(Provento)
+    def get_queryset(self, request):
+        # use our manager, rather than the default one
+        qs = self.model.gerador_objects.get_queryset()
+    
+        # we need this from the superclass method
+        ordering = self.ordering or () # otherwise we might try to *None, which is bad ;)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+    
+admin.site.register(Provento, ProventoAdmin)
 
 admin.site.register(AcaoProvento)
     
