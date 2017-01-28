afedd8b0e06cddda47c0cfdf189504602cab7036
Guilherme de Deus <kingbowserii@gmail.com>
2017-01-28 13:13:59 -0200

    Preparando script para consertar quest?o de Debenture da agro

--- bagogold/bagogold/management/commands/alterar_documento_agro.py
+++ bagogold/bagogold/management/commands/alterar_documento_agro.py
@@ -1,34 +1,42 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.models.gerador_proventos import DocumentoProventoBovespa, \
+    PendenciaDocumentoProvento, ProventoAcaoDocumento,\
+    InvestidorLeituraDocumento, InvestidorValidacaoDocumento,\
+    InvestidorRecusaDocumento
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = 'TEMPORÁRIO ajusta provento para documento de acionistas da AGRO'
     
-    def handle:
+    def handle(self, *args, **options):
         documento_deb = DocumentoProventoBovespa.objects.get(protocolo='536527')
         documento_aci = DocumentoProventoBovespa.objects.get(protocolo='536533')
         
+        InvestidorLeituraDocumento.objects.get(documento__protocolo=documento_aci.protocolo).delete()
+        InvestidorValidacaoDocumento.objects.get(documento__protocolo=documento_aci.protocolo).delete()
+                
         # Apagar pendencia
         PendenciaDocumentoProvento.objects.filter(documento__protocolo=documento_aci.protocolo).delete()
         
         # Adicionar responsáveis
-        responsavel_leitura_deb = InvestidorResponsavelLeitura.objects.get(documento__protocolo=documento_deb.protocolo)
-        responsavel_validacao_deb = InvestidorResponsavelValidacao.objects.get(documento__protocolo=documento_deb.protocolo)
+        responsavel_leitura_deb = InvestidorLeituraDocumento.objects.get(documento__protocolo=documento_deb.protocolo)
+        responsavel_validacao_deb = InvestidorValidacaoDocumento.objects.get(documento__protocolo=documento_deb.protocolo)
         
-        responsavel_leitura_aci = responsavel_leitura_deb
-        responsavel_leitura_aci.id = None
-        responsavel_leitura_aci.documento = documento_leitura_aci
-        responsavel_leitura_aci.decisao = 'C'
-        responsavel_leitura_aci.save()
-        responsavel_validacao_aci = responsavel_validacao_deb
-        responsavel_validacao_aci.id = None
-        responsavel_validacao_aci.documento = documento_leitura_aci
-        repsonsavel_validacao_aci.save()
-
         # Alterar decisão do responsável pela leitura do documento de debentures
         responsavel_leitura_deb.decisao = 'E'
         responsavel_leitura_deb.save()
         
+        responsavel_leitura_aci = responsavel_leitura_deb
+        responsavel_leitura_aci.pk = None
+        responsavel_leitura_aci.documento = documento_aci
+        responsavel_leitura_aci.decisao = 'C'
+        responsavel_leitura_aci.save()
+        responsavel_validacao_aci = responsavel_validacao_deb
+        responsavel_validacao_aci.pk = None
+        responsavel_validacao_aci.documento = documento_aci
+        responsavel_validacao_aci.save()
+        
         # Mirar provento no documento de acionistas
         provento_documento = ProventoAcaoDocumento.objects.get(documento__protocolo=documento_deb.protocolo)
         provento_documento.documento = documento_aci
-        provento_documento.documento.save()
+        provento_documento.save()
