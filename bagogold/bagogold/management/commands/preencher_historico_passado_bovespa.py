1733ab649290a14a43942ecd338d65a1ec7af11e
Guilherme de Deus <kingbowserii@gmail.com>
2017-02-08 12:34:32 -0200

    Preparado para adicionar UNITS

--- bagogold/bagogold/management/commands/preencher_historico_passado_bovespa.py
+++ bagogold/bagogold/management/commands/preencher_historico_passado_bovespa.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.models.fii import HistoricoFII, FII
 from bagogold.bagogold.tfs import ler_serie_historica_anual_bovespa
 from django.core.management.base import BaseCommand
+from django.db.models.aggregates import Count
 from threading import Thread
+import datetime
 
 class BuscaHistoricoBovespaThread(Thread):
     def __init__(self, ano):
@@ -21,9 +24,19 @@ class Command(BaseCommand):
     help = 'Preenche histórico para ações e FIIs com dados da bovespa'
 
     def handle(self, *args, **options):
+#         historicos_repetidos = list()
+#         for fii_id in HistoricoFII.objects.all().values_list('fii', flat=True).distinct():
+#             historicos_repetidos = HistoricoFII.objects.filter(fii__id=fii_id).values('data').annotate(num_datas=Count('id')).filter(num_datas__gt=1)
+#             for data_repetida in historicos_repetidos:
+#                 print FII.objects.get(id=fii_id), historicos_repetidos
+#                 historicos_apagar = HistoricoFII.objects.filter(fii__id=fii_id, data=data_repetida['data'])
+#                 for indice in range(1, data_repetida['num_datas']):
+#                     print 'apagar', historicos_apagar[0]
+#                     historicos_apagar[0].delete()
+                    
         # O incremento mostra quantas threads correrão por vez
-        incremento = 1
-        anos = range(2007, 2017)
+        incremento = 2
+        anos = range(2002, datetime.date.today().year+1)
         contador = 0
         while contador <= len(anos):
             threads = []
