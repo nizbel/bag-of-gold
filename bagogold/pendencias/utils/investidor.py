e17aa919418f30989b41986e0effcb0ed972de5b
Guilherme Henrique <kingbowserii@gmail.com>
2017-02-12 12:39:12 -0200

    Busca geral de pendencias funcionando

--- bagogold/pendencias/utils/investidor.py
+++ bagogold/pendencias/utils/investidor.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from bagogold.pendencias.models.pendencias import \
-    PendenciaVencimentoTesouroDireto
+from bagogold.bagogold.models.td import OperacaoTitulo
+from bagogold.pendencias.models.pendencias import PendenciaVencimentoTesouroDireto
+from django.db.models.aggregates import Sum
+from django.db.models.expressions import Case, When, F
+from django.db.models.fields import DecimalField
+import datetime
 
 def buscar_pendencias_investidor(investidor):
     """
@@ -19,3 +23,15 @@ def verificar_pendencias_investidor(investidor):
     Verifica todas as pendências que um investidor pode ter
     Parâmetros: Investidor
     """
+    # Pendências de vencimento de Tesouro Direto
+    qtd_titulos_vencidos = list(OperacaoTitulo.objects.filter(investidor=investidor, titulo__data_vencimento__lte=datetime.date.today()) \
+        .values('titulo') \
+        .annotate(qtd_soma=Sum(Case(When(tipo_operacao='C', then=F('quantidade')),
+                            When(tipo_operacao='V', then=F('quantidade')*-1),
+                            output_field=DecimalField()))))
+    
+    print qtd_titulos_vencidos
+    
+    for titulo_qtd in qtd_titulos_vencidos:
+        if titulo_qtd['qtd_soma'] > 0:
+            PendenciaVencimentoTesouroDireto.verificar_pendencia(investidor, titulo_qtd['titulo'], titulo_qtd['qtd_soma'])
