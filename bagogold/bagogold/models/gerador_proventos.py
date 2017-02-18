d35d03ffcba5037e56f93f78e06c4450f05bb7b7
Guilherme de Deus <kingbowserii@gmail.com>
2017-02-18 19:50:30 -0200

    Adicionando mais tipos de documento validos para fii

--- bagogold/bagogold/models/gerador_proventos.py
+++ bagogold/bagogold/models/gerador_proventos.py
@@ -14,7 +14,7 @@ def ticker_path(instance, filename):
     return 'doc proventos/{0}/{1}'.format(instance.ticker_empresa(), filename)
 
 class DocumentoProventoBovespa (models.Model):
-    TIPOS_DOCUMENTO_VALIDOS = [u'Fato Relevante', u'Comunicado ao Mercado', u'Aviso aos Acionistas']
+    TIPOS_DOCUMENTO_VALIDOS = [u'Fato Relevante', u'Comunicado ao Mercado', u'Aviso aos Acionistas', u'Aviso aos Cotistas', u'Aviso aos Cotistas - Estruturado']
     
     url = models.CharField(u'URL do documento', blank=True, null=True, max_length=200)
     empresa = models.ForeignKey('Empresa')
