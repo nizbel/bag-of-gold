5882e594f6fa04f8a5c689eacb936c6637ea3d0a
Guilherme de Deus <kingbowserii@gmail.com>
2017-02-26 13:48:42 -0300

    Parte 1 da leitura de FII ok

--- bagogold/bagogold/forms/gerador_proventos.py
+++ bagogold/bagogold/forms/gerador_proventos.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from bagogold.bagogold.forms.utils import LocalizedModelForm
 from bagogold.bagogold.models.gerador_proventos import \
-    ProventoAcaoDescritoDocumentoBovespa, AcaoProventoAcaoDescritoDocumentoBovespa
+    ProventoAcaoDescritoDocumentoBovespa, AcaoProventoAcaoDescritoDocumentoBovespa, \
+    ProventoFIIDescritoDocumentoBovespa
 from django import forms
 from django.forms import widgets
 
 
-ESCOLHAS_TIPO_PROVENTO=(('A', "Ações"),
+ESCOLHAS_TIPO_PROVENTO_ACAO=(('A', "Ações"),
                         ('D', "Dividendos"),
                         ('J', "Juros sobre capital próprio"),)
+
+ESCOLHAS_TIPO_PROVENTO_FII=(('R', "Rendimento"),
+                        ('A', "Amortização"),)
 
 class ProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
 
@@ -20,7 +24,7 @@ class ProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
                                             'placeholder':'Selecione uma data'}),
                  'data_pagamento': widgets.DateInput(attrs={'class':'datepicker', 
                                             'placeholder':'Selecione uma data'}),
-                 'tipo_provento': widgets.Select(choices=ESCOLHAS_TIPO_PROVENTO),}
+                 'tipo_provento': widgets.Select(choices=ESCOLHAS_TIPO_PROVENTO_ACAO),}
         
     def clean(self):
         dados = super(ProventoAcaoDescritoDocumentoBovespaForm, self).clean()
@@ -38,3 +42,22 @@ class AcaoProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
         widgets={'data_pagamento_frac': widgets.DateInput(attrs={'class':'datepicker', 
                                             'placeholder':'Selecione uma data'})}
 
+class ProventoFIIDescritoDocumentoBovespaForm(LocalizedModelForm):
+
+    class Meta:
+        model = ProventoFIIDescritoDocumentoBovespa
+        fields = ('valor_unitario', 'data_ex', 'data_pagamento',
+                  'fii',)
+        widgets={'data_ex': widgets.DateInput(attrs={'class':'datepicker', 
+                                            'placeholder':'Selecione uma data'}),
+                 'data_pagamento': widgets.DateInput(attrs={'class':'datepicker', 
+                                            'placeholder':'Selecione uma data'}),
+                 'tipo_provento': widgets.Select(choices=ESCOLHAS_TIPO_PROVENTO_FII),}
+        
+    def clean(self):
+        dados = super(ProventoFIIDescritoDocumentoBovespaForm, self).clean()
+        data_ex = dados.get('data_ex')
+        data_pagamento = dados.get('data_pagamento')
+        if (data_ex > data_pagamento):
+            raise forms.ValidationError("Data EX deve ser anterior a data de pagamento")
+        return dados
