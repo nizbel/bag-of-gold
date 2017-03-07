ffd4f5b467faf0c5193a9afbaf0c1c41553cf780
Guilherme Henrique <kingbowserii@gmail.com>
2017-03-06 22:08:20 -0300

    Preparando aplicativo de cri_cra

---
+++ bagogold/cri_cra/models/cri_cra.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+class CRI_CRA (models.Model):
+    TIPO_CRI = 'I'
+    TIPO_CRA = 'A'
+    ESCOLHAS_TIPO_CRI_CRA = ((TIPO_CRI, 'CRI'),
+                             (TIPO_CRA, 'CRA'))
+    
+    
+    codigo = models.CharField(u'Código', max_length=20)
+    tipo = models.CharField(u'Tipo', max_length=1, choices=ESCOLHAS_TIPO_CRI_CRA)
+    """
+    1 = Prefixado, 2 = IPCA, 3 = DI
+    """
+    tipo_indexacao = models.PositiveSmallIntegerField(u'Tipo de indexação')
+    porcentagem = models.DecimalField(u'Porcentagem sobre indexação', decimal_places=3, max_digits=6)
+    juros_adicional = models.DecimalField(u'Juros adicional', decimal_places=3, max_digits=6)
+    data_emissao = models.DateField(u'Data de emissão')
+    valor_emissao = models.DecimalField(u'Valor nominal na emissão', max_digits=15, decimal_places=8)
+    data_vencimento = models.DateField(u'Data de vencimento')
+    
+    def __unicode__(self):
+        return '%s, emitida em %s a R$ %s, com vencimento em %s' % (self.codigo, str(self.data_emissao), self.valor_emissao, str(self.data_vencimento))
+    
+class OperacaoCRI_CRA (models.Model):
+    investidor = models.ForeignKey('Investidor')
+    cri_cra = models.ForeignKey('CRI_CRA')
+    preco_unitario = models.DecimalField(u'Preço unitário', max_digits=11, decimal_places=2)
+    quantidade = models.IntegerField(u'Quantidade')
+    data = models.DateField(u'Data da operação')
+    tipo_operacao = models.CharField(u'Tipo de operação', max_length=1)
