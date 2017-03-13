52ac17d1137b7283e99487d49cbab34062935b9a
Guilherme Henrique <kingbowserii@gmail.com>
2017-03-13 00:33:09 -0300

    Preparando edi??o de opera??o em CRI/CRA

--- bagogold/cri_cra/urls.py
+++ bagogold/cri_cra/urls.py
@@ -5,7 +5,7 @@ import views
 urlpatterns = [
     url(r'^detalhar_cri_cra/(?P<id_cri_cra>\d+)/$', views.cri_cra.detalhar_cri_cra, name='detalhar_cri_cra'),
     url(r'^editar_cri_cra/(?P<id_cri_cra>\d+)/$', views.cri_cra.editar_cri_cra, name='editar_cri_cra'),
-#     url(r'^editar_operacao/(?P<id_operacao>\d+)/$', views.cri_cra.editar_operacao_cri_cra, name='editar_operacao_cri_cra'),
+    url(r'^editar_operacao/(?P<id_operacao>\d+)/$', views.cri_cra.editar_operacao_cri_cra, name='editar_operacao_cri_cra'),
     url(r'^historico/$', views.cri_cra.historico, name='historico_cri_cra'),
     url(r'^inserir_cri_cra$', views.cri_cra.inserir_cri_cra, name='inserir_cri_cra'),
     url(r'^inserir_operacao_cri_cra$', views.cri_cra.inserir_operacao_cri_cra, name='inserir_operacao_cri_cra'),
