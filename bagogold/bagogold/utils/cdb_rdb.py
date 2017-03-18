097854254f45624add534398d1ba7839d1b9a5c1
Guilherme Henrique <kingbowserii@gmail.com>
2017-03-18 03:47:47 -0300

    Melhorando buscas de quantidade por divisao

--- bagogold/bagogold/utils/cdb_rdb.py
+++ bagogold/bagogold/utils/cdb_rdb.py
@@ -45,6 +45,7 @@ def calcular_valor_cdb_rdb_ate_dia(investidor, dia):
     # Buscar taxas dos dias
     historico = HistoricoTaxaDI.objects.all()
     for operacao in operacoes:
+        # TODO consertar verificação de todas vendidas
         operacao.quantidade -= 0 if operacao.id not in qtd_vendida_operacoes.keys() else qtd_vendida_operacoes[operacao.id]
         if operacao.quantidade == 0:
             continue
@@ -69,9 +70,11 @@ def calcular_valor_cdb_rdb_ate_dia_por_divisao(dia, divisao_id):
                 ID da divisão
     Retorno: Valor de cada CDB/RDB da divisão na data escolhida {id_investimento: valor_na_data, }
     """
-    operacoes_divisao_id = DivisaoOperacaoCDB_RDB.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).values('operacao__id')
-    if len(operacoes_divisao_id) == 0:
+    if not DivisaoOperacaoCDB_RDB.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).exists():
         return {}
+    
+    operacoes_divisao_id = DivisaoOperacaoCDB_RDB.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).values('operacao__id')
+    
     operacoes_queryset = OperacaoCDB_RDB.objects.exclude(data__isnull=True).filter(id__in=operacoes_divisao_id).order_by('-tipo_operacao', 'data') 
     operacoes = list(operacoes_queryset)
     for operacao in operacoes:
@@ -82,62 +85,41 @@ def calcular_valor_cdb_rdb_ate_dia_por_divisao(dia, divisao_id):
     # Pegar data inicial
     data_inicial = operacoes_queryset.order_by('data')[0].data
     
-    data_final = max(HistoricoTaxaDI.objects.filter(data__lte=dia).order_by('-data')[0].data, datetime.date.today())
-    
-    data_iteracao = data_inicial
-    
     cdb_rdb = {}
-    
-    while data_iteracao <= data_final:
+    for operacao in operacoes:
         # Processar operações
-        operacoes_do_dia = operacoes_queryset.filter(data=data_iteracao)
-        for operacao in operacoes_do_dia:          
-            if operacao.investimento.id not in cdb_rdb.keys():
-                cdb_rdb[operacao.investimento.id] = 0
+        if operacao.investimento.id not in cdb_rdb.keys():
+            cdb_rdb[operacao.investimento.id] = 0
                 
-            # Vendas
-            if operacao.tipo_operacao == 'V':
-                # Remover quantidade da operação de compra
-                operacao_compra_id = operacao.operacao_compra_relacionada().id
-                for operacao_c in operacoes:
-                    if (operacao_c.id == operacao_compra_id):
-                        operacao.atual = (DivisaoOperacaoCDB_RDB.objects.get(divisao__id=divisao_id, operacao=operacao).quantidade/DivisaoOperacaoLC.objects.get(divisao__id=divisao_id, operacao=operacao_c).quantidade) * operacao_c.atual
-                        operacao_c.atual -= operacao.atual
-                        str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
-                        operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
-                        operacoes.remove(operacao)
-                        if operacao_c.atual == 0:
-                            operacoes.remove(operacao_c)
-                        break
-                    
-        # Calcular o valor atualizado do patrimonio diariamente
-        try:
-            taxa_do_dia = HistoricoTaxaDI.objects.get(data=data_iteracao).taxa
-        except:
-            taxa_do_dia = 0
+        # Vendas
+        if operacao.tipo_operacao == 'V':
+            # Remover quantidade da operação de compra
+            operacao_compra_id = operacao.operacao_compra_relacionada().id
+            for operacao_c in operacoes:
+                if (operacao_c.id == operacao_compra_id):
+                    operacao.atual = DivisaoOperacaoCDB_RDB.objects.get(divisao__id=divisao_id, operacao=operacao).quantidade
+                    operacao_c.atual -= operacao.atual
+                    break
+                
+    # Remover operações de venda e operações de compra totalmente vendidas
+    operacoes = [operacao for operacao in operacoes if operacao.tipo_operacao == 'C' and operacao.atual > 0]
+    
+    # Calcular o valor atualizado do patrimonio
+    historico = HistoricoTaxaDI.objects.filter(data__range=[data_inicial, dia])
+    
+    for operacao in operacoes:
+        taxas_dos_dias = dict(historico.filter(data__range=[operacao.data, dia]).values('taxa').annotate(qtd_dias=Count('taxa')).values_list('taxa', 'qtd_dias'))
+        operacao.atual = calcular_valor_atualizado_com_taxas(taxas_dos_dias, operacao.atual, operacao.taxa)
+        # Arredondar valores
+        str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
+        operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
             
-        for operacao in operacoes:
-            if (operacao.data <= data_iteracao):
-                if taxa_do_dia > 0:
-                    operacao.atual = calcular_valor_atualizado_com_taxa(taxa_do_dia, operacao.atual, operacao.taxa)
-                # Arredondar na última iteração
-                if (data_iteracao == data_final):
-                    str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
-                    operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
-            
-        # Proximo dia útil
-        proximas_datas = HistoricoTaxaDI.objects.filter(data__gt=data_iteracao).order_by('data')
-        if len(proximas_datas) > 0:
-            data_iteracao = proximas_datas[0].data
-        elif data_iteracao < datetime.date.today():
-            data_iteracao = datetime.date.today()
-        else:
-            break
-        
     # Preencher os valores nas letras de crédito
-    for investimento_id in cdb_rdb.keys():
-        for operacao in operacoes:
-            if operacao.investimento.id == investimento_id:
-                cdb_rdb[investimento_id] += operacao.atual
+    for cdb_rdb_id in cdb_rdb.keys():
+        cdb_rdb[cdb_rdb_id] += sum([operacao.atual for operacao in operacoes if operacao.investimento.id == cdb_rdb_id])
+        
+        if cdb_rdb[cdb_rdb_id] == 0:
+            del cdb_rdb[cdb_rdb_id]
+    
     
     return cdb_rdb
--- bagogold/bagogold/utils/lc.py
+++ bagogold/bagogold/utils/lc.py
@@ -2,11 +2,11 @@
 from bagogold.bagogold.models.divisoes import Divisao, DivisaoOperacaoLC
 from bagogold.bagogold.models.lc import OperacaoLetraCredito, HistoricoTaxaDI, \
     HistoricoPorcentagemLetraCredito, OperacaoVendaLetraCredito
+from bagogold.bagogold.utils.misc import qtd_dias_uteis_no_periodo
 from decimal import Decimal, ROUND_DOWN
 from django.db.models import Q
 from django.db.models.aggregates import Sum, Count
 import datetime
-from bagogold.bagogold.utils.misc import qtd_dias_uteis_no_periodo
 
 def calcular_valor_atualizado_com_taxa(taxa_do_dia, valor_atual, operacao_taxa):
     """
@@ -89,12 +89,13 @@ def calcular_valor_lc_ate_dia_por_divisao(dia, divisao_id):
                 ID da divisão
     Retorno: Valor de cada letra de crédito da divisão na data escolhida {id_letra: valor_na_data, }
     """
-    operacoes_divisao_id = DivisaoOperacaoLC.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).values('operacao__id')
-    if len(operacoes_divisao_id) == 0:
+    if not DivisaoOperacaoLC.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).exists():
         return {}
+    
+    operacoes_divisao_id = DivisaoOperacaoLC.objects.filter(operacao__data__lte=dia, divisao__id=divisao_id).values('operacao__id')
+    
     operacoes_queryset = OperacaoLetraCredito.objects.exclude(data__isnull=True).filter(id__in=operacoes_divisao_id).order_by('-tipo_operacao', 'data') 
     operacoes = list(operacoes_queryset)
-    historico_porcentagem = HistoricoPorcentagemLetraCredito.objects.filter(Q(data__lte=dia) | Q(data__isnull=True)).order_by('-data')
     for operacao in operacoes:
         if operacao.tipo_operacao == 'C':
             operacao.atual = DivisaoOperacaoLC.objects.get(divisao__id=divisao_id, operacao=operacao).quantidade
@@ -103,63 +104,41 @@ def calcular_valor_lc_ate_dia_por_divisao(dia, divisao_id):
     # Pegar data inicial
     data_inicial = operacoes_queryset.order_by('data')[0].data
     
-    data_final = max(HistoricoTaxaDI.objects.filter(data__lte=dia).order_by('-data')[0].data, datetime.date.today())
-    
-    data_iteracao = data_inicial
-    
     letras_credito = {}
-    
-    while data_iteracao <= data_final:
+    for operacao in operacoes:
         # Processar operações
-        operacoes_do_dia = operacoes_queryset.filter(data=data_iteracao)
-        for operacao in operacoes_do_dia:          
-            if operacao.letra_credito.id not in letras_credito.keys():
-                letras_credito[operacao.letra_credito.id] = 0
+        if operacao.letra_credito.id not in letras_credito.keys():
+            letras_credito[operacao.letra_credito.id] = 0
                 
-            # Vendas
-            if operacao.tipo_operacao == 'V':
-                # Remover quantidade da operação de compra
-                operacao_compra_id = operacao.operacao_compra_relacionada().id
-                for operacao_c in operacoes:
-                    if (operacao_c.id == operacao_compra_id):
-                        operacao.atual = (DivisaoOperacaoLC.objects.get(divisao__id=divisao_id, operacao=operacao).quantidade/DivisaoOperacaoLC.objects.get(divisao__id=divisao_id, operacao=operacao_c).quantidade) * operacao_c.atual
-                        operacao_c.atual -= operacao.atual
-                        str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
-                        operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
-                        operacoes.remove(operacao)
-                        if operacao_c.atual == 0:
-                            operacoes.remove(operacao_c)
-                        break
-                    
-        # Calcular o valor atualizado do patrimonio diariamente
-        try:
-            taxa_do_dia = HistoricoTaxaDI.objects.get(data=data_iteracao).taxa
-        except:
-            taxa_do_dia = 0
+        # Vendas
+        if operacao.tipo_operacao == 'V':
+            # Remover quantidade da operação de compra
+            operacao_compra_id = operacao.operacao_compra_relacionada().id
+            for operacao_c in operacoes:
+                if (operacao_c.id == operacao_compra_id):
+                    operacao.atual = DivisaoOperacaoLC.objects.get(divisao__id=divisao_id, operacao=operacao).quantidade
+                    operacao_c.atual -= operacao.atual
+                    break
+                
+    # Remover operações de venda e operações de compra totalmente vendidas
+    operacoes = [operacao for operacao in operacoes if operacao.tipo_operacao == 'C' and operacao.atual > 0]
+    
+    # Calcular o valor atualizado do patrimonio
+    historico = HistoricoTaxaDI.objects.filter(data__range=[data_inicial, dia])
+    
+    for operacao in operacoes:
+        taxas_dos_dias = dict(historico.filter(data__range=[operacao.data, dia]).values('taxa').annotate(qtd_dias=Count('taxa')).values_list('taxa', 'qtd_dias'))
+        operacao.atual = calcular_valor_atualizado_com_taxas(taxas_dos_dias, operacao.atual, operacao.taxa)
+        # Arredondar valores
+        str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
+        operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
             
-        for operacao in operacoes:
-            if (operacao.data <= data_iteracao):
-                if taxa_do_dia > 0:
-                    operacao.atual = calcular_valor_atualizado_com_taxa(taxa_do_dia, operacao.atual, operacao.taxa)
-                # Arredondar na última iteração
-                if (data_iteracao == data_final):
-                    str_auxiliar = str(operacao.atual.quantize(Decimal('.0001')))
-                    operacao.atual = Decimal(str_auxiliar[:len(str_auxiliar)-2])
-            
-        # Proximo dia útil
-        proximas_datas = HistoricoTaxaDI.objects.filter(data__gt=data_iteracao).order_by('data')
-        if len(proximas_datas) > 0:
-            data_iteracao = proximas_datas[0].data
-        elif data_iteracao < datetime.date.today():
-            data_iteracao = datetime.date.today()
-        else:
-            break
-        
     # Preencher os valores nas letras de crédito
     for letra_credito_id in letras_credito.keys():
-        for operacao in operacoes:
-            if operacao.letra_credito.id == letra_credito_id:
-                letras_credito[letra_credito_id] += operacao.atual
+        letras_credito[letra_credito_id] += sum([operacao.atual for operacao in operacoes if operacao.letra_credito.id == letra_credito_id])
+        
+        if letras_credito[letra_credito_id] == 0:
+            del letras_credito[letra_credito_id]
     
     return letras_credito
 
