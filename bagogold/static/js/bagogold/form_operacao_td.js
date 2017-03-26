54de67d72ad2e3ae941b8f44e144f9ec9b67b4e5
Guilherme de Deus <kingbowserii@gmail.com>
2017-03-26 19:45:49 -0300

    Preparando para adicionar opera??es em TD no novo formato

--- bagogold/static/js/bagogold/form_operacao_td.js
+++ bagogold/static/js/bagogold/form_operacao_td.js
@@ -3,50 +3,142 @@ $(document).ready(function() {
     // get extra form count so we know what index to use for the next item.
      $("#add-another").click(function() {
          divisao_id = parseInt(form_count) + 1;
-         titulo = $("<h3>Divisão " + divisao_id + "</h3>");
+         titulo = $("<div class='col-md-12'><h3 class='font-yellow-crusta' align='center'><strong>Divisão " + divisao_id + "</strong></h3><br/></div>");
+         row = $('#divisao-0').clone();
+         row.attr('id', 'divisao-' + form_count);
+         $(row).find("*").each(function() { 
+        	 if ($(this).is('input')) {
+        		 $(this).val('');
+        	 }
+        	 if ($(this).is('option')) {
+        		 $(this).removeAttr('selected');
+        	 }
+        	 $.each(this.attributes, function() {
+        		 this.value = this.value.replace('_set-0', '_set-' + form_count);
+        	 });
+         });
          
-         label_divisao = $("<label for=id_divisaooperacaotd_set-" + form_count + "-divisao>Divisão:</label>");
-         divisao = $("#id_divisaooperacaotd_set-0-divisao").clone();
-         divisao.attr("name", "divisaooperacaotd_set-" + form_count + "-divisao");
-         divisao.attr('id', "id_divisaooperacaotd_set-" + form_count + "-divisao");
-         divisao.removeAttr('value');
-         par_divisao = $('<p></p>');
-         par_divisao.append(label_divisao);
-         par_divisao.append(divisao);
-         
-    
-         label_quantidade = $("<label for=id_divisaooperacaotd_set-" + form_count + "-quantidade>Quantidade:</label>");
-         quantidade = $("#id_divisaooperacaotd_set-0-quantidade").clone();
-         quantidade.attr("name", "divisaooperacaotd_set-" + form_count + "-quantidade");
-         quantidade.attr('id', "id_divisaooperacaotd_set-" + form_count + "-quantidade");
-         quantidade.removeAttr('value');
-         par_quantidade = $('<p></p>');
-         par_quantidade.append(label_quantidade);
-         par_quantidade.append(quantidade);
-         
-         id = $("#id_divisaooperacaotd_set-0-id").clone();
-         id.attr("name", "divisaooperacaotd_set-" + form_count + "-id");
-         id.attr('id', "id_divisaooperacaotd_set-" + form_count + "-id");
-         id.removeAttr('value');
-         par_id = $('<p></p>');
-         par_id.append(id);
-    
-         operacao = $("#id_divisaooperacaotd_set-0-operacao").clone();
-         operacao.attr("name", "divisaooperacaotd_set-" + form_count + "-operacao");
-         operacao.attr('id', "id_divisaooperacaotd_set-" + form_count + "-operacao");
-         par_operacao = $('<p></p>');
-         par_operacao.append(operacao);
-      
          $("#forms").append(titulo);
-         $("#forms").append(par_divisao);
-         $("#forms").append(par_quantidade);
-         $("#forms").append(par_id);
-         $("#forms").append(par_operacao);
+         $("#forms").append(row);
          // build divisao and append it to our forms container
-    
-         form_count ++;
-         $("[name=divisaooperacaotd_set-TOTAL_FORMS]").val(form_count);
+         
          // increment form count so our view knows to populate 
          // that many fields for validation
-     })
+         form_count ++;
+         $("[name=divisaooperacaotd_set-TOTAL_FORMS]").val(form_count);
+         // ready bootstrap elements
+         row.find('.bootstrap-select').replaceWith(function() { return $('select', this); });
+         row.find('select').selectpicker('refresh');
+         row.find('.bootstrap-touchspin').find('*').not('input').remove();
+         row.find("input[name$='-quantidade']").TouchSpin({
+        	 initval: 0,
+             min: 0,
+             max: 1000000000,
+             step: 0.01,
+             decimals: 2,
+             maxboostedstep: 100,
+             postfix: 'títulos'
+         });
+     });
+     
+     $( "#id_data" ).change(function() {
+ 		var dataEscolhida = $(this).val();
+ 		if (dataEscolhida == null || dataEscolhida == '') {
+ 			return;
+ 		}
+ 		var tipoOperacao = $('#id_tipo_operacao').val();
+ 		$.ajax({
+ 			url : "{% url 'td:buscar_titulos_validos_na_data' %}", // the endpoint
+ 			type : "GET", // http method
+ 			data: {dataEscolhida: dataEscolhida, tipoOperacao: tipoOperacao},
+ 			
+ 			// handle a successful response
+ 			success : function(lista_titulos_validos) {
+ 				$("#id_titulo option").each(function() {
+ 					if ($.inArray(parseInt(this.value), lista_titulos_validos) > -1) {
+ 						$("#id_titulo option[value=" + this.value + "]").show();
+ 					} else {
+ 						$("#id_titulo option[value=" + this.value + "]").hide();
+ 					}
+ 				});
+ 			}, 
+ 				
+ 			// handle a non-successful response
+ 			error : function(xhr,errmsg,err) {
+ 				console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+ 			}
+ 		});
+ 	});
+ 	
+ 	$( "#id_tipo_operacao" ).change(function() {
+ 		var dataEscolhida = $('#id_data').val();
+ 		if (dataEscolhida == null || dataEscolhida == '') {
+ 			return;
+ 		}
+ 		var tipoOperacao = $(this).val();
+ 		$.ajax({
+ 			url : "{% url 'td:buscar_titulos_validos_na_data' %}", // the endpoint
+ 			type : "GET", // http method
+ 			data: {dataEscolhida: dataEscolhida, tipoOperacao: tipoOperacao},
+ 			
+ 			// handle a successful response
+ 			success : function(lista_titulos_validos) {
+ 				$("#id_titulo option").each(function() {
+ 					if ($.inArray(parseInt(this.value), lista_titulos_validos) > -1) {
+ 						$("#id_titulo option[value=" + this.value + "]").show();
+ 					} else {
+ 						$("#id_titulo option[value=" + this.value + "]").hide();
+ 					}
+ 				});
+ 			}, 
+ 				
+ 			// handle a non-successful response
+ 			error : function(xhr,errmsg,err) {
+ 				console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+ 			}
+ 		});
+ 	});
+ 	$("input[name='preco_unitario']").TouchSpin({
+ 		min: 0,
+ 		max: 1000000000,
+ 		step: 0.01,
+ 		decimals: 2,
+ 		maxboostedstep: 100,
+ 		prefix: 'R$'
+ 	});
+ 	$("input[name='quantidade']").TouchSpin({
+ 		min: 0,
+ 		max: 1000000000,
+        step: 0.01,
+        decimals: 2,
+ 		maxboostedstep: 100,
+        postfix: 'títulos'
+ 	});
+ 	$("input[name='taxa_bvmf']").TouchSpin({
+ 		min: 0,
+ 		max: 1000000000,
+ 		step: 0.01,
+ 		decimals: 2,
+ 		maxboostedstep: 100,
+ 		prefix: 'R$'
+ 	});
+ 	$("input[name='taxa_custodia']").TouchSpin({
+ 		min: 0,
+ 		max: 1000000000,
+ 		step: 0.01,
+ 		decimals: 2,
+ 		maxboostedstep: 100,
+ 		prefix: 'R$'
+ 	});
+ 	$("input[name$='-quantidade']").TouchSpin({
+ 		min: 0,
+ 		max: 1000000000,
+ 		step: 0.01,
+ 		decimals: 2,
+ 		maxboostedstep: 100,
+        postfix: 'títulos'
+ 	});
+ 	$('.date-picker').datepicker({
+ 		language: 'pt-BR'
+ 	});
 });
