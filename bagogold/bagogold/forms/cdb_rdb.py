bb431884a9e196e883603b23faba2ea7cf8636b9
Guilherme Henrique <kingbowserii@gmail.com>
2017-04-27 20:11:37 -0300

    Preparando edi??o de historico de porcentagem de cdb/rdb

--- bagogold/bagogold/forms/cdb_rdb.py
+++ bagogold/bagogold/forms/cdb_rdb.py
@@ -121,9 +121,19 @@ class HistoricoPorcentagemCDB_RDBForm(LocalizedModelForm):
             self.fields['data'].disabled = True
     
     def clean_cdb_rdb(self):
-        if self.cleaned_data['cdb_rdb'].investidor != self.investidor:
+        cdb_rdb = self.cleaned_data['cdb_rdb']
+        if cdb_rdb.investidor != self.investidor:
             raise forms.ValidationError('CDB/RDB inválido')
-        return self.cleaned_data['cdb_rdb']
+        if cdb_rdb != self.instance.cdb_rdb:
+            raise forms.ValidationError('CDB/RDB não deve ser alterado')
+        return cdb_rdb
+        
+    def clean_data(self):
+        data = self.cleaned_data['data']
+        # Verifica se o registro é da data incial, e se foi feita alteração
+        if not self.instance.data and data:
+            raise forms.ValidationError('Data inicial não pode ser alterada')
+        return data
     
     def clean_porcentagem(self):
         porcentagem = self.cleaned_data['porcentagem']
@@ -134,11 +144,9 @@ class HistoricoPorcentagemCDB_RDBForm(LocalizedModelForm):
     def clean(self):
         cleaned_data = super(HistoricoPorcentagemCDB_RDBForm, self).clean()
         # Testar se já existe algum histórico para o investimento na data
-        try:
-            historico = HistoricoPorcentagemCDB_RDB.objects.get(cdb_rdb=cleaned_data.get('cdb_rdb'), data=cleaned_data.get('data'))
+        data = cleaned_data.get('data')
+        if data and HistoricoPorcentagemCDB_RDB.objects.filter(cdb_rdb=cleaned_data.get('cdb_rdb'), data=data).exists():
             raise forms.ValidationError('Já existe uma alteração de porcentagem para essa data')
-        except HistoricoPorcentagemCDB_RDB.DoesNotExist:
-            pass
         return cleaned_data
         
 class HistoricoCarenciaCDB_RDBForm(LocalizedModelForm):
