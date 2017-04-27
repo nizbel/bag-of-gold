b168e59dd4d4dcc4f0d62cd5ae19fda9bcf5b5cf
Guilherme de Deus <kingbowserii@gmail.com>
2017-04-27 18:54:22 -0300

    Preparando tela de edi??o de CDB/RDB

--- bagogold/bagogold/views/investidores/investidores.py
+++ bagogold/bagogold/views/investidores/investidores.py
@@ -7,8 +7,6 @@ from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
 from django.dispatch import receiver
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render_to_response, redirect
-from django.template.context import RequestContext
 from django.template.response import TemplateResponse
 from registration.signals import user_activated
 
