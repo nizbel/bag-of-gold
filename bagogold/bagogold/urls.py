ac43919f1938f82dc6bb5c1d640c76bff08f80f4
Guilherme de Deus <kingbowserii@gmail.com>
2017-04-30 15:03:35 -0300

    P?gina de acompanhamento de FII OK. Acompanhamento mensal removido at? estar pronto

--- bagogold/bagogold/urls.py
+++ bagogold/bagogold/urls.py
@@ -77,7 +77,7 @@ divisoes_patterns = [
     ]
 
 fiis_patterns = [
-    url(r'^acompanhamento_mensal/$', views.fii.fii.acompanhamento_mensal_fii, name='acompanhamento_mensal_fii'),
+#     url(r'^acompanhamento_mensal/$', views.fii.fii.acompanhamento_mensal_fii, name='acompanhamento_mensal_fii'),
     url(r'^acompanhamento/$', views.fii.fii.acompanhamento_fii, name='acompanhamento_fii'),
     url(r'^calcular_resultado_corretagem/$', views.fii.fii.calcular_resultado_corretagem, name='calcular_resultado_corretagem'),
     url(r'^editar_operacao/(?P<operacao_id>\d+)/$', views.fii.fii.editar_operacao_fii, name='editar_operacao_fii'),
