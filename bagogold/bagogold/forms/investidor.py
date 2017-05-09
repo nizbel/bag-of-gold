c267ca57db87a3a6bc71f7bd062741578a6221c0
Guilherme de Deus <kingbowserii@gmail.com>
2017-05-09 10:49:20 -0300

    Evitando ataques de for?a bruta e for?ando senha a ter pelo menos 8 caracteres

--- bagogold/bagogold/forms/investidor.py
+++ bagogold/bagogold/forms/investidor.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from bagogold.bagogold.forms.utils import LocalizedModelForm
+from bagogold.bagogold.models.investidores import LoginIncorreto
+from bagogold.bagogold.utils.investidores import user_blocked
 from django import forms
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class DadosCadastraisForm(LocalizedModelForm):
@@ -26,3 +33,87 @@ class DadosCadastraisForm(LocalizedModelForm):
         if email and User.objects.filter(email=email).exclude(username=self.username):
             raise forms.ValidationError('Este email já está em uso, por favor insira outro email')
         return email
+
+class ExtendedUserCreationForm(UserCreationForm):
+    def clean_password1(self):
+        password = self.cleaned_data.get('password1')
+        if len(password) < 8:
+            raise forms.ValidationError('A senha deve ter no mínimo 8 caracteres')
+        return super(ExtendedUserCreationForm, self).clean_password1()
+
+class ExtendedAuthForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': (u"Senha inválida para o usuário."),
+        'inactive': (u"Esta conta está inativa."),
+        'blocked': (u"Esta conta está bloqueada por 10 minutos devido a quantidade de tentativas de "
+                     u"login sem sucesso em um curto período de tempo."),
+        'user_not_found': (u"Usuário não encontrado.")
+    }
+    mensagem_bloqueio = ''
+    
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                self.user_cache = authenticate(username=username,
+                                               password=password)
+                if self.user_cache is None:
+                    if user_blocked(User.objects.get(username=username)):
+                        raise forms.ValidationError(
+                            self.error_messages['blocked'],
+                            code='blocked',
+                            )
+                    else:
+                        horario = timezone.now()
+                        print 'login failed for: %s at %s' % (username, horario)
+                        # Já foram feitos logins anteriores
+                        if LoginIncorreto.objects.filter(user__username=username).exists():
+                            ultima_tentativa = LoginIncorreto.objects.filter(user__username=username).order_by('-horario')[0]
+                            # Verifica se última tentativa foi feita a menos de 10 minutos
+                            if (horario - ultima_tentativa.horario).total_seconds() < 10 * 60:
+                                LoginIncorreto.objects.create(user=User.objects.get(username=username), horario=horario)
+                                qtd_tentativas = LoginIncorreto.objects.filter(user__username=username).count()
+                                if qtd_tentativas >= 6:
+                                    raise forms.ValidationError(
+                                        self.error_messages['blocked'],
+                                        code='blocked',
+                                        )
+                                    send_mail(
+                                        'Conta bloqueada no Bag of Gold',
+                                        'Devido a quantidade de tentativas de login sem sucesso em um curto espaço de tempo, sua conta no Bag of Gold acaba de ser bloqueada.',
+                                        'do-not-reṕly@bagofgold.com.br',
+                                        [User.objects.get(username=username).email],
+                                        fail_silently=False,
+                                    )
+                                elif qtd_tentativas > 2:
+                                    self.mensagem_bloqueio = u' Você possui mais %s tentativa(s) de login antes da conta ser bloqueada por 10 minutos.' % (6 - qtd_tentativas)
+                            else:
+                                LoginIncorreto.objects.filter(user__username=username).delete()
+                                LoginIncorreto.objects.create(user=User.objects.get(username=username), horario=horario)
+                        else:
+                            LoginIncorreto.objects.create(user=User.objects.get(username=username), horario=horario)
+                        raise forms.ValidationError(
+                            self.error_messages['invalid_login'] + (self.mensagem_bloqueio or ''),
+                            code='invalid_login',
+                            )
+                else:
+                    self.confirm_login_allowed(self.user_cache)
+                    self.confirm_user_not_blocked(self.user_cache)
+            else:
+                raise forms.ValidationError(
+                    self.error_messages['user_not_found'],
+                    code='user_not_found'
+                )
+
+        return self.cleaned_data
+    
+    def confirm_user_not_blocked(self, user):
+        """ Verifica se usuário não está bloqueado """
+        if user_blocked(user):
+            raise forms.ValidationError(
+                self.error_messages['blocked'],
+                code='blocked',
+            )
+        LoginIncorreto.objects.filter(user=user).delete()
