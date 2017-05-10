1347fcb3605111b4bcdbc3965bcf50727bbfb593
Guilherme Henrique <kingbowserii@gmail.com>
2017-05-10 05:24:43 -0300

    Corrigindo erro do apps

--- bagogold/bagogold/apps.py
+++ bagogold/bagogold/apps.py
@@ -2,15 +2,21 @@
 from django.apps import AppConfig
 from django.conf import settings
 from subprocess import check_output
+import os
 import re
  
 class BagofGoldConfig(AppConfig):
     name = 'bagogold.bagogold'
     verbose_name = "Bag of Gold"
     def ready(self):
-        output = check_output(['hg', 'log'])
-        prod = re.findall(r'^branch:\s+prod', output, flags=re.MULTILINE)
-        current_version = '1.0.%s' % (len(prod) - 143)
+        # TODO adicionar -b para pegar branch especifico
+        if settings.ENV == 'PROD':
+            origWD = os.getcwd()
+            os.chdir(settings.PROD_HOME)
+        output = check_output(['hg', 'log', '-b', 'prod', '--template', '.'])
+        if settings.ENV == 'PROD':
+            os.chdir(origWD)
+        current_version = '1.0.%s' % (len(output) - 144)
         if settings.ENV == 'DEV':
             print 'Current Bag of Gold version: ' + current_version
         settings.CURRENT_VERSION = current_version
