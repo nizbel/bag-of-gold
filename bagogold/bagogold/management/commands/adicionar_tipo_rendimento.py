03ad90b3387afd35ce27fcc950cd59fac262d7ed
Guilherme Henrique <kingbowserii@gmail.com>
2017-05-19 02:11:08 -0300

    Alterando funcionamento do adicionar tipo rendimento

--- bagogold/bagogold/management/commands/adicionar_tipo_rendimento.py
+++ bagogold/bagogold/management/commands/adicionar_tipo_rendimento.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = 'TEMPORÁRIO adiciona tipo de rendimento a proventos de FII que não o tenham definido'
 
     def handle(self, *args, **options):
-        for provento in ProventoFII.objects.all():
+        for provento in ProventoFII.gerador_objects.all():
             if provento.tipo_provento not in [tipo_provento for tipo_provento, _ in ProventoFII.ESCOLHAS_TIPO_PROVENTO_FII]:
                 provento.tipo_provento = ProventoFII.TIPO_PROVENTO_RENDIMENTO
                 provento.save()
