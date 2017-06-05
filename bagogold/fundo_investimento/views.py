2297141ce13598d7e41d9de40ef32cb62d8a592b
Guilherme de Deus <kingbowserii@gmail.com>
2017-06-05 23:20:08 +0000

    Iniciando as views para fundo de investimento

--- bagogold/fundo_investimento/views.py
+++ bagogold/fundo_investimento/views.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def detalhar_fundo(request, fundo_id):
+	pass
+
+def editar_operacao(request, operacao_id):
+	pass
+	
+def historico(request):
+	pass
+	
+def inserir_operacao(request):
+	pass
+	
+def listar_fundos(request):
+	pass
+	
+def painel(request):
+	pass
