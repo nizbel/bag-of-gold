347ba123689416f1bbf6569f9e30bcda4a83a01d
Guilherme Henrique <kingbowserii@gmail.com>
2017-06-12 00:17:41 -0300

    Tela buscando fundos de investimento com pagina??o

--- bagogold/fundo_investimento/urls.py
+++ bagogold/fundo_investimento/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^historico/$', views.historico, name='historico_fundo_investimento'),
     url(r'^inserir_operacao_fundo_investimento/$', views.inserir_operacao_fundo_investimento, name='inserir_operacao_fundo_investimento'),
     url(r'^listar_fundos/$', views.listar_fundos, name='listar_fundo_investimento'),
+    url(r'^listar_fundos_por_nome/$', views.listar_fundos_por_nome, name='listar_fundos_por_nome'),
     url(r'^listar_historico_fundo_investimento/(?P<id_fundo>\d+)/$', views.listar_historico_fundo_investimento, name='listar_historico_fundo_investimento'),
     url(r'^painel/$', views.painel, name='painel_fundo_investimento'),
     url(r'^sobre/$', views.sobre, name='sobre_fundo_investimento'),
