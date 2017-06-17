6ee9e2e9265900fd9083d839483b036ff329c1ba
Guilherme Henrique <kingbowserii@gmail.com>
2017-06-17 01:47:59 -0300

    Preparando admin para teste

--- bagogold/fundo_investimento/admin/fundo_investimento.py
+++ bagogold/fundo_investimento/admin/fundo_investimento.py
@@ -4,15 +4,39 @@ from bagogold.fundo_investimento.models import FundoInvestimento,\
     OperacaoFundoInvestimento, Administrador, HistoricoValorCotas,\
     DocumentoCadastro, LinkDocumentoCadastro
 
-admin.site.register(FundoInvestimento)
+class FundoInvestimentoAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'cnpj']
+    list_display = ('nome', 'cnpj', 'administrador', 'data_constituicao', 'situacao', 'classe', 'exclusivo_qualificados', 'ultimo_registro')
+    
+admin.site.register(FundoInvestimento, FundoInvestimentoAdmin)
     
 admin.site.register(OperacaoFundoInvestimento)
     
-admin.site.register(Administrador)
+class AdministradorAdmin(admin.ModelAdmin):
+    search_fields = ['nome', 'cnpj']
+    list_display = ('nome', 'cnpj')
     
-admin.site.register(HistoricoValorCotas)
+admin.site.register(Administrador, AdministradorAdmin)
+    
+class HistoricoValorCotasAdmin(admin.ModelAdmin):
+    search_fields = ['fundo_investimento']
+    list_display = ('fundo_investimento', 'data', 'valor_cota')
+    
+admin.site.register(HistoricoValorCotas, HistoricoValorCotasAdmin)
 
-admin.site.register(DocumentoCadastro)
+class DocumentoCadastroAdmin(admin.ModelAdmin):
+    search_fields = ['data_referencia']
+    list_display = ('data_referencia', 'data_pedido_cvm', 'leitura_realizada')
+    
+admin.site.register(DocumentoCadastro, DocumentoCadastroAdmin)
 
-admin.site.register(LinkDocumentoCadastro)
+class LinkDocumentoCadastroAdmin(admin.ModelAdmin):
+    search_fields = ['documento']
+    list_display = ('documento', 'url', 'data_pedido_cvm')
+    
+    def data_pedido_cvm(self, obj):
+        return obj.documento.data_pedido_cvm
+    data_pedido_cvm.short_description = 'Data de pedido a CVM'
+    
+admin.site.register(LinkDocumentoCadastro, LinkDocumentoCadastroAdmin)
     
