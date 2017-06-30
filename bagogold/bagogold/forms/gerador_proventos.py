0de4c590326536cdaa571b302d60cac373499ff5
Guilherme Henrique <kingbowserii@gmail.com>
2017-06-29 22:01:29 -0300

    Adicionando atualiza??o por selic para leitura de proventos

--- bagogold/bagogold/forms/gerador_proventos.py
+++ bagogold/bagogold/forms/gerador_proventos.py
@@ -4,13 +4,12 @@ from bagogold.bagogold.models.acoes import Provento
 from bagogold.bagogold.models.fii import ProventoFII
 from bagogold.bagogold.models.gerador_proventos import \
     ProventoAcaoDescritoDocumentoBovespa, AcaoProventoAcaoDescritoDocumentoBovespa, \
-    ProventoFIIDescritoDocumentoBovespa
+    ProventoFIIDescritoDocumentoBovespa, SelicProventoAcaoDescritoDocBovespa
 from django import forms
 from django.forms import widgets
 
 
 class ProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
-
     class Meta:
         model = ProventoAcaoDescritoDocumentoBovespa
         fields = ('valor_unitario', 'data_ex', 'data_pagamento', 'tipo_provento',
@@ -30,15 +29,24 @@ class ProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
         return dados
 
 class AcaoProventoAcaoDescritoDocumentoBovespaForm(LocalizedModelForm):
-    
     class Meta:
         model = AcaoProventoAcaoDescritoDocumentoBovespa
         fields = ('acao_recebida', 'data_pagamento_frac', 'valor_calculo_frac')
         widgets={'data_pagamento_frac': widgets.DateInput(attrs={'class':'datepicker', 
                                             'placeholder':'Selecione uma data'})}
 
-class ProventoFIIDescritoDocumentoBovespaForm(LocalizedModelForm):
+class SelicProventoAcaoDescritoDocBovespaForm(LocalizedModelForm):
+    class Meta:
+        model = SelicProventoAcaoDescritoDocBovespa
+        fields = ('data_inicio', 'data_fim',)
+        
+    def clean(self):
+        data = super(SelicProventoAcaoDescritoDocBovespaForm, self).clean()
+        if data.get('data_inicio') is not None and data.get('data_fim') is not None:
+            if data.get('data_inicio') > data.get('data_fim'):
+                raise forms.ValidationError('Data de início da atualização pela Selic deve ser anterior à data final')
 
+class ProventoFIIDescritoDocumentoBovespaForm(LocalizedModelForm):
     class Meta:
         model = ProventoFIIDescritoDocumentoBovespa
         fields = ('valor_unitario', 'data_ex', 'data_pagamento', 'tipo_provento',
