64404c0a179fd5b44fafe542954dab563f82f488
Guilherme Henrique <kingbowserii@gmail.com>
2017-08-06 12:01:21 -0300

    Adicionando hist?ricos de a??o e de FII ao admin

--- bagogold/bagogold/admin/acoes.py
+++ bagogold/bagogold/admin/acoes.py
@@ -30,8 +30,12 @@ admin.site.register(OperacaoAcao)
 admin.site.register(UsoProventosOperacaoAcao)
 
 admin.site.register(OperacaoCompraVenda)
+
+class HistoricoAcaoAdmin(admin.ModelAdmin):
+    search_fields = ['acao_ticker', 'data']
+    lista_display = ('acao', 'preco_unitario', 'data')
     
-admin.site.register(HistoricoAcao)
+admin.site.register(HistoricoAcao, HistoricoAcaoAdmin)
     
 admin.site.register(ValorDiarioAcao)
     
