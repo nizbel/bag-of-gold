f738fef1a6d9f2033b4026da599aba25f99ded15
Guilherme Henrique <kingbowserii@gmail.com>
2017-08-17 15:10:26 -0300

    Preparando inserir criptomoedas

--- bagogold/criptomoeda/forms.py
+++ bagogold/criptomoeda/forms.py
@@ -1,1 +1,29 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.forms.utils import LocalizedModelForm
+from bagogold.criptomoeda.models import OperacaoCriptomoeda
+from django import forms
+from django.forms import widgets
+
+ESCOLHAS_TIPO_OPERACAO=(('C', "Compra"),
+                        ('V', "Venda"))
+
+class OperacaoCriptomoedaForm(LocalizedModelForm):
+    moeda_utilizada = forms.ChoiceField(required=False)
+    
+    class Meta:
+        model = OperacaoCriptomoeda
+        fields = ('tipo_operacao', 'quantidade', 'valor', 'data', 'criptomoeda')
+        widgets={'data': widgets.DateInput(attrs={'class':'datepicker', 
+                                            'placeholder':'Selecione uma data'}),
+                 'tipo_operacao': widgets.Select(choices=ESCOLHAS_TIPO_OPERACAO),}
+        labels = {'criptomoeda': 'Criptomoeda',}
+    
+    class Media:
+        js = ('js/bagogold/form_operacao_criptomoeda.js',)
+        
+    def __init__(self, *args, **kwargs):
+        self.investidor = kwargs.pop('investidor')
+        # first call parent's constructor
+        super(OperacaoCriptomoedaForm, self).__init__(*args, **kwargs)
+        # there's a `fields` property now
+        self.fields['moeda_utilizada'].queryset = OperacaoCriptomoeda.objects.all()
--- bagogold/criptomoeda/urls.py
+++ bagogold/criptomoeda/urls.py
@@ -3,5 +3,14 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^listar_moedas/$', views.listar_moedas, name='listar_moedas'),
+#     url(r'^detalhar_criptomoeda/(?P<id_criptomoeda>\d+)/$', views.detalhar_criptomoeda, name='detalhar_criptomoeda'),
+    url(r'^editar_operacao/(?P<id_operacao>\d+)/$', views.editar_operacao_criptomoeda, name='editar_operacao_criptomoeda'),
+    url(r'^historico/$', views.historico, name='historico_criptomoeda'),
+    url(r'^inserir_operacao_criptomoeda/$', views.inserir_operacao_criptomoeda, name='inserir_operacao_criptomoeda'),
+    url(r'^listar_criptomoedas/$', views.listar_criptomoedas, name='listar_criptomoedas'),
+#     url(r'^listar_criptomoedas_por_nome/$', views.listar_criptomoedas_por_nome, name='listar_criptomoedas_por_nome'),
+#     url(r'^listar_historico_criptomoeda/(?P<id_criptomoeda>\d+)/$', views.listar_historico_criptomoeda, name='listar_historico_criptomoeda'),
+    url(r'^painel/$', views.painel, name='painel_criptomoeda'),
+    url(r'^sobre/$', views.sobre, name='sobre_criptomoeda'),
+#     url(r'^verificar_historico_criptomoeda_na_data/$', views.verificar_historico_criptomoeda_na_data, name='verificar_historico_criptomoeda_na_data'),
     ]
