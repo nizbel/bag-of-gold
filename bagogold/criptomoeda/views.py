7b8bc6b3eba2101959f60051163495179466b33f
Guilherme de Deus <kingbowserii@gmail.com>
2017-08-17 15:10:26 -0300

    Preparando inserir criptomoedas

--- bagogold/criptomoeda/views.py
+++ bagogold/criptomoeda/views.py
@@ -1,23 +1,101 @@
 # -*- coding: utf-8 -*-
+from bagogold import settings
 from bagogold.bagogold.decorators import adiciona_titulo_descricao
-from bagogold.criptomoeda.models import Criptomoeda
+from bagogold.bagogold.forms.divisoes import DivisaoOperacaoCriptomoedaFormSet
+from bagogold.bagogold.models.divisoes import DivisaoOperacaoCriptomoeda, \
+    Divisao
+from bagogold.criptomoeda.forms import OperacaoCriptomoedaForm
+from bagogold.criptomoeda.models import Criptomoeda, OperacaoCriptomoeda
 from decimal import Decimal
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.core.mail import mail_admins
+from django.core.urlresolvers import reverse
+from django.db import transaction
+from django.forms.models import inlineformset_factory
+from django.http.response import HttpResponseRedirect
 from django.template.response import TemplateResponse
 from urllib2 import urlopen
 import json
+import traceback
 
 
 def editar_operacao_criptomoeda(request):
     pass
 
+@login_required
+@adiciona_titulo_descricao('Inserir operação em criptomoedas', 'Inserir registro de operação de compra/venda em criptomoeda')
 def inserir_operacao_criptomoeda(request):
-    pass
+    investidor = request.user.investidor
+    
+    # Preparar formset para divisoes
+    DivisaoCriptomoedaFormSet = inlineformset_factory(OperacaoCriptomoeda, DivisaoOperacaoCriptomoeda, fields=('divisao', 'quantidade'), can_delete=False,
+                                            extra=1, formset=DivisaoOperacaoCriptomoedaFormSet)
+    
+    # Testa se investidor possui mais de uma divisão
+    varias_divisoes = len(Divisao.objects.filter(investidor=investidor)) > 1
+    
+    if request.method == 'POST':
+        form_operacao_criptomoeda = OperacaoCriptomoedaForm(request.POST, investidor=investidor)
+        formset_divisao = DivisaoCriptomoedaFormSet(request.POST, investidor=investidor) if varias_divisoes else None
+        
+        # Validar Fundo de Investimento
+        if form_operacao_criptomoeda.is_valid():
+            operacao_criptomoeda = form_operacao_criptomoeda.save(commit=False)
+            operacao_criptomoeda.investidor = investidor
+            
+            # Testar se várias divisões
+            if varias_divisoes:
+                formset_divisao = DivisaoFundoInvestimentoFormSet(request.POST, instance=operacao_criptomoeda, investidor=investidor)
+                if formset_divisao.is_valid():
+                    try:
+                        with transaction.atomic():
+                            operacao_criptomoeda.save()
+                            formset_divisao.save()
+                            messages.success(request, 'Operação inserida com sucesso')
+                            return HttpResponseRedirect(reverse('criptomoeda:historico_criptomoeda'))
+                    except:
+                        messages.error(request, 'Houve um erro ao inserir a operação')
+                        if settings.ENV == 'DEV':
+                            raise
+                        elif settings.ENV == 'PROD':
+                            mail_admins(u'Erro ao gerar operação em criptomoeda com várias divisões', traceback.format_exc())
+                for erro in formset_divisao.non_form_errors():
+                    messages.error(request, erro)
+            else:
+                try:
+                    with transaction.atomic():
+                        operacao_criptomoeda.save()
+                        divisao_operacao = DivisaoOperacaoCriptomoeda(operacao=operacao_criptomoeda, divisao=investidor.divisaoprincipal.divisao, quantidade=operacao_criptomoeda.quantidade)
+                        divisao_operacao.save()
+                        messages.success(request, 'Operação inserida com sucesso')
+                        return HttpResponseRedirect(reverse('criptomoeda:historico_criptomoeda'))
+                except:
+                    messages.error(request, 'Houve um erro ao inserir a operação')
+                    if settings.ENV == 'DEV':
+                        raise
+                    elif settings.ENV == 'PROD':
+                        mail_admins(u'Erro ao gerar operação em criptomoeda com uma divisão', traceback.format_exc())
+            
+        for erros in form_operacao_criptomoeda.errors.values():
+            for erro in [erro for erro in erros.data if not isinstance(erro, ValidationError)]:
+                messages.error(request, erro.message)
+#                         print '%s %s'  % (divisao_fundo_investimento.quantidade, divisao_fundo_investimento.divisao)
+                
+    else:
+        form_operacao_criptomoeda = OperacaoCriptomoedaForm(investidor=investidor)
+        formset_divisao = DivisaoCriptomoedaFormSet(investidor=investidor)
+    
+    return TemplateResponse(request, 'criptomoedas/inserir_operacao_criptomoeda.html', {'form_operacao_criptomoeda': form_operacao_criptomoeda, \
+                                                                                              'formset_divisao': formset_divisao, 'varias_divisoes': varias_divisoes})
+
 
 def historico(request):
     pass
 
-@adiciona_titulo_descricao('Listar fundos de investimento cadastrados', 'Lista os fundos cadastrados no sistema e suas principais características')
-def listar_moedas(request):
+@adiciona_titulo_descricao('Listar criptomoedas cadastrados', 'Lista as criptomoedas no sistema')
+def listar_criptomoedas(request):
     moedas = Criptomoeda.objects.all()
     
     # Carrega o valor de um dólar em reais, mais atual
