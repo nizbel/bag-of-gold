f68cd64a9e655f6b75d29894ad16de64b0936735
Guilherme de Deus <kingbowserii@gmail.com>
2017-08-18 15:06:06 -0300

    Adicionando templates padrao vindo de fundos de investimento Criando logica de cria??o de opera??o em criptomoedas

--- bagogold/criptomoeda/forms.py
+++ bagogold/criptomoeda/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from bagogold.bagogold.forms.utils import LocalizedModelForm
-from bagogold.criptomoeda.models import OperacaoCriptomoeda
+from bagogold.criptomoeda.models import OperacaoCriptomoeda, Criptomoeda
 from django import forms
 from django.forms import widgets
 
@@ -8,7 +8,11 @@ ESCOLHAS_TIPO_OPERACAO=(('C', "Compra"),
                         ('V', "Venda"))
 
 class OperacaoCriptomoedaForm(LocalizedModelForm):
-    moeda_utilizada = forms.ChoiceField(required=False)
+    escolhas_moeda = (('', 'R$ (Reais)'),)
+    for criptomoeda in Criptomoeda.objects.all():
+        escolhas_moeda += ((criptomoeda.id, '%s (%s)' % (criptomoeda.ticker, criptomoeda.nome)),)
+    print escolhas_moeda
+    moeda_utilizada = forms.ChoiceField(required=False, choices=escolhas_moeda)
     
     class Meta:
         model = OperacaoCriptomoeda
---
+++ bagogold/static/js/bagogold/form_operacao_criptomoeda.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    form_count = $("input[name=divisaooperacaocriptomoeda_set-TOTAL_FORMS]").val();
+    // get extra form count so we know what index to use for the next item.
+    $("#add-another").click(function() {
+        divisao_id = parseInt(form_count) + 1;
+        titulo = $("<div class='col-md-12'><h3 class='font-yellow-crusta' align='center'><strong>Divisão " + divisao_id + "</strong></h3><br/></div>");
+        row = $('#divisao-0').clone();
+        row.attr('id', 'divisao-' + form_count);
+        $(row).find("*").each(function() { 
+       	 if ($(this).is('input')) {
+       		 $(this).val('');
+       	 }
+       	 if ($(this).is('option')) {
+       		 $(this).removeAttr('selected');
+       	 }
+       	 $.each(this.attributes, function() {
+       		 this.value = this.value.replace('_set-0', '_set-' + form_count);
+       	 });
+        });
+        
+        $("#forms").append(titulo);
+        $("#forms").append(row);
+        // build divisao and append it to our forms container
+
+        // increment form count so our view knows to populate 
+        // that many fields for validation
+        form_count ++;
+        $("[name=divisaooperacaocriptomoeda_set-TOTAL_FORMS]").val(form_count);
+        // ready bootstrap elements
+        row.find('.bootstrap-select').replaceWith(function() { return $('select', this); });
+        row.find('select').selectpicker('refresh');
+        row.find('.bootstrap-touchspin').find('*').not('input').remove();
+        row.find("input[name$='-quantidade']").TouchSpin({
+       	 	initval: 0,
+    		min: 0,
+    		max: 1000000000,
+    		step: 0.000000000000001,
+    		decimals: 15,
+    		maxboostedstep: 100,
+    		postfix: 'criptomoedas'
+        });
+    });
+    
+    $("input[name='valor']").TouchSpin({
+		min: 0.000000000001,
+		max: 1000000000,
+		step: 0.000000000001,
+		decimals: 12,
+		maxboostedstep: 100,
+		prefix: 'R$'
+	});
+	$("input[name='quantidade']").TouchSpin({
+		min: 0.000000000000001,
+		max: 1000000000,
+		step: 0.000000000000001,
+		decimals: 15,
+		maxboostedstep: 100,
+		postfix: 'criptomoedas'
+	});
+	$("input[name$='-quantidade']").TouchSpin({
+		min: 0,
+		max: 1000000000,
+		step: 0.000000000000001,
+		decimals: 15,
+		maxboostedstep: 100,
+		postfix: 'criptomoedas'
+	});
+	
+
+    $('#id_moeda_utilizada').change(function() {
+    	$("input[name='valor']").trigger("touchspin.updatesettings", {postfix: 'BTC'});
+    });
+});
