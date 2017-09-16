34a363a98c9df0d63a2de5e3404b0f20c43cdfff
Guilherme Henrique <kingbowserii@gmail.com>
2017-09-16 20:18:47 -0300

    Removendo datetimepicker

--- bagogold/outros_investimentos/forms.py
+++ bagogold/outros_investimentos/forms.py
@@ -4,13 +4,14 @@ from bagogold.outros_investimentos.models import Investimento, Rendimento
 from django import forms
 from django.forms import widgets
 
-ESCOLHAS_TIPO_OPERACAO=(('C', "Compra"),
-                        ('V', "Venda"))
-
 class InvestimentoForm(LocalizedModelForm):
+    taxa = forms.DecimalField()
     class Meta:
         model = Investimento
         fields = ('nome', 'descricao', 'quantidade', 'data')
+    
+    class Media:
+        js = ('js/bagogold/form_investimento.js',)
         
     def __init__(self, *args, **kwargs):
         self.investidor = kwargs.pop('investidor')
