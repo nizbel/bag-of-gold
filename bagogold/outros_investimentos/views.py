429cf3b4ceb49fac1255c37b380dbbdc638b2ac5
Guilherme Henrique <kingbowserii@gmail.com>
2017-09-16 20:22:28 -0300

    Preparando tela de inserir investimentos Adicionando divis?o de investimento Adicionando menu de outros investimentos

--- bagogold/outros_investimentos/views.py
+++ bagogold/outros_investimentos/views.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
+from bagogold import settings
 from bagogold.bagogold.decorators import adiciona_titulo_descricao
-from bagogold.outros_investimentos.models import Investimento
+from bagogold.bagogold.forms.divisoes import DivisaoInvestimentoFormSet
+from bagogold.bagogold.models.divisoes import DivisaoInvestimento, Divisao
+from bagogold.outros_investimentos.forms import InvestimentoForm
+from bagogold.outros_investimentos.models import Investimento, InvestimentoTaxa
 from bagogold.outros_investimentos.utils import \
     calcular_valor_outros_investimentos_ate_data
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.mail import mail_admins
+from django.core.urlresolvers import reverse
+from django.db import transaction
+from django.forms.models import inlineformset_factory
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
+import traceback
+
+def detalhar_investimento(request, id_investimento):
+    
+    pass
 
 def editar_investimento(request):
     pass
@@ -12,9 +27,75 @@ def editar_investimento(request):
 def historico(request):
     pass
 
+@login_required
+@adiciona_titulo_descricao('Inserir investimento', 'Inserir registro de investimento')
 def inserir_investimento(request):
-    pass
-
+    investidor = request.user.investidor
+    
+    # Preparar formset para divisoes
+    DivisaoOutrosInvestimentosFormSet = inlineformset_factory(Investimento, DivisaoInvestimento, fields=('divisao', 'quantidade'), can_delete=False,
+                                            extra=1, formset=DivisaoInvestimentoFormSet)
+    
+    # Testa se investidor possui mais de uma divisão
+    varias_divisoes = len(Divisao.objects.filter(investidor=investidor)) > 1
+    
+    if request.method == 'POST':
+        form_outros_invest = InvestimentoForm(request.POST, investidor=investidor)
+        formset_divisao = DivisaoOutrosInvestimentosFormSet(request.POST, investidor=investidor) if varias_divisoes else None
+        
+        # Validar operação
+        if form_outros_invest.is_valid():
+            investimento = form_outros_invest.save(commit=False)
+            investimento.investidor = investidor
+                
+            # Testar se várias divisões
+            if varias_divisoes:
+                formset_divisao = DivisaoInvestimentoFormSet(request.POST, instance=investimento, investidor=investidor)
+                if formset_divisao.is_valid():
+                    try:
+                        with transaction.atomic():
+                            investimento.save()
+                            if form_outros_invest.cleaned_data['taxa'] > 0:
+                                InvestimentoTaxa.objects.create(investimento=investimento, valor=form_outros_invest.cleaned_data['taxa'])
+                            formset_divisao.save()
+                            messages.success(request, 'Investimento inserido com sucesso')
+                            return HttpResponseRedirect(reverse('outros_investimentos:historico_outros_invest'))
+                    except:
+                        messages.error(request, 'Houve um erro ao inserir o investimento')
+                        if settings.ENV == 'DEV':
+                            raise
+                        elif settings.ENV == 'PROD':
+                            mail_admins(u'Erro ao gerar investimento na seção outros investimentos, com várias divisões', traceback.format_exc())
+                for erro in formset_divisao.non_form_errors():
+                    messages.error(request, erro)
+            else:
+                try:
+                    with transaction.atomic():
+                        investimento.save()
+                        if form_outros_invest.cleaned_data['taxa'] > 0:
+                            InvestimentoTaxa.objects.create(investimento=investimento, valor=form_outros_invest.cleaned_data['taxa'])
+                        divisao_operacao = DivisaoInvestimento(investimento=investimento, divisao=investidor.divisaoprincipal.divisao, 
+                                                               quantidade=investimento.quantidade)
+                        divisao_operacao.save()
+                        messages.success(request, 'Investimento inserido com sucesso')
+                        return HttpResponseRedirect(reverse('outros_investimentos:historico_outros_invest'))
+                except:
+                    messages.error(request, 'Houve um erro ao inserir o investimento')
+                    if settings.ENV == 'DEV':
+                        raise
+                    elif settings.ENV == 'PROD':
+                        mail_admins(u'Erro ao gerar investimento na seção outros investimentos, com uma divisão', traceback.format_exc())
+            
+        for erro in [erro for erro in form_outros_invest.non_field_errors()]:
+            messages.error(request, erro)
+#                         print '%s %s'  % (divisao_fundo_investimento.quantidade, divisao_fundo_investimento.divisao)
+                
+    else:
+        form_outros_invest = InvestimentoForm(investidor=investidor)
+        formset_divisao = DivisaoOutrosInvestimentosFormSet(investidor=investidor)
+    
+    return TemplateResponse(request, 'outros_investimentos/inserir_investimento.html', {'form_outros_invest': form_outros_invest, \
+                                                                                              'formset_divisao': formset_divisao, 'varias_divisoes': varias_divisoes})
 
 @adiciona_titulo_descricao('Listar outros investimentos', 'Lista de investimentos cadastrados pelo investidor')
 def listar_investimentos(request):
@@ -36,3 +117,8 @@ def painel(request):
     investimentos = Investimento.objects.filter(id__in=qtd_investimentos.keys())
     
     return TemplateResponse(request, 'outros_investimentos/painel.html', {'investimentos': investimentos})
+
+@adiciona_titulo_descricao('', '')
+def sobre(request):
+    
+    return TemplateResponse(request, 'outros_investimentos/sobre.html', {})
---
+++ bagogold/static/js/bagogold/form_investimento.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+	form_count = $("input[name=divisaoinvestimento_set-TOTAL_FORMS]").val();
+    // get extra form count so we know what index to use for the next item.
+     $("#add-another").click(function() {
+         divisao_id = parseInt(form_count) + 1;
+         titulo = $("<div class='col-md-12'><h3 class='font-yellow-crusta' align='center'><strong>Divisão " + divisao_id + "</strong></h3><br/></div>");
+         row = $('#divisao-0').clone();
+         row.attr('id', 'divisao-' + form_count);
+         $(row).find("*").each(function() { 
+        	 if ($(this).is('input')) {
+        		 $(this).val('');
+        	 }
+        	 if ($(this).is('option')) {
+        		 $(this).removeAttr('selected');
+        	 }
+        	 $.each(this.attributes, function() {
+        		 this.value = this.value.replace('_set-0', '_set-' + form_count);
+        	 });
+         });
+         
+         $("#forms").append(titulo);
+         $("#forms").append(row);
+         // build divisao and append it to our forms container
+
+         // increment form count so our view knows to populate 
+         // that many fields for validation
+         form_count ++;
+         $("[name=divisaoinvestimento_set-TOTAL_FORMS]").val(form_count);
+         // ready bootstrap elements
+         row.find('.bootstrap-select').replaceWith(function() { return $('select', this); });
+         row.find('select').selectpicker('refresh');
+         row.find('.bootstrap-touchspin').find('*').not('input').remove();
+         row.find("input[name$='-quantidade']").TouchSpin({
+        	 initval: 0,
+             min: 0,
+             max: 1000000000,
+             step: 0.01,
+             decimals: 2,
+             maxboostedstep: 100,
+             prefix: 'R$'
+         });
+     });
+
+     // Preparar campos
+ 	$("input[name='quantidade']").TouchSpin({
+ 		initval: 0.01,
+ 		min: 0.01,
+ 		max: 999999999,
+        step: 0.01,
+        decimals: 2,
+        maxboostedstep: 100,
+        prefix: 'R$'
+ 	});
+ 	$("input[name$='-quantidade']").TouchSpin({
+ 		min: 0,
+ 		max: 999999999,
+ 		step: 0.01,
+ 		decimals: 2,
+ 		maxboostedstep: 100,
+ 		prefix: 'R$'
+ 	});
+ 	$('.date-picker').datepicker({
+ 	   language: 'pt-BR'
+ 	});
+     $("select[id$='-divisao']").selectpicker('refresh');
+	$("input[name='descricao']").maxlength();
+
+});
