7f3f6665a3af48e83b1b40cce63cfb1951070e70
Guilherme de Deus <kingbowserii@gmail.com>
2017-09-15 21:16:11 -0300

    Alterando formato dos investimentos

--- bagogold/outros_investimentos/models.py
+++ bagogold/outros_investimentos/models.py
@@ -8,6 +8,8 @@ class Investimento (models.Model):
     descricao = models.CharField(u'Descrição', max_length=100, blank=True, null=True)
     investidor = models.ForeignKey('bagogold.Investidor', related_name='outros_investimentos')
     encerrado = models.BooleanField(u'Encerrado?', default=False)
+    quantidade = models.DecimalField(u'Quantidade', max_digits=11, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+    data = models.DateField(u'Data do investimento')
     
     class Meta:
         unique_together=('nome', 'investidor',)
@@ -15,14 +17,8 @@ class Investimento (models.Model):
     def __unicode__(self):
         return self.nome
     
-class OperacaoInvestimento (models.Model):
-    quantidade = models.DecimalField(u'Quantidade', max_digits=11, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
-    data = models.DateField(u'Data da operação')
-    tipo_operacao = models.CharField(u'Tipo de operação', max_length=1)
-    investimento = models.ForeignKey('Investimento')
-    
-class OperacaoInvestimentoTaxa (models.Model):
-    operacao = models.OneToOneField('OperacaoInvestimento')
+class InvestimentoTaxa (models.Model):
+    operacao = models.OneToOneField('Investimento')
     valor = models.DecimalField(u'Valor da taxa', max_digits=9, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     
 class Rendimento (models.Model):
@@ -32,8 +28,8 @@ class Rendimento (models.Model):
     
 class PeriodoRendimentos (models.Model):
     investimento = models.ForeignKey('Investimento')
-    tipo_periodo = models.CharField(u'Tipo de período')
+    tipo_periodo = models.CharField(u'Tipo de período', max_length=1)
     qtd_periodo = models.IntegerField(u'Quantidade do período')
     valor_rendimento = models.DecimalField(u'Valor do rendimento', max_digits=9, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
-    inicio_vigencia = models.DateField(u'Data da operação')
-    fim_vigencia = models.DateField(u'Data da operação')
+    inicio_vigencia = models.DateField(u'Início da vigência')
+    fim_vigencia = models.DateField(u'Fim da vigência')
