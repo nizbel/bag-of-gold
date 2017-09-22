c7ae9f5e567e90cf718057d5871656cb48d2ea32
Guilherme Henrique <kingbowserii@gmail.com>
2017-09-21 23:30:51 -0300

    Testes de quantidade incluindo amortizacoes feitos, falta apenas o por divisao

--- bagogold/outros_investimentos/tests/test_quantidades.py
+++ bagogold/outros_investimentos/tests/test_quantidades.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from bagogold.outros_investimentos.models import Investimento, Amortizacao
 from bagogold.outros_investimentos.utils import \
-    calcular_valor_outros_investimentos_ate_data
+    calcular_valor_outros_investimentos_ate_data, \
+    calcular_valor_outros_investimentos_ate_data_por_investimento
 from decimal import Decimal
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -20,14 +21,47 @@ class QuantidadesOutrosInvestimentosTestCase(TestCase):
 
         amortizacao_1 = Amortizacao.objects.create(investimento=investimento_1, data=datetime.date(2017, 4, 20), valor=Decimal('333.33'))
         
+        amortizacao_2 = Amortizacao.objects.create(investimento=investimento_1, data=datetime.date(2017, 5, 20), valor=Decimal('333.33'))
+        
+        amortizacao_3 = Amortizacao.objects.create(investimento=investimento_2, data=datetime.date(2017, 6, 20), valor=Decimal(2000))
+        
     def test_quantidades_ate_dia(self):
         """Testa quantidades de investimentos até dias determinados"""
         investidor = User.objects.get(username='tester').investidor
+        investimento_1 = Investimento.objects.get(nome='Investimento 1')
+        investimento_2 = Investimento.objects.get(nome='Investimento 2')
+        
         qtd_apos_investimento_1 = calcular_valor_outros_investimentos_ate_data(investidor, datetime.date(2017, 3, 2))
-        self.assertDictEqual(qtd_apos_investimento_1, {1: Decimal(1000)})
+        self.assertDictEqual(qtd_apos_investimento_1, {investimento_1.id: Decimal(1000)})
         
         qtd_apos_investimento_2 = calcular_valor_outros_investimentos_ate_data(investidor, datetime.date(2017, 4, 2))
-        self.assertDictEqual(qtd_apos_investimento_2, {1: Decimal(1000), 2: Decimal(2000)})
+        self.assertDictEqual(qtd_apos_investimento_2, {investimento_1.id: Decimal(1000), investimento_2.id: Decimal(2000)})
         
         qtd_apos_amortizacao_1 = calcular_valor_outros_investimentos_ate_data(investidor, datetime.date(2017, 5, 2))
-        self.assertDictEqual(qtd_apos_amortizacao_1, {1: Decimal('666.67'), 2: Decimal(2000)})
+        self.assertDictEqual(qtd_apos_amortizacao_1, {investimento_1.id: Decimal('666.67'), investimento_2.id: Decimal(2000)})
+        
+        qtd_apos_amortizacao_2 = calcular_valor_outros_investimentos_ate_data(investidor, datetime.date(2017, 5, 21))
+        self.assertDictEqual(qtd_apos_amortizacao_2, {investimento_1.id: Decimal('333.34'), investimento_2.id: Decimal(2000)})
+        
+        qtd_apos_amortizacao_3 = calcular_valor_outros_investimentos_ate_data(investidor, datetime.date(2017, 6, 21))
+        self.assertDictEqual(qtd_apos_amortizacao_3, {investimento_1.id: Decimal('333.34')})
+        
+    def test_quantidade_ate_dia_por_investimento(self):
+        """Testa quantidades investida em investimento determinado até dia"""
+        investidor = User.objects.get(username='tester').investidor
+        investimento_1 = Investimento.objects.get(nome='Investimento 1')
+        qtd_antes_amortizacao_1 = calcular_valor_outros_investimentos_ate_data_por_investimento(investimento_1, datetime.date(2017, 3, 20))
+        self.assertEqual(qtd_antes_amortizacao_1, Decimal(1000))
+        
+        qtd_apos_amortizacao_1 = calcular_valor_outros_investimentos_ate_data_por_investimento(investimento_1, datetime.date(2017, 4, 21))
+        self.assertEqual(qtd_apos_amortizacao_1, Decimal('666.67'))
+        
+        qtd_apos_amortizacao_2 = calcular_valor_outros_investimentos_ate_data_por_investimento(investimento_1, datetime.date(2017, 5, 21))
+        self.assertEqual(qtd_apos_amortizacao_2, Decimal('333.34'))
+        
+        investimento_2 = Investimento.objects.get(nome='Investimento 2')
+        qtd_antes_amortizacao_3 = calcular_valor_outros_investimentos_ate_data_por_investimento(investimento_2, datetime.date(2017, 4, 2))
+        self.assertEqual(qtd_antes_amortizacao_3, Decimal(2000))
+        
+        qtd_apos_amortizacao_3 = calcular_valor_outros_investimentos_ate_data_por_investimento(investimento_2, datetime.date(2017, 6, 21))
+        self.assertEqual(qtd_apos_amortizacao_3, 0)
--- bagogold/outros_investimentos/utils.py
+++ bagogold/outros_investimentos/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from bagogold.outros_investimentos.models import Investimento, Amortizacao
+from django.db.models.aggregates import Sum
 import datetime
-from bagogold.outros_investimentos.models import Investimento
 
 def calcular_valor_outros_investimentos_ate_data(investidor, data=datetime.date.today()):
     """
@@ -10,7 +11,22 @@ def calcular_valor_outros_investimentos_ate_data(investidor, data=datetime.date.
     Retorno: Valores por investimento {investimento_id: valor}
     """
     investimentos = dict(Investimento.objects.filter(investidor=investidor, data__lte=data, data_encerramento__isnull=True).values_list('id', 'quantidade'))
+
+    amortizacoes = dict(Amortizacao.objects.filter(investimento__investidor=investidor, data__lte=data, investimento__data_encerramento__isnull=True) \
+                        .values('investimento__id').annotate(total_amortizado=Sum('valor')).values_list('investimento__id', 'total_amortizado'))
     
-    # TODO remover amortizações
-    
-    return investimentos
+    qtd_investimentos = { k: investimentos.get(k, 0) - amortizacoes.get(k, 0) for k in set(investimentos) | set(amortizacoes) \
+                         if investimentos.get(k, 0) - amortizacoes.get(k, 0) > 0 }
+    return qtd_investimentos
+
+def calcular_valor_outros_investimentos_ate_data_por_investimento(investimento, data=datetime.date.today()):
+    """
+    Calcula o valor investido em um investimento até determinada data
+    Parâmetros: Investimento
+                Data
+    Retorno: Quantidade investida
+    """
+    total_investido = investimento.quantidade - (Amortizacao.objects.filter(investimento=investimento, data__lte=data) \
+        .aggregate(total_amortizado=Sum('valor'))['total_amortizado'] or 0)
+        
+    return total_investido
