cf441bb7a9feb044302cb6b6fe2c949fa3a1a4d7
Guilherme Henrique <kingbowserii@gmail.com>
2017-09-28 00:03:18 -0300

    Encerramento de investimento OK Corrigidos erros nas edi??es de rendimento, amortiza??o e investimento

--- bagogold/outros_investimentos/forms.py
+++ bagogold/outros_investimentos/forms.py
@@ -4,6 +4,7 @@ from bagogold.outros_investimentos.models import Investimento, Rendimento, \
     Amortizacao
 from django import forms
 from django.forms import widgets
+import datetime
 
 class InvestimentoForm(LocalizedModelForm):
     taxa = forms.DecimalField(min_value=0,  max_digits=9, decimal_places=2)
@@ -55,7 +56,6 @@ class AmortizacaoForm(LocalizedModelForm):
         if Amortizacao.objects.filter(investimento=cleaned_data.get('investimento'), data=cleaned_data.get('data')).exists() \
             and Amortizacao.objects.get(investimento=cleaned_data.get('investimento'), data=cleaned_data.get('data')).investimento.id != self.investimento.id:
             raise forms.ValidationError('Já existe uma amortização para essa data')
-        return cleaned_data
 
 class RendimentoForm(LocalizedModelForm):
     class Meta:
@@ -92,7 +92,6 @@ class RendimentoForm(LocalizedModelForm):
         if Rendimento.objects.filter(investimento=cleaned_data.get('investimento'), data=cleaned_data.get('data')).exists() \
             and Rendimento.objects.get(investimento=cleaned_data.get('investimento'), data=cleaned_data.get('data')).investimento.id != self.investimento.id:
             raise forms.ValidationError('Já existe um rendimento para essa data')
-        return cleaned_data
         
         
 class EncerramentoForm(LocalizedModelForm):
@@ -101,24 +100,31 @@ class EncerramentoForm(LocalizedModelForm):
     class Meta:
         model = Investimento
         fields = ('data_encerramento',)
-        widgets={'data': widgets.DateInput(attrs={'class':'datepicker', 
+        widgets={'data_encerramento': widgets.DateInput(attrs={'class':'datepicker', 
                                             'placeholder':'Selecione uma data'})}
         
     def __init__(self, *args, **kwargs):
         self.investidor = kwargs.pop('investidor')
         # first call parent's constructor
-        super(RendimentoForm, self).__init__(*args, **kwargs)
+        super(EncerramentoForm, self).__init__(*args, **kwargs)
+        self.fields['data_encerramento'].required = True
         self.fields['amortizacao'].required = False
+        self.fields['amortizacao'].label = u'Amortização'
         
     def clean_amortizacao(self):
+        print 'oi2'
         valor = self.cleaned_data['amortizacao']
-        if valor <= 0:
-            raise forms.ValidationError('Valor da amortização deve ser maior que zero')
+        if valor < 0:
+            raise forms.ValidationError('Valor da amortização deve ser pelo menos igual a zero')
         return valor
     
     def clean(self):
-        cleaned_data = super(RendimentoForm, self).clean()
-        if self.instance.data <= cleaned_data.get('data_encerramento'):
-            raise forms.ValidationError('Data de encerramento deve ser posterior à data de início do investimento')
-        return cleaned_data
+        cleaned_data = super(EncerramentoForm, self).clean()
+        if cleaned_data.get('data_encerramento'):
+#             data_encerramento = datetime.datetime.strptime(cleaned_data.get('data_encerramento'), '%d/%m/%Y')
+            if self.instance.data >= cleaned_data.get('data_encerramento'):
+                raise forms.ValidationError('Data de encerramento deve ser posterior à data de início do investimento, %s' % (self.instance.data.strftime('%d/%m/%Y')))
+            # Verificar se não há amortização posterior ao fim do rendimento
+            if self.instance.amortizacao_set.filter(data__gt=cleaned_data.get('data_encerramento')).exists():
+                raise forms.ValidationError('Há uma data de amortização cadastrada em data posterior a data de encerramento informada')
         
