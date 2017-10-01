e5174a57048c5d50c658ef8b9e0737aa88e9d9fa
Guilherme Henrique <kingbowserii@gmail.com>
2017-10-01 19:13:33 -0300

    Removendo print

--- bagogold/cdb_rdb/utils.py
+++ bagogold/cdb_rdb/utils.py
@@ -133,6 +133,4 @@ def buscar_operacoes_vigentes_ate_data(investidor, data=datetime.date.today()):
         .annotate(qtd_vendida=Coalesce(Sum(Case(When(operacao_compra__operacao_venda__data__lt=data, then='operacao_compra__operacao_venda__quantidade'))), 0)).exclude(quantidade=F('qtd_vendida')) \
         .annotate(qtd_disponivel_venda=(F('quantidade') - F('qtd_vendida')))
 
-    print operacoes
-
     return operacoes
