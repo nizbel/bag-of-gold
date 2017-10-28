f9bd28f44ae2ad6aa6e8e3b69cd6078a1e246b6b
Guilherme Henrique <kingbowserii@gmail.com>
2017-10-28 16:54:00 -0200

    Preparando ideia para testes de management commands

--- bagogold/bagogold/management/commands/preencher_historico_ano_atual_td.py
+++ bagogold/bagogold/management/commands/preencher_historico_ano_atual_td.py
@@ -29,6 +29,9 @@ class Command(BaseCommand):
                     titulo.save()
         except:
             if settings.ENV == 'DEV':
-                print traceback.format_exc()
+                if options['test']:
+                    raise
+                else:
+                    print traceback.format_exc()
             elif settings.ENV == 'PROD':
                 mail_admins(u'Erro em Preencher histórico para TD no ano atual', traceback.format_exc().decode('utf-8'))
