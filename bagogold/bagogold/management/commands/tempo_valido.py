8fbba211471a148e1c21b51de16f607ead860504
Guilherme de Deus <kingbowserii@gmail.com>
2017-10-28 20:01:55 -0200

    Removendo slicing

--- bagogold/bagogold/management/commands/tempo_valido.py
+++ bagogold/bagogold/management/commands/tempo_valido.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         leituras = InvestidorLeituraDocumento.objects.filter(data_leitura__date=datetime.date(2017, 10, 20)) \
-            .filter(data_leitura__gt=datetime.datetime(2017, 10, 20, 19, 10)).order_by('data_leitura')[1:]
+            .filter(data_leitura__gt=datetime.datetime(2017, 10, 20, 19, 10)).order_by('data_leitura')
         leituras_fii = leituras.filter(documento__tipo='F')
         leituras_acao = leituras.filter(documento__tipo='A')
         
