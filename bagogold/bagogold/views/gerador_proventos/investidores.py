7f14ea48a96ee9049f6f048b4990ec7776b2a46c
Guilherme de Deus <kingbowserii@gmail.com>
2017-11-10 23:00:40 +0000

    Preparando novo formato de acompanhamento de pagamentos na view

--- bagogold/bagogold/views/gerador_proventos/investidores.py
+++ bagogold/bagogold/views/gerador_proventos/investidores.py
@@ -98,13 +98,13 @@ def detalhar_pendencias_usuario(request, id_usuario):
         
         # TODO usar novo modelo pagamento leitura
         valor_hora = Decimal(25)
-        usuario.pago = 0
+        usuario.pago = LeituraDocumento.objects.filter(investidor=usuario.investidor).aggregate(total_pago=Sum('valor'))['total_pago'] or 0
         usuario.a_pagar = Decimal(floor(usuario.tempo_validado)) * valor_hora - usuario.pago
         
         # Parar popular a barra de acompanhamento
-        usuario.progresso_tempo_total = tempo_total * valor_hora
-        usuario.progresso_pago = usuario.pago
-        usuario.percentual_progresso_pago = usuario.progresso_pago / usuario.progresso_tempo_total * 100
+        usuario.progresso_tempo_total = tempo_total * valor_hora - usuario.pago
+#         usuario.progresso_pago = usuario.pago
+#         usuario.percentual_progresso_pago = usuario.progresso_pago / usuario.progresso_tempo_total * 100
         usuario.progresso_a_pagar = usuario.a_pagar
         usuario.percentual_progresso_a_pagar = usuario.progresso_a_pagar / usuario.progresso_tempo_total * 100
         usuario.progresso_validado = usuario.tempo_validado * valor_hora - usuario.a_pagar - usuario.pago
