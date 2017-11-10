e841838a181a1f5d9ef53bdf80ad7a9c9fa11351
Guilherme de Deus <kingbowserii@gmail.com>
2017-11-10 23:18:30 +0000

    Utilizando constante com o valor da hora e preparando para adicionar lista de pagamentos

--- bagogold/bagogold/views/gerador_proventos/investidores.py
+++ bagogold/bagogold/views/gerador_proventos/investidores.py
@@ -97,8 +97,8 @@ def detalhar_pendencias_usuario(request, id_usuario):
         usuario.tempo_a_validar = tempo_total - usuario.tempo_validado
         
         # TODO usar novo modelo pagamento leitura
-        valor_hora = Decimal(25)
-        usuario.pago = LeituraDocumento.objects.filter(investidor=usuario.investidor).aggregate(total_pago=Sum('valor'))['total_pago'] or 0
+        valor_hora = PagamentoLeitura.VALOR_HORA
+        usuario.pago = PagamentoLeitura.objects.filter(investidor=usuario.investidor).aggregate(total_pago=Sum('valor'))['total_pago'] or 0
         usuario.a_pagar = Decimal(floor(usuario.tempo_validado)) * valor_hora - usuario.pago
         
         # Parar popular a barra de acompanhamento
@@ -109,6 +109,8 @@ def detalhar_pendencias_usuario(request, id_usuario):
         usuario.percentual_progresso_a_pagar = usuario.progresso_a_pagar / usuario.progresso_tempo_total * 100
         usuario.progresso_validado = usuario.tempo_validado * valor_hora - usuario.a_pagar - usuario.pago
         usuario.percentual_progresso_validado = usuario.progresso_validado / usuario.progresso_tempo_total * 100
+        
+        # TODO Adicionar lista de pagamentos feitos
 
     return TemplateResponse(request, 'gerador_proventos/detalhar_pendencias_usuario.html', {'usuario': usuario, 'graf_leituras': graf_leituras, 'graf_validacoes': graf_validacoes,
                                                                                             'graf_leituras_que_recusou': graf_leituras_que_recusou, 'graf_leituras_recusadas': graf_leituras_recusadas})
