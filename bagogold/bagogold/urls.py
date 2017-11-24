5746ed95d67c118224d90948446a744ee181e8e8
Guilherme de Deus <kingbowserii@gmail.com>
2017-11-24 22:10:35 +0000

    Adicionando urls para listagem e detalhamento de proventos de FIIs

--- bagogold/bagogold/urls.py
+++ bagogold/bagogold/urls.py
@@ -90,9 +90,11 @@ fiis_patterns = [
 #     url(r'^acompanhamento_mensal/$', views.fii.fii.acompanhamento_mensal_fii, name='acompanhamento_mensal_fii'),
     url(r'^acompanhamento/$', views.fii.fii.acompanhamento_fii, name='acompanhamento_fii'),
     url(r'^calcular_resultado_corretagem/$', views.fii.fii.calcular_resultado_corretagem, name='calcular_resultado_corretagem'),
+    url(r'^detalhar_provento/(?P<provento_id>\d+)/$', views.fii.fii.detalhar_provento, name='detalhar_provento_fii'),
     url(r'^editar_operacao/(?P<operacao_id>\d+)/$', views.fii.fii.editar_operacao_fii, name='editar_operacao_fii'),
     url(r'^historico/$', views.fii.fii.historico_fii, name='historico_fii'),
     url(r'^inserir_operacao_fii/$', views.fii.fii.inserir_operacao_fii, name='inserir_operacao_fii'),
+    url(r'^listar_proventos/$', views.fii.fii.listar_proventos, name='listar_proventos_fii'),
     url(r'^painel/$', views.fii.fii.painel, name='painel_fii'),
     url(r'^sobre/$', views.fii.fii.sobre, name='sobre_fii'),
     ]
