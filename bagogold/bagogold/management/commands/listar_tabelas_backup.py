2612810e279d406a80c418e64bb1fbc54f399b95
Guilherme de Deus <kingbowserii@gmail.com>
2017-11-28 23:39:39 +0000

    Preparando listar tabelas para englobar db_dump ou pelo menos alterar as tabelas a fazer backup

--- bagogold/bagogold/management/commands/listar_tabelas_backup.py
+++ bagogold/bagogold/management/commands/listar_tabelas_backup.py
@@ -9,9 +9,17 @@ class Command(BaseCommand):
     help = 'Lista tabelas para backup'
 
     def handle(self, *args, **options):
-        cursor = connection.cursor()
-        lista_tabelas = connection.introspection.get_table_list(cursor)
-        lista_tabelas = [tabela.name for tabela in lista_tabelas if tabela.type == 't' and tabela.name not in TABELAS_SEM_BACKUP]
-        str_lista = ['--table "public.%s"' % (tabela) for tabela in sorted(lista_tabelas)]
-        str_lista = ' '.join(str_lista)
-        print str_lista
+        str_tabelas = buscar_tabelas_string()
+        # TODO testar ambiente
+        
+        # Alterar db_dump.sh correspondente
+        
+        # Verificar se é possível chamar o db_dump por subprocess
+
+def buscar_tabelas_string():
+    cursor = connection.cursor()
+    lista_tabelas = connection.introspection.get_table_list(cursor)
+    lista_tabelas = [tabela.name for tabela in lista_tabelas if tabela.type == 't' and tabela.name not in TABELAS_SEM_BACKUP]
+    str_lista = ['--table "public.%s"' % (tabela) for tabela in sorted(lista_tabelas)]
+    str_lista = ' '.join(str_lista)
+    return str_lista
