5327976f9ee2c74ca033053dd3f92958b1c61477
Guilherme Henrique <kingbowserii@gmail.com>
2017-11-29 13:15:03 -0200

    Trazendo js para pr?ximo do min.js

--- bagogold/static/assets/pages/scripts/table-datatables-fixedheader.js
+++ bagogold/static/assets/pages/scripts/table-datatables-fixedheader.js
@@ -28,13 +28,13 @@ var TableDatatablesFixedHeader = function () {
                     "sortAscending": ": activate to sort column ascending",
                     "sortDescending": ": activate to sort column descending"
                 },
-                "emptyTable": "No data available in table",
-                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-                "infoEmpty": "No entries found",
-                "infoFiltered": "(filtered1 from _MAX_ total entries)",
-                "lengthMenu": "_MENU_ entries",
-                "search": "Search:",
-                "zeroRecords": "No matching records found"
+                "emptyTable": "Nenhum dado disponível",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ itens",
+                "infoEmpty": "Nenhum resultado encontrado",
+                "infoFiltered": "(total de _MAX_ itens )",
+                "lengthMenu": "_MENU_ itens",
+                "search": "Busca:",
+                "zeroRecords": "Nenhum registro correspondente encontrado"
             },
 
             // Or you can use remote translation file
@@ -54,7 +54,7 @@ var TableDatatablesFixedHeader = function () {
             
             "lengthMenu": [
                 [5, 10, 15, 20, -1],
-                [5, 10, 15, 20, "All"] // change per page values here
+                [5, 10, 15, 20, "Todos"] // change per page values here
             ],
             // set the initial value
             "pageLength": 20,
