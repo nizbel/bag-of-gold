8d8a2490aa431a9320b9f8549d61f36a049ef98e
Guilherme de Deus <kingbowserii@gmail.com>
2018-01-10 18:05:04 -0200

    Preparando form de opera??es em lote, testando required nos inputs do form

--- bagogold/criptomoeda/urls.py
+++ bagogold/criptomoeda/urls.py
@@ -4,15 +4,15 @@ import views
 
 urlpatterns = [
 #     url(r'^detalhar_criptomoeda/(?P<id_criptomoeda>\d+)/$', views.detalhar_criptomoeda, name='detalhar_criptomoeda'),
-    url(r'^editar_operacao/(?P<id_operacao>\d+)/$', views.editar_operacao_criptomoeda, name='editar_operacao_criptomoeda'),
-    url(r'^editar_transferencia/(?P<id_transferencia>\d+)/$', views.editar_transferencia, name='editar_transferencia_criptomoeda'),
+    url(r'^editar-operacao/(?P<id_operacao>\d+)/$', views.editar_operacao_criptomoeda, name='editar_operacao_criptomoeda'),
+    url(r'^editar-transferencia/(?P<id_transferencia>\d+)/$', views.editar_transferencia, name='editar_transferencia_criptomoeda'),
     url(r'^historico/$', views.historico, name='historico_criptomoeda'),
-    url(r'^inserir_operacao_criptomoeda/$', views.inserir_operacao_criptomoeda, name='inserir_operacao_criptomoeda'),
-    url(r'^inserir_operacao_lote/$', views.inserir_operacao_lote, name='inserir_operacao_criptomoeda_lote'),
-    url(r'^inserir_transferencia/$', views.inserir_transferencia, name='inserir_transferencia_criptomoeda'),
-    url(r'^inserir_transferencia_lote/$', views.inserir_transferencia_lote, name='inserir_transferencia_criptomoeda_lote'),
-    url(r'^listar_criptomoedas/$', views.listar_criptomoedas, name='listar_criptomoedas'),
-    url(r'^listar_transferencias/$', views.listar_transferencias, name='listar_transferencias_criptomoeda'),
+    url(r'^inserir-operacao-criptomoeda/$', views.inserir_operacao_criptomoeda, name='inserir_operacao_criptomoeda'),
+    url(r'^inserir-operacao-lote/$', views.inserir_operacao_lote, name='inserir_operacao_criptomoeda_lote'),
+    url(r'^inserir-transferencia/$', views.inserir_transferencia, name='inserir_transferencia_criptomoeda'),
+    url(r'^inserir-transferencia-lote/$', views.inserir_transferencia_lote, name='inserir_transferencia_criptomoeda_lote'),
+    url(r'^listar-criptomoedas/$', views.listar_criptomoedas, name='listar_criptomoedas'),
+    url(r'^listar-transferencias/$', views.listar_transferencias, name='listar_transferencias_criptomoeda'),
     url(r'^painel/$', views.painel, name='painel_criptomoeda'),
 #     url(r'^sobre/$', views.sobre, name='sobre_criptomoeda'),
     ]
