83a0685a85bc5a84cd0cc8e501f1403875a15224
Guilherme de Deus <kingbowserii@gmail.com>
2018-01-14 20:29:18 -0200

    Minificando script para p?ginas em construcao

---
+++ bagogold/static/js/bagogold/base.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('.em-construcao').attr('data-original-title', 'Em construção').attr('data-placement', 'top').addClass('tooltips').removeClass('em-construcao');
+});
