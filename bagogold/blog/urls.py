f164a7e98701a528f436c98641ed7386e971b361
Guilherme Henrique <kingbowserii@gmail.com>
2018-01-24 02:08:22 -0200

    Listagem de posts quase unificada. Falta retirar sublinhado das anchors e alterar suas cores

--- bagogold/blog/urls.py
+++ bagogold/blog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
+    url(r'^listar-posts', views.listar_posts, name='listar_posts'),
     url(r'^post/(?P<post_slug>[\w-]+)/$', views.detalhar_post, name='detalhar_post'),
-#     url(r'^historico/$', views.historico, name='historico_criptomoeda'),
+    url(r'^tag/(?P<tag_slug>[\w-]+)/posts/$', views.listar_posts_por_tag, name='listar_posts_por_tag'),
     ]
