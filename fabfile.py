de08e70b4823d10d0cbf741046faae9857d2d1e4
Guilherme Henrique <kingbowserii@gmail.com>
2018-02-01 11:56:31 -0200

    Removendo gerar_css_def do fabfile pois n?o estava funcionando

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,6 @@ import datetime
 import re
 import time
 
-STATIC_FOLDER = settings.STATICFILES_DIRS[0]
-CSS_BASE_FOLDER = STATIC_FOLDER + '/assets/global/css'
-CSS_LAYOUT_FOLDER = STATIC_FOLDER + '/assets/layouts/layout3/css'
-CSS_ICONS_FOLDER = ''
-
 # Servers
 def prod():
     env.config = 'PROD'
@@ -65,40 +60,6 @@ def alterar_cron():
         run('crontab ~/%s/crontab_prod' % env.path)
     elif env.config == 'DEV':
         run('crontab ~/%s/crontab_copy' % env.path)
-
-def gerar_css_def():
-    require('config')
-    require('path')
-
-    if env.config != 'DEV':
-        print u'Comando deve ser usado apenas para DEV'
-        return
-    
-    with cd(env.path):
-        with cd('bagogold/static'):
-            run('gulp minify')
-            
-        # CSS base
-        texto = ''
-        with open(CSS_BASE_FOLDER + '/components-md.min.css', 'r') as arquivo:
-            texto += arquivo.read()
-        with open(CSS_BASE_FOLDER + '/plugins-md.min.css', 'r') as arquivo:
-            texto += arquivo.read()
-
-        with open(CSS_BASE_FOLDER + '/base.min.css', 'w') as arquivo_final:
-            arquivo_final.write(texto)
-        
-        # CSS de layout
-        texto = ''
-        with open(CSS_LAYOUT_FOLDER + '/layout.min.css', 'r') as arquivo:
-            texto += arquivo.read()
-        with open(CSS_LAYOUT_FOLDER + '/themes/default.min.css', 'r') as arquivo:
-            texto += arquivo.read()
-        with open(CSS_LAYOUT_FOLDER + '/custom.min.css', 'r') as arquivo:
-            texto += arquivo.read()
-
-        with open(CSS_LAYOUT_FOLDER + '/layout-def.min.css', 'w') as arquivo_final:
-            arquivo_final.write(texto)
 
 # Deve ser utilizado em
 def minificar_html():
