3785c6b8648a2f3b1fecc1d8d0f8cf5d4472ac9a
Guilherme de Deus <kingbowserii@gmail.com>
2018-02-06 19:34:55 -0200

    Atualiza??o de c?lculos de checkpoints

--- bagogold/bagogold/middleware/ultimo_acesso.py
+++ bagogold/bagogold/middleware/ultimo_acesso.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from bagogold.bagogold.utils.investidores import atualizar_checkpoints
 import datetime
 
 
@@ -7,6 +8,10 @@ class UltimoAcessoMiddleWare(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if not request.user.is_anonymous():
             if (not request.user.investidor.data_ultimo_acesso) or request.user.investidor.data_ultimo_acesso.day != datetime.date.today().day:
+                # Se último acesso foi em ano anterior, atualizar checkpoints
+                if request.user.investidor.data_ultimo_acesso.year < datetime.date.today().year:
+                    atualizar_checkpoints(request.user.investidor)
+                
                 request.user.investidor.data_ultimo_acesso = datetime.date.today()
                 request.user.investidor.save()
         return None
--- bagogold/bagogold/signals/fii.py
+++ bagogold/bagogold/signals/fii.py
@@ -210,15 +210,21 @@ def preparar_checkpointfii_evento_delete(sender, instance, **kwargs):
 def gerar_checkpoint_fii(investidor, fii, ano):
     quantidade = calcular_qtd_fiis_ate_dia_por_ticker(investidor, datetime.date(ano, 12, 31), fii.ticker)
     preco_medio = calcular_preco_medio_fiis_ate_dia_por_ticker(investidor, datetime.date(ano, 12, 31), fii.ticker)
-    if CheckpointFII.objects.filter(investidor=investidor, fii=fii, ano=ano-1, quantidade__gt=0).exists() or (quantidade > 0 or preco_medio != 0):
+    if CheckpointFII.objects.filter(investidor=investidor, fii=fii, ano=ano-1).exclude(quantidade=0).exists() or quantidade != 0:
         CheckpointFII.objects.update_or_create(investidor=investidor, fii=fii, ano=ano, 
                                            defaults={'quantidade': quantidade, 'preco_medio': preco_medio})
+    else:
+        # Não existe checkpoint anterior e quantidade atual é 0
+        CheckpointFII.objects.filter(investidor=investidor, fii=fii, ano=ano).delete()
     
 def gerar_checkpoint_proventos_fii(investidor, ano):
     valor = calcular_poupanca_prov_fii_ate_dia(investidor, datetime.date(ano, 12, 31))
-    if CheckpointProventosFII.objects.filter(investidor=investidor, ano__lte=ano).exists() or valor > 0:
+    if CheckpointProventosFII.objects.filter(investidor=investidor, ano=ano-1).exclude(valor=0).exists() or valor != 0:
         CheckpointProventosFII.objects.update_or_create(investidor=investidor, ano=ano, 
                                                    defaults={'valor': valor})
+    else:
+        # Não existe checkpoint anterior e valor atual é 0
+        CheckpointProventosFII.objects.filter(investidor=investidor, ano=ano).delete()
     
 def apagar_checkpoint_fii(investidor, fii):
     for checkpoint in CheckpointFII.objects.filter(investidor=investidor, fii=fii).order_by('ano'):
