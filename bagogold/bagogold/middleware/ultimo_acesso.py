73d12802a7aff5d20c0419015e2966c040e762e1
Guilherme Henrique <kingbowserii@gmail.com>
2018-02-06 21:24:28 -0200

    Adicionando proventos a atualiza??o de checkpoints do investidor

--- bagogold/bagogold/middleware/ultimo_acesso.py
+++ bagogold/bagogold/middleware/ultimo_acesso.py
@@ -7,7 +7,7 @@ class UltimoAcessoMiddleWare(object):
      
     def process_view(self, request, view_func, view_args, view_kwargs):
         if not request.user.is_anonymous():
-            if (not request.user.investidor.data_ultimo_acesso) or request.user.investidor.data_ultimo_acesso.day != datetime.date.today().day:
+            if (not request.user.investidor.data_ultimo_acesso) or request.user.investidor.data_ultimo_acesso != datetime.date.today():
                 # Se último acesso foi em ano anterior, atualizar checkpoints
                 if request.user.investidor.data_ultimo_acesso.year < datetime.date.today().year:
                     atualizar_checkpoints(request.user.investidor)
