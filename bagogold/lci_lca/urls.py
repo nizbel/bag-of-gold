a82fbca31d6cdeb61ff841e0b8d846806a729b4c
Guilherme Henrique <kingbowserii@gmail.com>
2018-02-12 14:00:32 -0200

    Separando FII e LCI/LCA do bagogold

---
+++ bagogold/lci_lca/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+import views
+
+urlpatterns = [
+    url(r'^editar-post/(?P<post_slug>[\w-]+)/$', views.editar_post, name='editar_post'),
+    url(r'^novo-post/$', views.inserir_post, name='novo_post'),
+    url(r'^posts/$', views.listar_posts, name='listar_posts'),
+    url(r'^post/(?P<post_slug>[\w-]+)/$', views.detalhar_post, name='detalhar_post'),
+    url(r'^tag/(?P<tag_slug>[\w-]+)/posts/$', views.listar_posts_por_tag, name='listar_posts_por_tag'),
+    
+    # URLs para o app do facebook
+    url(r'^tos/$', views.tos, name='tos'),
+    url(r'^privacy_policy/$', views.privacy_policy, name='privacy_policy'),
+    ]
