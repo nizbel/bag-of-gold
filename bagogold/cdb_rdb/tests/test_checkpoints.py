1d6c9af62bd42eef4fce12c0be38c5779530d685
Guilherme Henrique <kingbowserii@gmail.com>
2018-03-18 00:26:13 -0300

    Testes passando agora com o novo formato de calculo de valor para compras e vendas

--- bagogold/cdb_rdb/tests/test_checkpoints.py
+++ bagogold/cdb_rdb/tests/test_checkpoints.py
@@ -7,10 +7,11 @@ from bagogold.bagogold.utils.investidores import atualizar_checkpoints
 from bagogold.bagogold.utils.misc import verificar_feriado_bovespa, \
     qtd_dias_uteis_no_periodo
 from bagogold.cdb_rdb.models import CDB_RDB, OperacaoVendaCDB_RDB, \
-    OperacaoCDB_RDB, HistoricoPorcentagemCDB_RDB, CheckpointCDB_RDB
+    OperacaoCDB_RDB, HistoricoPorcentagemCDB_RDB, CheckpointCDB_RDB, \
+    HistoricoVencimentoCDB_RDB
 from bagogold.cdb_rdb.utils import calcular_valor_cdb_rdb_ate_dia, \
     buscar_operacoes_vigentes_ate_data, calcular_valor_operacao_cdb_rdb_ate_dia, \
-    calcular_valor_um_cdb_rdb_ate_dia_por_divisao
+    calcular_valor_um_cdb_rdb_ate_dia_por_divisao, calcular_valor_venda_cdb_rdb
 from decimal import Decimal
 from django.contrib.auth.models import User
 from django.db.models.aggregates import Sum
@@ -28,11 +29,15 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
         
         HistoricoPorcentagemCDB_RDB.objects.create(cdb_rdb=cdb_1, porcentagem=100)
         HistoricoPorcentagemCDB_RDB.objects.create(cdb_rdb=cdb_3, porcentagem=10)
+        
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb_1, vencimento=1080)
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb_2, vencimento=1080)
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb_3, vencimento=1080)
          
         # Históricos DI e IPCA
         # Data final é 14/02/2018 mas atualizações só contam até data anterior para manter DI e prefixado pareados
-        data_final = datetime.date(2018, 2, 13)
-        date_list = [data_final - datetime.timedelta(days=x) for x in range(0, (data_final - datetime.date(2017, 5, 11)).days+1)]
+        data_final = datetime.date(2018, 3, 13)
+        date_list = [data_final - datetime.timedelta(days=x) for x in range(0, (data_final - datetime.date(2017, 3, 11)).days+1)]
         date_list = [data for data in date_list if data.weekday() < 5 and not verificar_feriado_bovespa(data)]
         
         for data in date_list:
@@ -48,8 +53,10 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
                 HistoricoTaxaDI.objects.create(data=data, taxa=Decimal(9.14))
             elif data >= datetime.date(2017, 6, 1):
                 HistoricoTaxaDI.objects.create(data=data, taxa=Decimal(10.14))
-            else:
+            elif data >= datetime.date(2017, 4, 13):
                 HistoricoTaxaDI.objects.create(data=data, taxa=Decimal(11.13))
+            else:
+                HistoricoTaxaDI.objects.create(data=data, taxa=Decimal(12.13))
         
         # Operações de compra
         compra_1 = OperacaoCDB_RDB.objects.create(cdb_rdb=cdb_1, investidor=user.investidor, tipo_operacao='C', data=datetime.date(2017, 5, 11), quantidade=2000)
@@ -91,21 +98,22 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
         for operacao in OperacaoCDB_RDB.objects.filter(investidor=investidor, tipo_operacao='C').exclude(cdb_rdb=CDB_RDB.objects.get(nome="CDB 2")):
             if operacao.cdb_rdb == cdb_1:
                 data = datetime.date(2017, 5, 11)
-                self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.quantidade*Decimal('1.00042'), delta=Decimal('0.01'))
+                self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.quantidade * Decimal('1.00042'), delta=Decimal('0.01'))
                 data = datetime.date(2017, 6, 15)
                 self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.qtd_disponivel_venda_na_data(data) \
                                        * Decimal('1.01016557'), delta=Decimal('0.01'))
-                data = datetime.date(2018, 2, 14)
+                data = datetime.date(2018, 2, 13)
                 self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.qtd_disponivel_venda_na_data(data) \
                                        * Decimal('1.06363295'), delta=Decimal('0.01'))
             elif operacao.cdb_rdb == cdb_3:
                 data = datetime.date(2017, 5, 11)
-                self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.quantidade, delta=Decimal('0.01'))
+                self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.quantidade \
+                                       * pow((1+Decimal('0.1')), (Decimal(1)/252)), delta=Decimal('0.01'))
                 data = datetime.date(2017, 6, 15)
                 qtd_dias_uteis = Decimal(qtd_dias_uteis_no_periodo(operacao.data, data))
                 self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.qtd_disponivel_venda_na_data(data) \
                                   * pow((1+Decimal('0.1')), (qtd_dias_uteis/252)), delta=Decimal('0.01'))
-                data = datetime.date(2018, 2, 14)
+                data = datetime.date(2018, 2, 13)
                 qtd_dias_uteis = Decimal(qtd_dias_uteis_no_periodo(operacao.data, data))
                 self.assertAlmostEqual(calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data), operacao.qtd_disponivel_venda_na_data(data) \
                                   * pow((1+Decimal('0.1')), (qtd_dias_uteis/252)), delta=Decimal('0.01'))
@@ -124,7 +132,7 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
         for cdb_rdb_id in cdb_rdb.keys():
             self.assertEqual(cdb_rdb[cdb_rdb_id], sum([calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data) for operacao in OperacaoCDB_RDB.objects.filter(tipo_operacao='C', cdb_rdb__id=cdb_rdb_id)]))
         
-        data = datetime.date(2018, 2, 14)
+        data = datetime.date(2018, 2, 13)
         cdb_rdb = calcular_valor_cdb_rdb_ate_dia(investidor, data)
         for cdb_rdb_id in cdb_rdb.keys():
             self.assertEqual(cdb_rdb[cdb_rdb_id], sum([calcular_valor_operacao_cdb_rdb_ate_dia(operacao, data) for operacao in OperacaoCDB_RDB.objects.filter(tipo_operacao='C', cdb_rdb__id=cdb_rdb_id)]))
@@ -149,7 +157,7 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
             self.assertAlmostEqual(valor_cdb_rdb[cdb_rdb_id], sum([calcular_valor_um_cdb_rdb_ate_dia_por_divisao(cdb_rdb, divisao.id, data) \
                                                              for divisao in Divisao.objects.filter(investidor=investidor)]), delta=Decimal('0.01'))
             
-        data = datetime.date(2018, 2, 14)
+        data = datetime.date(2018, 2, 13)
         valor_cdb_rdb = calcular_valor_cdb_rdb_ate_dia(investidor, data)
         # Comparar a soma das divisões para cada investimento com o total calculado
         for cdb_rdb_id in valor_cdb_rdb.keys():
@@ -236,6 +244,23 @@ class CalcularQuantidadesCDB_RDBTestCase(TestCase):
                     self.assertFalse(CheckpointCDB_RDB.objects.filter(operacao=operacao, ano=2017).exists())
                     self.assertFalse(CheckpointDivisaoCDB_RDB.objects.filter(divisao_operacao__operacao=operacao, ano=2017).exists())
                 
+    def test_verificar_qtd_atualizada(self):
+        """Testa cálculo de valor da venda de CDB utilizando checkpoint"""
+        investidor = Investidor.objects.get(user__username='test')
+        cdb = CDB_RDB.objects.create(nome="CDB 1", investidor=investidor, tipo='C', tipo_rendimento=CDB_RDB.CDB_RDB_DI)
+        HistoricoPorcentagemCDB_RDB.objects.create(cdb_rdb=cdb, porcentagem=110)
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb, vencimento=361)
+        
+        operacao_compra = OperacaoCDB_RDB.objects.create(cdb_rdb=cdb, quantidade=1250, data=datetime.date(2017, 3, 13), investidor=investidor, tipo_operacao='C')
+        operacao_venda = OperacaoCDB_RDB.objects.create(cdb_rdb=cdb, quantidade=1250, data=datetime.date(2018, 3, 9), investidor=investidor, tipo_operacao='V')
+        OperacaoVendaCDB_RDB.objects.create(operacao_compra=operacao_compra, operacao_venda=operacao_venda)
+        
+        self.assertAlmostEqual(Decimal('1349.33'), calcular_valor_venda_cdb_rdb(operacao_venda, True, True), delta=Decimal('0.01'))
+        
+        
+        
+        
+        
         
 # class PerformanceCheckpointCDB_RDBTestCase(TestCase):
 #     def setUp(self):
@@ -356,6 +381,7 @@ class AtualizarCheckpointAnualTestCase(TestCase):
         cdb_1 = CDB_RDB.objects.create(nome="CDB 1", investidor=user.investidor, tipo='C', tipo_rendimento=CDB_RDB.CDB_RDB_DI)
         
         HistoricoPorcentagemCDB_RDB.objects.create(cdb_rdb=cdb_1, porcentagem=100)
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb_1, vencimento=1080)
          
         # Históricos DI e IPCA
         # Data final é 14/02/2018 mas atualizações só contam até data anterior para manter DI e prefixado pareados
