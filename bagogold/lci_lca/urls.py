5e46ef2033675cd50b8c7d2ae39a8bcb4b31d93f
Guilherme de Deus <kingbowserii@gmail.com>
2018-03-19 03:52:58 -0300

    Corrigindo urls de lci_lca

--- bagogold/lci_lca/urls.py
+++ bagogold/lci_lca/urls.py
@@ -3,13 +3,18 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^editar-post/(?P<post_slug>[\w-]+)/$', views.editar_post, name='editar_post'),
-    url(r'^novo-post/$', views.inserir_post, name='novo_post'),
-    url(r'^posts/$', views.listar_posts, name='listar_posts'),
-    url(r'^post/(?P<post_slug>[\w-]+)/$', views.detalhar_post, name='detalhar_post'),
-    url(r'^tag/(?P<tag_slug>[\w-]+)/posts/$', views.listar_posts_por_tag, name='listar_posts_por_tag'),
-    
-    # URLs para o app do facebook
-    url(r'^tos/$', views.tos, name='tos'),
-    url(r'^privacy_policy/$', views.privacy_policy, name='privacy_policy'),
+    url(r'^detalhar-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.detalhar_lci_lca, name='detalhar_lci_lca'),
+    url(r'^editar-historico-carencia/(?P<historico_carencia_id>\d+)/$', views.lc.lc.editar_historico_carencia, name='editar_historico_carencia_lci_lca'),
+    url(r'^editar-historico-porcentagem/(?P<historico_porcentagem_id>\d+)/$', views.lc.lc.editar_historico_porcentagem, name='editar_historico_porcentagem_lci_lca'),
+    url(r'^editar-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.editar_lci_lca, name='editar_lci_lca'),
+    url(r'^editar-operacao/(?P<id>\d+)/$', views.lc.lc.editar_operacao_lc, name='editar_operacao_lci_lca'),
+    url(r'^historico/$', views.lc.lc.historico, name='historico_lci_lca'),
+    url(r'^inserir-letra-credito/$', views.lc.lc.inserir_lc, name='inserir_lci_lca'),
+    url(r'^inserir-operacao-lc/$', views.lc.lc.inserir_operacao_lc, name='inserir_operacao_lci_lca'),
+    url(r'^listar-letras-credito/$', views.lc.lc.listar_lc, name='listar_lci_lca'),
+    url(r'^listar-operacoes-passada-carencia/$', views.lc.lc.listar_operacoes_passada_carencia, name='listar_operacoes_passada_carencia'),
+    url(r'^inserir-historico-carencia-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.inserir_historico_carencia, name='inserir_historico_carencia_lci_lca'),
+    url(r'^inserir-historico-porcentagem-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.inserir_historico_porcentagem, name='inserir_historico_porcentagem_lci_lca'),
+    url(r'^painel/$', views.lc.lc.painel, name='painel_lci_lca'),
+    url(r'^sobre/$', views.lc.lc.sobre, name='sobre_lci_lca'),
     ]
