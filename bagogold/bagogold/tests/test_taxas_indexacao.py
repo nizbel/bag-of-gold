afdcc9264de92fb442ce8cbbbc78ef60de184346
Guilherme de Deus <kingbowserii@gmail.com>
2018-04-18 18:08:47 +0000

    Preparando teste para busca de indices DI

---
+++ bagogold/bagogold/tests/test_taxas_indexacao.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class DITestCase(TestCase):
+
+    def test_buscar_datas(self):
+        """Testa se a busca funciona"""
+        
+    def test_buscar_datas_anterior_a_ultima(self):
+        """Testa buscar datas que não haviam sido preenchidas e são anteriores a ultima data na base"""
+        
+    def test_buscar_datas_com_datas_registradas(self):
+        """Testa a busca quando já existem algumas datas na base"""
