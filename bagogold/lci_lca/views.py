5b68590ed2d3b6fc147c479d8498d905c37fe7fa
Guilherme de Deus <kingbowserii@gmail.com>
2018-04-23 23:00:08 +0000

    Corrigindo ordena??o dos gr?ficos de ?ndices no sobre lci/lca

--- bagogold/lci_lca/views.py
+++ bagogold/lci_lca/views.py
@@ -848,10 +848,10 @@ def sobre(request):
         return HttpResponse(json.dumps({'sucesso': True, 'graf_simulador': graf_simulador}), content_type = "application/json") 
     else:
         data_atual = datetime.date.today()
-        historico_di = HistoricoTaxaDI.objects.filter(data__gte=data_atual.replace(year=data_atual.year-3))
+        historico_di = HistoricoTaxaDI.objects.filter(data__gte=data_atual.replace(year=data_atual.year-3)).order_by('data')
         graf_historico_di = [[str(calendar.timegm(valor_historico.data.timetuple()) * 1000), float(valor_historico.taxa)] for valor_historico in historico_di]
         
-        historico_ipca = HistoricoIPCA.objects.filter(ano__gte=(data_atual.year-3)).exclude(mes__lt=data_atual.month, ano=data_atual.year-3)
+        historico_ipca = HistoricoIPCA.objects.filter(ano__gte=(data_atual.year-3)).exclude(mes__lt=data_atual.month, ano=data_atual.year-3).order_by('ano', 'mes')
         graf_historico_ipca = [[str(calendar.timegm(valor_historico.data().timetuple()) * 1000), float(valor_historico.valor)] for valor_historico in historico_ipca]
         
         if request.user.is_authenticated():
