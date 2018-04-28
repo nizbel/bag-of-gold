a6db7ddb100b886a5188864b1d71f5bfdceb187a
Guilherme Henrique <kingbowserii@gmail.com>
2018-04-28 17:10:14 -0300

    Tela de detalhamento de FIIs funcional

--- bagogold/fii/tests/test_views.py
+++ bagogold/fii/tests/test_views.py
@@ -1,45 +1,110 @@
+# -*- coding: utf-8 -*-
+import datetime
+from decimal import Decimal
+from django.contrib.auth.models import User
+from django.test.testcases import TestCase
+
+from django.core.urlresolvers import reverse
+
+from bagogold.bagogold.models.empresa import Empresa
+from bagogold.fii.models import FII, HistoricoFII, ProventoFII, \
+    EventoAgrupamentoFII, OperacaoFII
 
 
 class DetalharFIITestCase (TestCase):
-    def setUp():
-        # TODO Criar empresa
-        # TODO Criar 2 FII
-        # TODO Criar histórico de preços para 1
-        # TODO Criar proventos para 1
-        # TODO Criar agrupamento para 1
+    def setUp(self):
+        nizbel = User.objects.create_user('nizbel', 'nizbel@teste.com', 'nizbel')
+        user_vendido = User.objects.create_user('vendido', 'vendido@teste.com', 'vendido')
+
+        empresa = Empresa.objects.create(nome='Teste', nome_pregao='TEST')
+        
+        fii_1 = FII.objects.create(ticker='TEST11', empresa=empresa)
+        fii_2 = FII.objects.create(ticker='TSTE11', empresa=empresa)
+        
+        for data in [datetime.date.today() - datetime.timedelta(days=x) for x in range(365)]:
+            if data >= datetime.date.today() - datetime.timedelta(days=3):
+                HistoricoFII.objects.create(fii=fii_2, preco_unitario=1200, data=data)
+            else:
+                HistoricoFII.objects.create(fii=fii_2, preco_unitario=120, data=data)
+        
+        for data in [datetime.date.today() - datetime.timedelta(days=30*x) for x in range(1, 7)]:
+            ProventoFII.objects.create(fii=fii_2, valor_unitario=Decimal('0.9'), data_ex=data, data_pagamento=data+datetime.timedelta(days=7),
+                                       oficial_bovespa=True)
+        
+        EventoAgrupamentoFII.objects.create(fii=fii_2, data=datetime.date.today() - datetime.timedelta(days=3), proporcao=Decimal('0.1'))
+        
+        OperacaoFII.objects.create(fii=fii_2, investidor=nizbel.investidor, data=datetime.date.today() - datetime.timedelta(days=130),
+                                   quantidade=5, preco_unitario=120, corretagem=10, emolumentos=Decimal('0.1'), tipo_operacao='C')
+        OperacaoFII.objects.create(fii=fii_2, investidor=nizbel.investidor, data=datetime.date.today() - datetime.timedelta(days=80),
+                                   quantidade=5, preco_unitario=120, corretagem=10, emolumentos=Decimal('0.1'), tipo_operacao='C')
+        
+        OperacaoFII.objects.create(fii=fii_2, investidor=user_vendido.investidor, data=datetime.date.today() - datetime.timedelta(days=130),
+                                   quantidade=5, preco_unitario=120, corretagem=10, emolumentos=Decimal('0.1'), tipo_operacao='C')
+        OperacaoFII.objects.create(fii=fii_2, investidor=user_vendido.investidor, data=datetime.date.today() - datetime.timedelta(days=80),
+                                   quantidade=5, preco_unitario=120, corretagem=10, emolumentos=Decimal('0.1'), tipo_operacao='V')
     
-    def test_usuario_deslogado_fii_vazio:
+    def test_usuario_deslogado_fii_vazio(self):
         """Testa o acesso a view para um fii sem infos com um usuário deslogado"""
-        # TODO acessar a view
+        response = self.client.get(reverse('fii:detalhar_fii', kwargs={'fii_ticker': 'TEST11'}))
+        self.assertEqual(response.status_code, 200)
         
-        self.assertEqual(status_code, 200)
+        # Verificar contexto
+        self.assertEqual(len(response.context_data['operacoes']), 0)
+        self.assertEqual(len(response.context_data['historico']), 0)
+        self.assertEqual(len(response.context_data['proventos']), 0)
+        self.assertEqual(response.context_data['fii'], FII.objects.get(ticker='TEST11'))
         
-    def test_usuario_logado_fii_vazio:
+    def test_usuario_logado_fii_vazio(self):
         """Testa o acesso a view para um fii sem infos com um usuário logado"""
-        # TODO Criar usuário
+        self.client.login(username='nizbel', password='nizbel')
         
-        # TODO Criar operações
+        response = self.client.get(reverse('fii:detalhar_fii', kwargs={'fii_ticker': 'TEST11'}))
+        self.assertEqual(response.status_code, 200)
         
-        # TODO logar usuário
+        # Verificar contexto
+        self.assertEqual(len(response.context_data['operacoes']), 0)
+        self.assertEqual(len(response.context_data['historico']), 0)
+        self.assertEqual(len(response.context_data['proventos']), 0)
+        self.assertEqual(response.context_data['fii'], FII.objects.get(ticker='TEST11'))
         
-        self.assertEqual(status_code, 200)
-        
-        # TODO Verificar contexto
-        
-    def test_usuario_deslogado_fii_com_infos:
+    def test_usuario_deslogado_fii_com_infos(self):
         """Testa o acesso a view para um fii com infos com um usuário deslogado"""
-        # TODO acessar a view
+        response = self.client.get(reverse('fii:detalhar_fii', kwargs={'fii_ticker': 'TSTE11'}))
         
-        self.assertEqual(status_code, 200)
+        self.assertEqual(response.status_code, 200)
         
-    def test_usuario_logado_fii_com_infos:
+        # Verificar contexto
+        self.assertEqual(len(response.context_data['operacoes']), 0)
+        self.assertEqual(len(response.context_data['historico']), 365)
+        self.assertEqual(len(response.context_data['proventos']), 6)
+        self.assertEqual(response.context_data['fii'], FII.objects.get(ticker='TSTE11'))
+        
+    def test_usuario_logado_fii_com_infos(self):
         """Testa o acesso a view para um fii com infos com um usuário logado"""
-        # TODO Criar usuário
+        self.client.login(username='nizbel', password='nizbel')
+
+        response = self.client.get(reverse('fii:detalhar_fii', kwargs={'fii_ticker': 'TSTE11'}))      
+        self.assertEqual(response.status_code, 200)
         
-        # TODO Criar operações
+        # Verificar contexto
+        self.assertEqual(len(response.context_data['operacoes']), 2)
+        self.assertEqual(len(response.context_data['historico']), 365)
+        self.assertEqual(len(response.context_data['proventos']), 6)
+        self.assertEqual(response.context_data['fii'], FII.objects.get(ticker='TSTE11'))
+        # Quantidade de cotas atual é 1 devido ao agrupamento
+        self.assertEqual(response.context_data['fii'].qtd_cotas, 1)
         
-        # TODO logar usuário
         
-        self.assertEqual(status_code, 200)
+    def test_usuario_logado_fii_com_infos_vendido(self):
+        """Testa o acesso a view para um fii com infos com um usuário logado, que já tinha liquidado sua posição"""
+        self.client.login(username='vendido', password='vendido')
+
+        response = self.client.get(reverse('fii:detalhar_fii', kwargs={'fii_ticker': 'TSTE11'}))      
+        self.assertEqual(response.status_code, 200)
         
-        # TODO Verificar contexto
+        # Verificar contexto
+        self.assertEqual(len(response.context_data['operacoes']), 2)
+        self.assertEqual(len(response.context_data['historico']), 365)
+        self.assertEqual(len(response.context_data['proventos']), 6)
+        self.assertEqual(response.context_data['fii'], FII.objects.get(ticker='TSTE11'))
+        self.assertEqual(response.context_data['fii'].qtd_cotas, 0)
--- bagogold/fii/views.py
+++ bagogold/fii/views.py
@@ -198,7 +198,9 @@ def detalhar_fii(request, fii_ticker):
         fii.data_valor_atual = None
         
     
-    proventos = ProventoFII.objects.filter(fii=fii).order_by('data_ex')
+    proventos = ProventoFII.objects.filter(fii=fii).annotate(tipo=Case(When(tipo_provento=ProventoFII.TIPO_PROVENTO_AMORTIZACAO, then=Value(u'Amortização')),
+                                                                       When(tipo_provento=ProventoFII.TIPO_PROVENTO_RENDIMENTO, then=Value(u'Rendimento')), output_field=CharField())) \
+                                                   .order_by('data_ex')
     
     if proventos.exists():
         rendimentos = proventos.filter(tipo_provento=ProventoFII.TIPO_PROVENTO_RENDIMENTO)
@@ -209,6 +211,9 @@ def detalhar_fii(request, fii_ticker):
         else:
             fii.ultimo_provento = None
             fii.data_ultimo_provento = None
+    else:
+        fii.ultimo_provento = None
+        fii.data_ultimo_provento = None
 
     # Calcular percentual de rendimento do ultimo provento
     if fii.valor_atual and fii.ultimo_provento:
@@ -222,8 +227,11 @@ def detalhar_fii(request, fii_ticker):
         # Buscar operações
         operacoes = OperacaoFII.objects.filter(investidor=investidor, fii=fii).annotate(tipo=Case(When(tipo_operacao='C', then=Value(u'Compra')),
                                                                                                   When(tipo_operacao='V', then=Value(u'Venda')), output_field=CharField())) \
-                                                                              .annotate(taxas=F('emolumentos') + F('corretagem')) \
-                                                                              .order_by('data')
+                                      .annotate(taxas=F('emolumentos') + F('corretagem')) \
+                                      .annotate(total=Case(When(tipo_operacao='C', then=-1 * (F('quantidade') * F('preco_unitario') + F('emolumentos') + F('corretagem'))),
+                                                           When(tipo_operacao='V', then=F('quantidade') * F('preco_unitario') - F('emolumentos') - F('corretagem')),
+                                                           output_field=DecimalField())) \
+                                      .order_by('data')
         
         fii.qtd_cotas = calcular_qtd_fiis_ate_dia_por_ticker(investidor, datetime.date.today(), fii.ticker)
         fii.preco_medio = calcular_preco_medio_fiis_ate_dia_por_ticker(investidor, datetime.date.today(), fii.ticker)
@@ -231,20 +239,25 @@ def detalhar_fii(request, fii_ticker):
         fii.total_provento_recebido = 0
         
         if operacoes.exists():
-            for provento in proventos.filter(data_ex__gt=operacoes[0].data):
-                provento.pago = datetime.date.today() > provento.data_pagamento
-                provento.qtd_na_data_ex = calcular_qtd_fiis_ate_dia_por_ticker(request.user.investidor, provento.data_ex, provento.fii.ticker)
-                provento.valor_recebido = (provento.qtd_na_data_ex * provento.valor_unitario).quantize(Decimal('0.01'), rounding=ROUND_FLOOR)
+            # Adicionar utilização de proventos
+            for operacao in operacoes:
+                operacao.uso_proventos = Decimal(0)
+                if operacao.utilizou_proventos():
+                    operacao.uso_proventos += operacao.qtd_proventos_utilizada()
+                    
+            proventos = list(proventos)
+#             for provento in proventos.filter(data_ex__gt=operacoes[0].data):
+            for provento in [provento for provento in proventos if provento.data_ex > operacoes[0].data]:
+                provento.pago = datetime.date.today() >= provento.data_pagamento
+                provento.qtd_na_data_base = calcular_qtd_fiis_ate_dia_por_ticker(request.user.investidor, provento.data_ex-datetime.timedelta(days=1), provento.fii.ticker)
+                provento.valor_recebido = (provento.qtd_na_data_base * provento.valor_unitario).quantize(Decimal('0.01'), rounding=ROUND_FLOOR)
                 fii.total_provento_recebido += provento.valor_recebido
     else:
         operacoes = list()
-    
+        
     # Preparar gráfico histórico
-    historico = HistoricoFII.objects.filter(fii=fii).order_by('data')
-    # TODO Dependendo do período, pegar apenas 100 pontos no gráfico histórico
-#     if historico.count() > 100:
-#         historico = [[(data, preco_unitario) for (data, preco_unitario) in historico]]
-    
+    historico = [[str(calendar.timegm(data.timetuple()) * 1000), float(preco_unitario)] for (data, preco_unitario) in HistoricoFII.objects.filter(fii=fii).values_list('data', 'preco_unitario').order_by('data')]
+
     return TemplateResponse(request, 'fii/detalhar_fii.html', {'fii': fii, 'historico': historico, 'operacoes': operacoes, 'proventos': proventos})
     
 @adiciona_titulo_descricao('Detalhar provento', 'Detalhamento de proventos em FIIs')
@@ -388,7 +401,6 @@ def editar_operacao_fii(request, operacao_id):
 
 @adiciona_titulo_descricao('Histórico de FII', 'Histórico de operações de compra/venda e rendimentos/amortizações do investidor')
 def historico_fii(request):
-    inicio = datetime.datetime.now()
     if request.user.is_authenticated():
         investidor = request.user.investidor
     else:
@@ -545,7 +557,6 @@ def historico_fii(request):
     dados['lucro'] = patrimonio + total_proventos + total_gasto
     dados['lucro_percentual'] = (patrimonio + total_proventos + total_gasto) / -total_gasto * 100
     
-    print datetime.datetime.now() - inicio
     return TemplateResponse(request, 'fii/historico.html', {'dados': dados, 'lista_conjunta': lista_conjunta, 'graf_poupanca_proventos': graf_poupanca_proventos, 
                                                      'graf_gasto_total': graf_gasto_total, 'graf_patrimonio': graf_patrimonio, 'lista_eventos': ['Agrupamento', 'Desdobramento', 'Incorporação']})
     
