93163dbd907037705f67febf21404f2799f25a0e
Guilherme de Deus <kingbowserii@gmail.com>
2018-05-03 23:35:07 +0000

    Trazendo acumulados de indexacao para taxas indexacao

--- bagogold/bagogold/utils/taxas_indexacao.py
+++ bagogold/bagogold/utils/taxas_indexacao.py
@@ -84,6 +84,45 @@ def calcular_valor_atualizado_com_taxas_selic(taxas_dos_dias, valor_atual):
         taxa_acumulada *= pow(taxa_do_dia, taxas_dos_dias[taxa_do_dia])
     return taxa_acumulada * valor_atual
 
+def calcular_valor_acumulado_ipca(data_base, data_final=datetime.date.today()):
+    """
+    Calcula o valor acumulado do IPCA desde a data base, até uma data final
+    
+    Parâmetros: Data base
+                Data final
+    Retorno: Taxa total acumulada
+    """
+    # COTACAO = 1000 / (1 + TAXA)**(DIAS_UTEIS/252)
+    ipca_inicial = HistoricoIPCA.objects.get(mes=data_base.month, ano=data_base.year)
+    # Calcular quantidade de dias em que a taxa inicial foi aplicada
+    ultimo_dia_mes_inicial = datetime.date(data_base.year, data_base.month, calendar.monthrange(data_base.year, data_base.month)[1])
+    qtd_dias = (min(data_final, ultimo_dia_mes_inicial) - data_base).days
+    # Transformar taxa mensal em diaria
+    ipca_inicial_diario = pow(1 + ipca_inicial.valor/Decimal(100), Decimal(1)/30) - 1
+    # Iniciar IPCA do periodo com o acumulado nos dias
+    ipca_periodo = pow(1 + ipca_inicial_diario, qtd_dias) - 1
+#     print 'IPCA inicial:', ipca_periodo
+    # TODO melhorar isso
+    for mes_historico in HistoricoIPCA.objects.filter((Q(mes__gt=ipca_inicial.mes) & Q(ano=ipca_inicial.ano)) | \
+                                                      Q(ano__gt=ipca_inicial.ano)).filter(ano__lte=data_final.year).order_by('ano', 'mes'):
+        if datetime.date(mes_historico.ano, mes_historico.mes, calendar.monthrange(mes_historico.ano, mes_historico.mes)[1]) <= data_final:
+#             print mes_historico.ano, '/', mes_historico.mes, '->', ipca_periodo, (1 + mes_historico.valor/Decimal(100))
+            ipca_periodo = (1 + ipca_periodo) * (1 + mes_historico.valor/Decimal(100)) - 1
+    return ipca_periodo
+    
+def calcular_valor_acumulado_selic(data_base, data_final=datetime.date.today()):
+    """
+    Calcula o valor acumulado da Selic desde a data base, até uma data final
+    
+    Parâmetros: Data base
+                Data final
+    Retorno: Taxa total acumulada
+    """
+    selic_periodo = 1
+    taxas_selic = dict(HistoricoTaxaSelic.objects.filter(data__range=[data_base, data_final]).order_by('taxa_diaria').annotate(qtd_dias=Count('taxa_diaria')).values_list('taxa_diaria', 'qtd_dias'))
+    selic_periodo *= [taxa for (taxa, qtd_dias) in taxas_selic.items()]
+    return selic_periodo
+
 # TODO trazer busca de valores diários do DI para essa função
 def buscar_valores_diarios_di():
     """Busca valores históricos do DI no site da CETIP"""
