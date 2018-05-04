b76abda870da7d8a5002e88a49d9b7729136b82d
Guilherme Henrique <kingbowserii@gmail.com>
2018-05-04 13:03:05 -0300

    Melhorando testes de atualizacao por taxas de indexacao

--- bagogold/bagogold/tests/test_taxas_indexacao.py
+++ bagogold/bagogold/tests/test_taxas_indexacao.py
@@ -4,9 +4,13 @@ from decimal import Decimal
 
 from django.test import TestCase
 
-from bagogold.bagogold.models.taxas_indexacao import HistoricoTaxaDI
-from bagogold.bagogold.utils.misc import verifica_se_dia_util
-from bagogold.bagogold.utils.taxas_indexacao import buscar_valores_diarios_di
+from bagogold.bagogold.models.taxas_indexacao import HistoricoTaxaDI, \
+    HistoricoTaxaSelic
+from bagogold.bagogold.utils.misc import verifica_se_dia_util, \
+    verificar_feriado_bovespa
+from bagogold.bagogold.utils.taxas_indexacao import buscar_valores_diarios_di, \
+    calcular_valor_atualizado_com_taxa_di, calcular_valor_atualizado_com_taxas_di, \
+    calcular_valor_atualizado_com_taxa_selic
 
 
 class BuscaDITestCase(TestCase):
@@ -41,6 +45,14 @@ class AtualizacaoTaxasTestCase(TestCase):
     """Testa atualização de valores pelas taxas"""
     def setUp(self):
         # Preencher valores DI
+        date_list = [datetime.date(2018, 3, 1) + datetime.timedelta(days=x) for x in range((datetime.date(2018, 4, 1) - datetime.date(2018, 3, 1)).days)]
+        date_list = [data for data in date_list if data.weekday() < 5 and not verificar_feriado_bovespa(data)]
+        
+        for data in date_list:
+            if data >= datetime.date(2018, 3, 22):
+                HistoricoTaxaDI.objects.create(data=data, taxa=Decimal('6.39'))
+            else:
+                HistoricoTaxaDI.objects.create(data=data, taxa=Decimal('6.64'))
         
         # Preencher valores IPCA
         
@@ -52,13 +64,14 @@ class AtualizacaoTaxasTestCase(TestCase):
         ultimo_di = HistoricoTaxaDI.objects.all().order_by('-data')[0].taxa
         
         # Atualizar 1 dia pela taxa integral
-        self.assertEqual(calcular_valor_atualizado_com_taxa_di(taxa, 1000, 100), 1000 * (1 + ultimo_di/100)**(Decimal(1)/252))
+        self.assertEqual(calcular_valor_atualizado_com_taxa_di(ultimo_di, 1000, 100), 1000 * (1 + ultimo_di/100)**(Decimal(1)/252))
         
         # Atualizar 1 dia por metade da taxa
-        self.assertEqual(calcular_valor_atualizado_com_taxa_di(taxa, 1000, 50), 1000 * (1 + ultimo_di/100 / 2)**(Decimal(1)/252))
+        self.assertEqual(calcular_valor_atualizado_com_taxa_di(ultimo_di, 1000, 50), 1000 * ((1 + ultimo_di/100)**(Decimal(1)/252) - 1) / 2 + 1)
         
         # Buscar taxas DI em um período
-        taxas_di = dict(HistoricoTaxaDI.objects.filter(data__range=[datetime.date(2018, 3, 1), datetime.date(2018, 3, 30)]).values_list('data', 'taxa'))
+        taxas_di = dict(HistoricoTaxaDI.objects.filter(data__range=[datetime.date(2018, 3, 1), datetime.date(2018, 3, 30)]).order_by('taxa') \
+                        .aggregate(qtd_dias=Count('data')).values_list('taxa', 'qtd_dias'))
         
         # Atualizar vários dias pela taxa integral
         self.assertEqual(calcular_valor_atualizado_com_taxas_di(taxas_di, 1000, 100), 1000 * Decimal('1.00531564'))
