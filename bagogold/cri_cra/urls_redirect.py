f54accca12eab1a85d1e4fed62e1a21b1f8bb032
Guilherme Henrique <kingbowserii@gmail.com>
2018-05-14 02:07:24 -0300

    Ajustando redirecionamentos de URL

--- bagogold/cri_cra/urls_redirect.py
+++ bagogold/cri_cra/urls_redirect.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^detalhar_cri_cra/(?P<id_cri_cra>\d+)/$', RedirectView.as_view(pattern_name='detalhar_cri_cra', permanent=True)),
--- bagogold/fundo_investimento/urls_redirect.py
+++ bagogold/fundo_investimento/urls_redirect.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^detalhar_fundo/(?P<id_fundo>\d+)/$', RedirectView.as_view(pattern_name='fundo_investimento:detalhar_fundo_id', permanent=True)),
