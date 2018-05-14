a9c07d776897bfea56e300f9aae99816cd35673a
Guilherme de Deus <kingbowserii@gmail.com>
2018-05-14 22:53:20 +0000

    Adicionando redirecionamentos para lci_lca

--- bagogold/bagogold/urls.py
+++ bagogold/bagogold/urls.py
@@ -128,22 +128,22 @@ imposto_renda_patterns = [
     url(r'^listar-anos/$', views.imposto_renda.imposto_renda.listar_anos, name='listar_anos_imposto_renda'),
     ]
 
-# lci_lca_patterns = [
-#     url(r'^detalhar-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.detalhar_lci_lca, name='detalhar_lci_lca'),
-#     url(r'^editar-historico-carencia/(?P<historico_carencia_id>\d+)/$', views.lc.lc.editar_historico_carencia, name='editar_historico_carencia_lci_lca'),
-#     url(r'^editar-historico-porcentagem/(?P<historico_porcentagem_id>\d+)/$', views.lc.lc.editar_historico_porcentagem, name='editar_historico_porcentagem_lci_lca'),
-#     url(r'^editar-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.editar_lci_lca, name='editar_lci_lca'),
-#     url(r'^editar-operacao/(?P<id>\d+)/$', views.lc.lc.editar_operacao_lc, name='editar_operacao_lci_lca'),
-#     url(r'^historico/$', views.lc.lc.historico, name='historico_lci_lca'),
-#     url(r'^inserir-letra-credito/$', views.lc.lc.inserir_lc, name='inserir_lci_lca'),
-#     url(r'^inserir-operacao-lc/$', views.lc.lc.inserir_operacao_lc, name='inserir_operacao_lci_lca'),
-#     url(r'^listar-letras-credito/$', views.lc.lc.listar_lc, name='listar_lci_lca'),
-#     url(r'^listar-operacoes-passada-carencia/$', views.lc.lc.listar_operacoes_passada_carencia, name='listar_operacoes_passada_carencia'),
-#     url(r'^inserir-historico-carencia-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.inserir_historico_carencia, name='inserir_historico_carencia_lci_lca'),
-#     url(r'^inserir-historico-porcentagem-lci-lca/(?P<lci_lca_id>\d+)/$', views.lc.lc.inserir_historico_porcentagem, name='inserir_historico_porcentagem_lci_lca'),
-#     url(r'^painel/$', views.lc.lc.painel, name='painel_lci_lca'),
-#     url(r'^sobre/$', views.lc.lc.sobre, name='sobre_lci_lca'),
-#     ]
+lci_lca_patterns = [
+    url(r'^detalhar_lci_lca/(?P<lci_lca_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:detalhar_lci_lca', permanent=True)),
+    url(r'^editar_historico_carencia/(?P<historico_carencia_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:editar_historico_carencia_lci_lca', permanent=True)),
+    url(r'^editar_historico_porcentagem/(?P<historico_porcentagem_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:editar_historico_porcentagem_lci_lca', permanent=True)),
+    url(r'^editar_lci_lca/(?P<lci_lca_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:editar_lci_lca', permanent=True)),
+    url(r'^editar_operacao/(?P<id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:editar_operacao_lci_lca', permanent=True)),
+    url(r'^historico/$', RedirectView.as_view(pattern_name='lci_lca:historico_lci_lca', permanent=True)),
+    url(r'^inserir_letra_credito/$', RedirectView.as_view(pattern_name='lci_lca:inserir_lci_lca', permanent=True)),
+    url(r'^inserir_operacao_lc/$', RedirectView.as_view(pattern_name='lci_lca:inserir_operacao_lci_lca', permanent=True)),
+    url(r'^listar_letras_credito/$', RedirectView.as_view(pattern_name='lci_lca:listar_lci_lca', permanent=True)),
+    url(r'^listar_operacoes_passada_carencia/$', RedirectView.as_view(pattern_name='lci_lca:listar_operacoes_passada_carencia', permanent=True)),
+    url(r'^inserir_historico_carencia_lci_lca/(?P<lci_lca_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:inserir_historico_carencia_lci_lca', permanent=True)),
+    url(r'^inserir_historico_porcentagem_lci_lca/(?P<lci_lca_id>\d+)/$', RedirectView.as_view(pattern_name='lci_lca:inserir_historico_porcentagem_lci_lca', permanent=True)),
+    url(r'^painel/$', RedirectView.as_view(pattern_name='lci_lca:painel_lci_lca', permanent=True)),
+    url(r'^sobre/$', RedirectView.as_view(pattern_name='lci_lca:sobre_lci_lca', permanent=True)),
+    ]
 
 td_patterns = [
     url(r'^acompanhamento/$', RedirectView.as_view(pattern_name='tesouro_direto:acompanhamento_td', permanent=True)),
@@ -205,7 +205,7 @@ urlpatterns = [
     # Poupança
     
     # LCA e LCI
-#     url(r'^lci-lca/', include(lci_lca_patterns, namespace='lci_lca')),
+    url(r'^lci_lca/', include(lci_lca_patterns, namespace='lci_lca')),
     
     # CDB e RDB
 #     url(r'^cdb-rdb/', include(cdb_rdb_patterns, namespace='cdb_rdb')),
