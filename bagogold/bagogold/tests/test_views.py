8aff5d8e19c0fe96ac9d1626d30bd2b7456d3835
Guilherme de Deus <kingbowserii@gmail.com>
2018-11-29 20:46:52 +0000

    Preparando teste das views gerais

---
+++ bagogold/bagogold/tests/test_views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class ProxVencimentosPainelGeralTestCase(TestCase):
+    def setUp(self):
+        tester
+        nizbel 
+        
+        # Cadastrar investimentos
+        cdb_rdb = CDB_RDB.objects.create(investidor=nizbel,)
+        HistoricoPorcentagemCDB_RDB.objects.create()
+        
+        
+    def test_investidor_deslogado(self):
+        """Testa investidor deslogado"""
+        pass
+        
+    def test_investidor_logado_sem_investimentos(self):
+        """Testa investidor logado sem investimentos cadastrados"""
+        pass
+        
+    def test_investidor_logado_com_investimentos(self):
+        """Testa investidor logado com investimentos cadastrados"""
+        pass
