f52e79740dbcbb7952bdcb8ff12c0047876efe68
Guilherme de Deus <kingbowserii@gmail.com>
2018-12-18 00:00:20 -0200

    Valor de vencimento de CRI/CRA sendo calculado

--- bagogold/cri_cra/utils/valorizacao.py
+++ bagogold/cri_cra/utils/valorizacao.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
+import datetime
+from decimal import Decimal
+
+from django.db.models.aggregates import Count, Sum
+
 from bagogold.bagogold.models.taxas_indexacao import HistoricoTaxaDI
-from bagogold.bagogold.utils.misc import verifica_se_dia_util
+from bagogold.bagogold.utils.misc import verifica_se_dia_util, \
+    qtd_dias_uteis_no_periodo
 from bagogold.bagogold.utils.taxas_indexacao import \
     calcular_valor_atualizado_com_taxas_di, \
     calcular_valor_atualizado_com_taxas_di_e_juros, \
     calcular_valor_atualizado_com_taxa_prefixado
-from bagogold.cri_cra.models.cri_cra import CRI_CRA, DataRemuneracaoCRI_CRA,\
+from bagogold.cri_cra.models.cri_cra import CRI_CRA, DataRemuneracaoCRI_CRA, \
     DataAmortizacaoCRI_CRA
-from decimal import Decimal
-from django.db.models.aggregates import Count
-import datetime
 
-def calcular_valor_um_cri_cra_na_data(certificado, data=datetime.date.today()):
+
+def calcular_valor_um_cri_cra_na_data(certificado, data=datetime.date.today(), simular_valores=False):
     """
     Calcula o valor de um certificado na data apontada
     
     Parâmetros: Certificado (CRI/CRA)
                 Data
+                Caso não existam valores até o momento, simular com últimos índices?
     Retorno:    Valor na data
     """
     # Pegar último dia útil da data caso não seja útil
@@ -32,23 +37,26 @@ def calcular_valor_um_cri_cra_na_data(certificado, data=datetime.date.today()):
         raise ValueError('Indexador inválido')
     
     # Buscar data inicial, considerando a última data de remuneração antes da data enviada
-    if DataRemuneracaoCRI_CRA.objects.filter(cri_cra=certificado, data__lte=data).exists():
-        data_inicial = DataRemuneracaoCRI_CRA.objects.filter(cri_cra=certificado, data__lte=data).order_by('-data')[0].data
+    if DataRemuneracaoCRI_CRA.objects.filter(cri_cra=certificado, data__lt=data).exists():
+        data_inicial = DataRemuneracaoCRI_CRA.objects.filter(cri_cra=certificado, data__lt=data).order_by('-data')[0].data
     else:
         data_inicial = certificado.data_inicio_rendimento 
     
     # TODO incluir amortizações
-    valor_inicial = certificado.valor_emissao
-    valor_inicial -= (certificado.valor_emissao \
-                      * (DataAmortizacaoCRI_CRA.objects.filter(cri_cra=certificado, data__lte=data).aggregate(soma=Sum('percentual'))['soma'] or 0) / 100)
+    valor_inicial = certificado.valor_emissao \
+        * (1 - (DataAmortizacaoCRI_CRA.objects.filter(cri_cra=certificado, data__lt=data).aggregate(soma=Sum('percentual'))['soma'] or 0) / 100)
     
+    # Para calcular rendimento, não considerar data final como passível de valorização
+    if data == certificado.data_vencimento:
+        data -= datetime.timedelta(days=1)
+        
     # TODO incluir outros cálculos
     if certificado.tipo_indexacao == CRI_CRA.TIPO_INDEXACAO_DI:
-        return calcular_valor_cri_cra_di(valor_inicial, certificado.porcentagem, data_inicial, data, certificado.juros_adicional)
+        return calcular_valor_cri_cra_di(valor_inicial, certificado.porcentagem, data_inicial, data, certificado.juros_adicional, simular_valores)
     elif certificado.tipo_indexacao == CRI_CRA.TIPO_INDEXACAO_PREFIXADO:
         return calcular_valor_cri_cra_prefixado(valor_inicial, certificado.porcentagem, data_inicial, data)
         
-def calcular_valor_cri_cra_di(valor_inicial, percentual_di, data_inicial, data_final, juros_adicional):
+def calcular_valor_cri_cra_di(valor_inicial, percentual_di, data_inicial, data_final, juros_adicional, simular_valores=False):
     """
     Calcula o valor de um certificado atualizado pelo DI
     
@@ -57,6 +65,7 @@ def calcular_valor_cri_cra_di(valor_inicial, percentual_di, data_inicial, data_f
                 Data de início da atualização
                 Data de fim da atualização
                 Juros adicional (percentual ao ano)
+                Calcular valor futuro considerando último índice DI?
     Retorno:    Valor atualizado
     """
     count = 0
@@ -75,6 +84,15 @@ def calcular_valor_cri_cra_di(valor_inicial, percentual_di, data_inicial, data_f
     taxa_qtd_dias = {}
     for taxa in taxas:
         taxa_qtd_dias[Decimal(taxa['taxa'])] = taxa['qtd_dias']
+    
+    # Simular valores com última posição do DI
+    if simular_valores:
+        data_final_di, taxa_final_di = HistoricoTaxaDI.objects.filter(data__range=[data_inicial, data_final]).values_list('data', 'taxa').order_by('-data')[0]
+        qtd_dias_uteis_ate_data = qtd_dias_uteis_no_periodo(data_final_di + datetime.timedelta(days=1), data_final + datetime.timedelta(days=1))
+        # Considerar rendimento do dia anterior
+        taxa_qtd_dias[taxa_final_di] += qtd_dias_uteis_ate_data
+        
+    # Calcular
     if (juros_adicional == 0):
         valor_atualizado = calcular_valor_atualizado_com_taxas_di(taxa_qtd_dias, valor_inicial, percentual_di)
     else:
