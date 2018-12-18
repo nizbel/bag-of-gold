2eda060f15aa3215d1d37f00b1e86a8c24b24e23
Guilherme de Deus <kingbowserii@gmail.com>
2018-12-18 17:45:01 +0000

    Preparando simulador de valor de deb?nture na data espec?fica

--- bagogold/debentures/utils.py
+++ bagogold/debentures/utils.py
@@ -106,3 +106,58 @@ def calcular_valor_debentures_ate_dia_por_divisao(divisao_id, dia=datetime.date.
         qtd_debentures[debenture_codigo] = HistoricoValorDebenture.objects.filter(data__lte=dia, debenture__codigo=debenture_codigo).order_by('-data')[0].valor_total() * qtd_debentures[debenture_codigo]
         
     return qtd_debentures
+    
+def simular_valor_na_data(debenture_id, data=None):
+    """
+    Calcula o valor previsto para debênture em uma data específica
+    
+    Parâmetros: ID da debênture
+                Data
+    Retorno: Valor previsto para a debênture
+    """
+    ultimo_historico = HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=data).order_by('-data')[0]
+    ultima_data = ultimo_historico.data
+    
+    qtd_dias_uteis_ate_data = calcular_qtd_dias_uteis(ultima_data + datetime.timedelta(days=1), data)
+    
+    if qtd_dias_uteis_ate_data == 0:
+        return ultimo_historico.juros + ultimo_historico.valor_nominal
+    
+    # Verificar com a valorização de datas anteriores
+    historicos_anteriores = {}
+    if HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=1))).exists():
+        historicos_anteriores[1] = HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=1))).order_by('-data')[0]
+    if HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=7))).exists():
+        historicos_anteriores[7] = HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=7))).order_by('-data')[0]
+    if HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=30))).exists():
+        historicos_anteriores[30] = HistoricoValorDebenture.objects.filter(debenture=debenture_id, data__lte=(ultima_data - datetime.timedelta(days=30))).order_by('-data')[0]
+    
+    
+    # Calcular de acordo com o índice
+    valorizacoes = {'divisor': 0}
+    valorizacao_final = 0
+
+    # Verificar valorização nos históricos anteriores
+    if 1 in historicos_anteriores and ultimo_historico.juros > historicos_anteriores[1].juros:
+        valorizacoes[1] = ultimo_historico.juros / historicos_anteriores[1].juros
+        valorizacao_final += valorizacoes[1] * 3
+        valorizacoes['divisor'] += 3
+        
+    if 7 in historicos_anteriores and ultimo_historico.juros > historicos_anteriores[7].juros:
+        qtd_dias_uteis_periodo = calcular_qtd_dias_uteis(historicos_anteriores[7].data, ultima_data)
+        valorizacoes[7] = ultimo_historico.juros / historicos_anteriores[7].juros
+        valorizacoes[7] = (1 + valorizacoes[7]/100) ** (Decimal(1)/qtd_duas_uteis_periodo)
+        valorizacao_final += valorizacoes[7] * 2
+        valorizacoes['divisor'] += 2
+        
+    if 30 in historicos_anteriores and ultimo_historico.juros > historicos_anteriores[30].juros:
+        qtd_dias_uteis_periodo = calcular_qtd_dias_uteis(historicos_anteriores[30].data, ultima_data)
+        valorizacoes[30] = ultimo_historico.juros / historicos_anteriores[30].juros
+        valorizacoes[30] = (1 + valorizacoes[30]/100) ** (Decimal(1)/qtd_duas_uteis_periodo)
+        valorizacao_final += valorizacoes[30] * 1
+        valorizacoes['divisor'] += 1
+        
+    valorizacao_final = valorizacao_final / valorizacoes['divisor']
+    
+    return (ultimo_historico.valor_nominal + ultimo_historico.juros) * ((1 + valorizacao_final) ** (qtd_dias_uteis_ate_data))
+            
