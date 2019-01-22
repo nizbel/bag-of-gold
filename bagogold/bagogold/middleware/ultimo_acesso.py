b509d2e65a2d0835000ec92cfc5371c3322af6fa
Guilherme de Deus <kingbowserii@gmail.com>
2019-01-21 23:24:13 -0200

    Atualizar checkpoints at? 5 de janeiro

--- bagogold/bagogold/middleware/ultimo_acesso.py
+++ bagogold/bagogold/middleware/ultimo_acesso.py
@@ -8,8 +8,9 @@ class UltimoAcessoMiddleWare(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if not request.user.is_anonymous():
             if (not request.user.investidor.data_ultimo_acesso) or request.user.investidor.data_ultimo_acesso != datetime.date.today():
-                # Se último acesso foi em ano anterior, atualizar checkpoints
-                if (request.user.investidor.data_ultimo_acesso) and request.user.investidor.data_ultimo_acesso.year < datetime.date.today().year:
+                # Se último acesso foi em ano anterior ou estamos nos primeiros 5 dias do ano, atualizar checkpoints
+                if (request.user.investidor.data_ultimo_acesso and request.user.investidor.data_ultimo_acesso.year < datetime.date.today().year) \
+                    or datetime.date.today() <= datetime.date.today().replace(month=1).replace(day=5):
                     atualizar_checkpoints(request.user.investidor)
                 
                 request.user.investidor.data_ultimo_acesso = datetime.date.today()
