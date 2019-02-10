da08fb494590c48d3be83cd7276c21f8341e9c0d
Guilherme de Deus <kingbowserii@gmail.com>
2019-02-10 20:33:22 -0200

    Corrigindo erro para c?lculo de valor de lci/lca em divis?es

--- bagogold/bagogold/tests/test_views.py
+++ bagogold/bagogold/tests/test_views.py
@@ -6,19 +6,33 @@ from django.contrib.auth.models import User
 from django.test.testcases import TestCase
 from django.urls.base import reverse
 
+from bagogold.bagogold.models.acoes import Acao, OperacaoAcao, HistoricoAcao
+from bagogold.bagogold.models.divisoes import DivisaoOperacaoLCI_LCA, Divisao, \
+    DivisaoOperacaoCDB_RDB, DivisaoOperacaoFII, DivisaoOperacaoCRI_CRA, \
+    DivisaoOperacaoLetraCambio, DivisaoOperacaoAcao, DivisaoOperacaoDebenture, \
+    DivisaoOperacaoFundoInvestimento, DivisaoOperacaoTD
+from bagogold.bagogold.models.empresa import Empresa
+from bagogold.bagogold.models.taxas_indexacao import HistoricoTaxaDI
+from bagogold.bagogold.utils.misc import verifica_se_dia_util
 from bagogold.cdb_rdb.models import CDB_RDB, HistoricoPorcentagemCDB_RDB, \
-    HistoricoCarenciaCDB_RDB, HistoricoVencimentoCDB_RDB, OperacaoCDB_RDB
+    HistoricoCarenciaCDB_RDB, HistoricoVencimentoCDB_RDB, OperacaoCDB_RDB, \
+    OperacaoVendaCDB_RDB
+from bagogold.cri_cra.models.cri_cra import CRI_CRA, OperacaoCRI_CRA
+from bagogold.debentures.models import Debenture, OperacaoDebenture, \
+    HistoricoValorDebenture
+from bagogold.fii.models import FII, OperacaoFII, HistoricoFII
+from bagogold.fundo_investimento.models import FundoInvestimento, \
+    OperacaoFundoInvestimento, HistoricoValorCotas
+from bagogold.fundo_investimento.utils import criar_slug_fundo_investimento_valido
 from bagogold.lc.models import LetraCambio, HistoricoPorcentagemLetraCambio, \
     HistoricoCarenciaLetraCambio, HistoricoVencimentoLetraCambio, \
-    OperacaoLetraCambio
+    OperacaoLetraCambio, OperacaoVendaLetraCambio
 from bagogold.lci_lca.models import LetraCredito, \
     HistoricoPorcentagemLetraCredito, HistoricoCarenciaLetraCredito, \
-    HistoricoVencimentoLetraCredito, OperacaoLetraCredito
-from bagogold.tesouro_direto.models import Titulo, OperacaoTitulo
-from bagogold.cri_cra.models.cri_cra import CRI_CRA, OperacaoCRI_CRA
-from bagogold.debentures.models import Debenture, OperacaoDebenture,\
-    HistoricoValorDebenture
-from bagogold.bagogold.models.taxas_indexacao import HistoricoTaxaDI
+    HistoricoVencimentoLetraCredito, OperacaoLetraCredito, \
+    OperacaoVendaLetraCredito
+from bagogold.tesouro_direto.models import Titulo, OperacaoTitulo,\
+    HistoricoTitulo
 
 
 class ProxVencimentosPainelGeralTestCase(TestCase):
@@ -286,3 +300,180 @@ class ProxVencimentosPainelGeralTestCase(TestCase):
         # Contexto
         self.assertIn('prox_vencimentos', response.context)
         self.assertEqual(response.context['prox_vencimentos'], [])
+
+class ListarDivisoesTestCase(TestCase):
+    
+    def test_investidor_deslogado(self):
+        """Testa acesso de usuário deslogado"""
+        # Acessar
+        response = self.client.get(reverse('divisoes:listar_divisoes'))
+        self.assertEqual(response.status_code, 200)
+        
+        # Verificar dados
+        self.assertEqual(response.context['divisoes'], [])
+        
+    def test_investidor_logado_sem_investimentos(self):
+        """Testa acesso de usuário logado sem nenhum investimento registrado"""
+        # Preparar usuário
+        usuario_sem_investimentos = User.objects.create_user('teste', 'teste@teste.com', 'teste')
+        usuario_sem_investimentos = usuario_sem_investimentos.investidor 
+        
+        # Acessar
+        self.client.login(username='teste', password='teste')
+        response = self.client.get(reverse('divisoes:listar_divisoes'))
+        self.assertEqual(response.status_code, 200)
+        
+        # Verificar dados
+        self.assertEqual(len(response.context['divisoes']), 1)
+        self.assertEqual(response.context['divisoes'][0].valor_atual, 0)
+        
+    def test_investidor_logado_com_investimentos(self):
+        """Testa acesso de usuário logado com investimentos registrados"""
+        # Preparar usuário
+        usuario_sem_investimentos = User.objects.create_user('teste', 'teste@teste.com', 'teste')
+        usuario_sem_investimentos = usuario_sem_investimentos.investidor 
+        divisao = Divisao.objects.get(investidor=usuario_sem_investimentos)
+        
+        data_30_dias_atras = datetime.date.today() - datetime.timedelta(days=30)
+        data_10_dias_atras = datetime.date.today() - datetime.timedelta(days=10)
+        data_100_dias_apos = datetime.date.today() + datetime.timedelta(days=100)
+        # Ações
+        empresa = Empresa.objects.create(nome='Empresa teste', nome_pregao='BBAS')
+        acao = Acao.objects.create(ticker='BBAS3', empresa=empresa)
+        HistoricoAcao.objects.create(acao=acao, data=data_30_dias_atras, preco_unitario=10)
+        HistoricoAcao.objects.create(acao=acao, data=data_10_dias_atras, preco_unitario=10)
+        HistoricoAcao.objects.create(acao=acao, data=datetime.date.today(), preco_unitario=10)
+        
+        compra = OperacaoAcao.objects.create(acao=acao, quantidade=100, preco_unitario=Decimal(10), data=data_30_dias_atras, tipo_operacao='C', 
+                                    consolidada=True, investidor=usuario_sem_investimentos, destinacao='B', corretagem=10, emolumentos=Decimal('0.5'))
+        DivisaoOperacaoAcao.objects.create(divisao=divisao, operacao=compra, quantidade=compra.quantidade)
+        venda = OperacaoAcao.objects.create(acao=acao, quantidade=50, preco_unitario=Decimal(10), data=data_10_dias_atras, tipo_operacao='V', 
+                                    consolidada=True, investidor=usuario_sem_investimentos, destinacao='B', corretagem=10, emolumentos=Decimal('0.5'))
+        DivisaoOperacaoAcao.objects.create(divisao=divisao, operacao=venda, quantidade=venda.quantidade)
+        
+        # CDB/RDB
+        cdb = CDB_RDB.objects.create(nome='CDB teste', investidor=usuario_sem_investimentos, tipo='C', tipo_rendimento=CDB_RDB.CDB_RDB_DI)
+        HistoricoCarenciaCDB_RDB.objects.create(cdb_rdb=cdb, carencia=365)
+        HistoricoVencimentoCDB_RDB.objects.create(cdb_rdb=cdb, vencimento=365)
+        HistoricoPorcentagemCDB_RDB.objects.create(cdb_rdb=cdb, porcentagem=120)
+        
+        compra = OperacaoCDB_RDB.objects.create(cdb_rdb=cdb, quantidade=1000, data=data_30_dias_atras, tipo_operacao='C', investidor=usuario_sem_investimentos)
+        DivisaoOperacaoCDB_RDB.objects.create(operacao=compra, quantidade=compra.quantidade, divisao=divisao)
+        venda = OperacaoCDB_RDB.objects.create(cdb_rdb=cdb, quantidade=500, data=data_10_dias_atras, tipo_operacao='V', 
+                                            investidor=usuario_sem_investimentos)
+        OperacaoVendaCDB_RDB.objects.create(operacao_compra=compra, operacao_venda=venda)
+        DivisaoOperacaoCDB_RDB.objects.create(operacao=venda, quantidade=venda.quantidade, divisao=divisao)
+        
+        # CRI/CRA
+        cri = CRI_CRA.objects.create(nome='CRI teste', codigo_isin='BRZAPSO0393', tipo=CRI_CRA.TIPO_CRI, tipo_indexacao=CRI_CRA.TIPO_INDEXACAO_DI,
+                                     porcentagem=100, juros_adicional=Decimal('0.5'), data_emissao=data_30_dias_atras, valor_emissao=1000,
+                                     data_inicio_rendimento=data_30_dias_atras, data_vencimento=data_100_dias_apos, 
+                                     investidor=usuario_sem_investimentos)
+        
+        compra = OperacaoCRI_CRA.objects.create(cri_cra=cri, preco_unitario=1000, quantidade=2, taxa=0, data=data_30_dias_atras, tipo_operacao='C')
+        DivisaoOperacaoCRI_CRA.objects.create(operacao=compra, quantidade=compra.quantidade, divisao=divisao)
+        venda = OperacaoCRI_CRA.objects.create(cri_cra=cri, preco_unitario=1000, quantidade=1, taxa=0, data=data_10_dias_atras, tipo_operacao='V')
+        DivisaoOperacaoCRI_CRA.objects.create(operacao=venda, quantidade=venda.quantidade, divisao=divisao)
+        
+        # Criptomoedas
+        
+        # Debêntures
+        debenture = Debenture.objects.create(codigo='BBAS63', indice=Debenture.DI, porcentagem=100, data_emissao=data_30_dias_atras, valor_emissao=1000,
+                                 data_inicio_rendimento=data_30_dias_atras, data_vencimento=data_100_dias_apos, incentivada=True, padrao_snd=True)
+        HistoricoValorDebenture.objects.create(debenture=debenture, juros=0, valor_nominal=1000, premio=0, data=data_30_dias_atras)
+        HistoricoValorDebenture.objects.create(debenture=debenture, juros=4, valor_nominal=1000, premio=0, data=data_10_dias_atras)
+        HistoricoValorDebenture.objects.create(debenture=debenture, juros=7, valor_nominal=1000, premio=0, data=datetime.date.today())
+        
+        compra = OperacaoDebenture.objects.create(investidor=usuario_sem_investimentos, debenture=debenture, preco_unitario=1000, quantidade=2,
+                                         data=data_30_dias_atras, tipo_operacao='C', taxa=0)
+        DivisaoOperacaoDebenture.objects.create(divisao=divisao, operacao=compra, quantidade=compra.quantidade)
+        venda = OperacaoDebenture.objects.create(investidor=usuario_sem_investimentos, debenture=debenture, preco_unitario=1000, quantidade=1,
+                                         data=data_10_dias_atras, tipo_operacao='V', taxa=0)
+        DivisaoOperacaoDebenture.objects.create(divisao=divisao, operacao=venda, quantidade=venda.quantidade)
+        
+        # Fundos de Investimento
+        fundo = FundoInvestimento.objects.create(nome='Fundo teste', cnpj='000.000.000/0001-91', data_constituicao=data_30_dias_atras, 
+                                         data_registro=data_30_dias_atras, situacao=FundoInvestimento.SITUACAO_FUNCIONAMENTO_NORMAL,
+                                         tipo_prazo=FundoInvestimento.PRAZO_LONGO, classe=FundoInvestimento.CLASSE_FUNDO_MULTIMERCADO,
+                                         exclusivo_qualificados=False, slug=criar_slug_fundo_investimento_valido('Fundo teste'))
+        HistoricoValorCotas.objects.create(fundo_investimento=fundo, valor_cotas=5, data=data_30_dias_atras)
+        HistoricoValorCotas.objects.create(fundo_investimento=fundo, valor_cotas=6, data=data_10_dias_atras)
+        HistoricoValorCotas.objects.create(fundo_investimento=fundo, valor_cotas=7, data=datetime.date.today())
+        
+        compra = OperacaoFundoInvestimento.objects.create(fundo_investimento=fundo, investidor=usuario_sem_investimentos, quantidade=200,
+                                                 valor=1000, data=data_30_dias_atras, tipo_operacao='C')
+        DivisaoOperacaoFundoInvestimento.objects.create(divisao=divisao, operacao=compra, quantidade=compra.quantidade)
+        venda = OperacaoFundoInvestimento.objects.create(fundo_investimento=fundo, investidor=usuario_sem_investimentos, quantidade=100,
+                                                 valor=700, data=data_10_dias_atras, tipo_operacao='V')
+        DivisaoOperacaoFundoInvestimento.objects.create(divisao=divisao, operacao=venda, quantidade=venda.quantidade)
+        
+        # FIIs
+        fii = FII.objects.create(empresa=empresa, ticker='BBAS11')
+        HistoricoFII.objects.create(fii=fii, data=data_30_dias_atras, preco_unitario=10)
+        HistoricoFII.objects.create(fii=fii, data=data_10_dias_atras, preco_unitario=10)
+        HistoricoFII.objects.create(fii=fii, data=datetime.date.today(), preco_unitario=10)
+        
+        compra = OperacaoFII.objects.create(fii=fii, quantidade=10, preco_unitario=Decimal(100), data=data_30_dias_atras, tipo_operacao='C',
+                                   consolidada=True, investidor=usuario_sem_investimentos, corretagem=10, emolumentos=Decimal('0.5'))
+        DivisaoOperacaoFII.objects.create(operacao=compra, divisao=divisao, quantidade=compra.quantidade)
+        venda = OperacaoFII.objects.create(fii=fii, quantidade=5, preco_unitario=Decimal(100), data=data_10_dias_atras, tipo_operacao='V',
+                                   consolidada=True, investidor=usuario_sem_investimentos, corretagem=10, emolumentos=Decimal('0.5'))
+        DivisaoOperacaoFII.objects.create(operacao=venda, divisao=divisao, quantidade=venda.quantidade)
+        
+        # LCI/LCA
+        lci = LetraCredito.objects.create(nome='LCA teste', investidor=usuario_sem_investimentos, tipo_rendimento=LetraCredito.LCI_LCA_DI)
+        HistoricoCarenciaLetraCredito.objects.create(letra_credito=lci, carencia=365)
+        HistoricoVencimentoLetraCredito.objects.create(letra_credito=lci, vencimento=365)
+        HistoricoPorcentagemLetraCredito.objects.create(letra_credito=lci, porcentagem=92)
+        
+        compra = OperacaoLetraCredito.objects.create(letra_credito=lci, quantidade=1000, data=data_30_dias_atras, tipo_operacao='C', 
+                                            investidor=usuario_sem_investimentos)
+        DivisaoOperacaoLCI_LCA.objects.create(operacao=compra, quantidade=compra.quantidade, divisao=divisao)
+        venda = OperacaoLetraCredito.objects.create(letra_credito=lci, quantidade=500, data=data_10_dias_atras, tipo_operacao='V', 
+                                            investidor=usuario_sem_investimentos)
+        OperacaoVendaLetraCredito.objects.create(operacao_compra=compra, operacao_venda=venda)
+        DivisaoOperacaoLCI_LCA.objects.create(operacao=venda, quantidade=venda.quantidade, divisao=divisao)
+        
+        
+        # Letra de Câmbio
+        lc = LetraCambio.objects.create(nome='LC teste', investidor=usuario_sem_investimentos, tipo_rendimento=LetraCambio.LC_DI)
+        HistoricoCarenciaLetraCambio.objects.create(lc=lc, carencia=365)
+        HistoricoVencimentoLetraCambio.objects.create(lc=lc, vencimento=365)
+        HistoricoPorcentagemLetraCambio.objects.create(lc=lc, porcentagem=120)
+        
+        compra = OperacaoLetraCambio.objects.create(lc=lc, quantidade=1000, data=data_30_dias_atras, tipo_operacao='C', investidor=usuario_sem_investimentos)
+        DivisaoOperacaoLetraCambio.objects.create(operacao=compra, quantidade=compra.quantidade, divisao=divisao)
+        venda = OperacaoLetraCambio.objects.create(lc=lc, quantidade=500, data=data_10_dias_atras, tipo_operacao='V', 
+                                            investidor=usuario_sem_investimentos)
+        OperacaoVendaLetraCambio.objects.create(operacao_compra=compra, operacao_venda=venda)
+        DivisaoOperacaoLetraCambio.objects.create(operacao=venda, quantidade=venda.quantidade, divisao=divisao)
+        
+        # Tesouro Direto
+        titulo = Titulo.objects.create(tipo=Titulo.TIPO_OFICIAL_LETRA_TESOURO, data_inicio=data_30_dias_atras, data_vencimento=data_100_dias_apos)
+        HistoricoTitulo.objects.create(titulo=titulo, data=data_30_dias_atras, preco_compra=700, preco_venda=680, taxa_compra=Decimal(10), taxa_venda=Decimal('9.8'))
+        HistoricoTitulo.objects.create(titulo=titulo, data=data_10_dias_atras, preco_compra=710, preco_venda=690, taxa_compra=Decimal(10), taxa_venda=Decimal('9.8'))
+        HistoricoTitulo.objects.create(titulo=titulo, data=datetime.date.today(), preco_compra=720, preco_venda=700, taxa_compra=Decimal(10), taxa_venda=Decimal('9.8'))
+        
+        compra = OperacaoTitulo.objects.create(investidor=usuario_sem_investimentos, quantidade=1, preco_unitario=700, data=data_30_dias_atras,
+                                      taxa_bvmf=0, taxa_custodia=0, tipo_operacao='C', titulo=titulo, consolidada=True)
+        DivisaoOperacaoTD.objects.create(divisao=divisao, operacao=compra, quantidade=compra.quantidade)
+        venda = OperacaoTitulo.objects.create(investidor=usuario_sem_investimentos, quantidade=Decimal('0.5'), preco_unitario=700, data=data_10_dias_atras,
+                                      taxa_bvmf=0, taxa_custodia=0, tipo_operacao='V', titulo=titulo, consolidada=True)
+        DivisaoOperacaoTD.objects.create(divisao=divisao, operacao=venda, quantidade=venda.quantidade)
+        
+        # Preparar histórico DI
+        data = data_30_dias_atras
+        while data <= datetime.date.today():
+            if verifica_se_dia_util(data):
+                HistoricoTaxaDI.objects.create(data=data, taxa=Decimal('6.5'))
+            data = data + datetime.timedelta(days=1)
+        
+        # Acessar
+        self.client.login(username='teste', password='teste')
+        response = self.client.get(reverse('divisoes:listar_divisoes'))
+        self.assertEqual(response.status_code, 200)
+        
+        # Verificar dados
+        self.assertEqual(len(response.context['divisoes']), 1)
+        self.assertNotEqual(response.context['divisoes'][0].valor_atual, 0)
+        
