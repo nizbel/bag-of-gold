428956a592658528194bc904144574cfc103d7ba
Guilherme de Deus <kingbowserii@gmail.com>
2019-02-11 19:48:35 +0000

    Preparando linha do tempo para tesouro direto

--- bagogold/bagogold/views/divisoes/linha_do_tempo.py
+++ bagogold/bagogold/views/divisoes/linha_do_tempo.py
@@ -51,6 +51,12 @@ def linha_do_tempo(request, divisao_id):
 
 
 def linha_do_tempo_cdb_rdb(divisao):
+    """
+    Carrega dados da linha do tempo para CDB/RDB
+    
+    Parâmetros: Divisão
+    Retorno: Eventos no formato para linha do tempo
+    """
     class Object(object):
         pass
     
@@ -247,5 +253,57 @@ def linha_do_tempo_lci_lca(divisao):
     for evento in eventos:
         evento.saldo = divisao.saldo_lci_lca(evento.data)
         evento.investido = sum(calcular_valor_lci_lca_ate_dia_por_divisao(evento.data, divisao.id).values())
+        
+    return eventos
+
+def linha_do_tempo_tesouro_direto(divisao):
+    """
+    Carrega dados da linha do tempo para Tesouro Direto
+    
+    Parâmetros: Divisão
+    Retorno: Eventos no formato para linha do tempo
+    """
+    class Object(object):
+        pass
+    
+    operacoes_divisao = DivisaoOperacaoTesouroDireto.objects.filter(divisao=divisao).annotate(data=F('operacao__data')) \
+        .annotate(titulo=Case(When(operacao__tipo_operacao='C', then=Value(u'Operação de compra', CharField())),
+                              When(operacao__tipo_operacao='V', then=Value(u'Operação de venda', CharField())), output_field=CharField()))
+    for operacao_divisao in operacoes_divisao:
+        operacao_divisao.operacao.quantidade = operacao_divisao.quantidade
+        operacao_divisao.texto = [operacao_divisao.operacao]
+    
+    # Transferências
+    transf_cedente = TransferenciaEntreDivisoes.objects.filter(divisao_cedente=divisao, investimento_origem=TransferenciaEntreDivisoes.TIPO_INVESTIMENTO_TESOURO_DIRETO)
+    for transferencia in transf_cedente:
+        transferencia.titulo = u'Transferência de recursos da divisão'
+        transferencia.texto = [transferencia]
+        
+    transf_recebedora = TransferenciaEntreDivisoes.objects.filter(divisao_recebedora=divisao, investimento_destino=TransferenciaEntreDivisoes.TIPO_INVESTIMENTO_TESOURO_DIRETO)
+    for transferencia in transf_recebedora:
+        transferencia.titulo = u'Transferência de recursos para a divisão'
+        transferencia.texto = [transferencia]
+        
+    eventos = sorted(chain(transf_recebedora, transf_cedente, operacoes_divisao),
+                            key=attrgetter('data'))
+
+    for indice, evento in enumerate(eventos):
+        # Verifica se há próximo evento
+        while indice + 1 < len(eventos) and eventos[indice + 1].data == evento.data:
+            evento_repetido = eventos.pop(indice + 1)
+            evento.titulo = u'Múltiplos eventos'
+            evento.texto.extend(evento_repetido.texto)
+            
+    # Adicionar data atual para garantir que sempre haja pelo menos 1 evento
+    if len(eventos) == 0 or eventos[-1].data < datetime.date.today():
+        evento = Object()
+        evento.titulo = u'Data atual'
+        evento.texto = [u'Situação na data atual']
+        evento.data = datetime.date.today()
+        eventos.append(evento)        
+            
+    for evento in eventos:
+        evento.saldo = divisao.saldo_td(evento.data)
+        evento.investido = sum(calcular_valor_tesouro_direto_ate_dia_por_divisao(evento.data, divisao.id).values())
         
     return eventos
