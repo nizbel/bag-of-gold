05a45d62dd12e1fe4d3bd1163e78479ddef45e4f
Guilherme de Deus <kingbowserii@gmail.com>
2019-02-26 00:55:17 -0300

    Trazendo altera??es para hotfix Ordena??o de pagamentos Corre??o de formato de data nos forms de TD COrre??o e teste de busca de valores di?rios para TD

--- bagogold/tesouro_direto/management/commands/preencher_valores_diarios_td.py
+++ bagogold/tesouro_direto/management/commands/preencher_valores_diarios_td.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import traceback
+
 from django.core.mail import mail_admins
 from django.core.management.base import BaseCommand
-import traceback
 
 from bagogold import settings
 from bagogold.bagogold.testTD import buscar_valores_diarios
+from bagogold.tesouro_direto.models import ValorDiarioTitulo
 
 
 class Command(BaseCommand):
@@ -13,12 +15,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             valores_diarios = buscar_valores_diarios()
+            
+            # Filtrar valores diários ainda não existentes
+            if len(valores_diarios) > 0:
+                valores_existentes = ValorDiarioTitulo.objects.filter(data_hora=valores_diarios[0].data_hora) \
+                    .values_list('titulo', flat=True)
+                print valores_existentes
+                valores_diarios = [valor_diario for valor_diario in valores_diarios if valor_diario.titulo.id not in valores_existentes]
+            
             for valor_diario in valores_diarios:
                 # Salva apenas valores que não estejam zerados na venda
                 if valor_diario.preco_venda > 0:
                     valor_diario.save()
         except:
             if settings.ENV == 'DEV':
+                if options['test']:
+                    raise
                 print traceback.format_exc()
             elif settings.ENV == 'PROD':
                 mail_admins(u'Erro em Preencher valores diários de Tesouro Direto', traceback.format_exc().decode('utf-8'))
---
+++ bagogold/tesouro_direto/tests/test_commands.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+import datetime
+import re
+from urllib2 import Request, urlopen
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from bagogold.tesouro_direto.models import Titulo, HistoricoTitulo,\
+    ValorDiarioTitulo
+
+
+class ComandoPreencherHistoricoAnoAtualTDTestCase(TestCase):
+    def test_comando(self):
+        """Testa comando de preencher histórico para ano atual de Tesouro Direto"""
+
+        args = []
+        opts = {'test': True}
+        # Roda uma vez para preencher a tabela e outra para verificar o que foi inserido
+        call_command('preencher_historico_ano_atual_td', *args, **opts)
+        call_command('preencher_historico_ano_atual_td', *args, **opts)
+
+        self.assertTrue(Titulo.objects.all().exists())
+        self.assertTrue(HistoricoTitulo.objects.all().exists())
+        
+class ComandoPreencherValoresDiariosTDTestCase(TestCase):
+    def setUp(self):
+        self.assertTrue(Titulo.objects.all().exists())
+    
+    @classmethod
+    def setUpClass(cls):
+        super(ComandoPreencherValoresDiariosTDTestCase, cls).setUpClass()
+        # Popular títulos
+        td_url = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos'
+        req = Request(td_url)
+        response = urlopen(req)
+        data = response.read()
+        string_importante = data[data.find('mercadostatus'):
+                                 data.find('Descubra o título mais indicado para você')]
+        string_compra = string_importante[:string_importante.rfind('mercadostatus')]
+        linhas = re.findall('<tr class="[^"]*?camposTesouroDireto[^"]*?">.*?</tr>', string_compra)
+        contador = 0
+        for linha in linhas:
+            campos = re.findall('<td.*?>.*?</td>', linha)
+            tipo_titulo = ''
+#             print campos
+            for campo in campos:
+                # Parte importante da coluna para o preenchimento dos valores
+                dado = re.sub(r'<.*?>', "", campo).strip()
+#                 print dado
+                if contador == 0:
+                    tipo_titulo = re.sub(r'\d', '', dado).strip()
+#                     print tipo_titulo
+                    tipo_titulo = tipo_titulo.replace('(', '').replace(')', '')
+                elif contador == 1:
+                    data_formatada = datetime.datetime.strptime(dado, "%d/%m/%Y")
+                    Titulo.objects.update_or_create(tipo=Titulo.buscar_vinculo_oficial(tipo_titulo), data_vencimento=data_formatada, 
+                                          data_inicio=(data_formatada - datetime.timedelta(days=730)))
+                # Garante o posicionamento
+                contador += 1
+                if contador == 5:
+                    contador = 0
+                    
+        string_venda = string_importante[string_importante.rfind('mercadostatus'):]
+        linhas = re.findall('<tr class="[^"]*?camposTesouroDireto[^"]*?">.*?</tr>', string_venda)
+        contador = 0
+        for linha in linhas:
+            campos = re.findall('<td.*?>.*?</td>', linha)
+            tipo_titulo = ''
+            for campo in campos:
+                # Parte importante da coluna para o preenchimento dos valores
+                dado = re.sub(r'<.*?>', "", campo).strip()
+#                 print dado
+                if contador == 0:
+                    tipo_titulo = re.sub(r'\d', '', dado).strip()
+#                     print tipo_titulo
+                    tipo_titulo = tipo_titulo.replace('(', '').replace(')', '')
+                elif contador == 1:
+                    data_formatada = datetime.datetime.strptime(dado, "%d/%m/%Y")
+                    Titulo.objects.update_or_create(tipo=Titulo.buscar_vinculo_oficial(tipo_titulo), data_vencimento=data_formatada, 
+                                          data_inicio=(data_formatada - datetime.timedelta(days=730)))
+                # Garante o posicionamento
+                contador += 1
+                if contador == 4:
+                    contador = 0
+        
+    def test_comando_sucesso(self):
+        """Testa comando de preencher valores diários para Tesouro Direto"""
+        
+        args = []
+        opts = {'test': True}
+
+        self.assertFalse(ValorDiarioTitulo.objects.all().exists())
+        
+        call_command('preencher_valores_diarios_td', *args, **opts)
+        
+        self.assertTrue(ValorDiarioTitulo.objects.all().exists())
+        
+    def test_comando_valores_duplicados(self):
+        """Testa comando buscando mesmos valores"""
+        
+        args = []
+        opts = {'test': True}
+        self.assertFalse(ValorDiarioTitulo.objects.all().exists())
+        
+        # Roda 2 vezes para replicar caso de erro em que valores diários são duplicados
+        call_command('preencher_valores_diarios_td', *args, **opts)
+        call_command('preencher_valores_diarios_td', *args, **opts)
+        
+        self.assertTrue(ValorDiarioTitulo.objects.all().exists())
+    
